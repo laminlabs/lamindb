ae803fc8b171dfd4056d7c16f490edd0a30f0116
Alex Wolf <f.alexander.wolf@gmail.com>
2022-05-11 00:36:52 +0200

    :memo: fix html_title

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ from lamin_sphinx import *  # noqa
 import lamindb  # noqa
 
 project = "lamindb"
-html_title = f"{lamindb} | Lamin Labs"
+html_title = f"{project} | Lamin Labs"
 release = lamindb.__version__
 html_context["github_repo"] = "lamindb"  # noqa
 html_sidebars = {
