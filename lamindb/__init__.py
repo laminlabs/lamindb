4bcc2100288b973b2ba51666290565cb530798a3
Alex Wolf <f.alexander.wolf@gmail.com>
2022-05-23 05:09:01 +0200

    :rotating_light: pre-commit run all

--- lamindb/__init__.py
+++ lamindb/__init__.py
@@ -3,8 +3,7 @@
 from . import _version
 
 __version__ = _version.get_versions()["version"]
-from ._logging import logger  # noqa
-
 from . import storage  # noqa
-from .database._notion import Dataset  # noqa
 from ._configure import Configure  # noqa
+from ._logging import logger  # noqa
+from .database._notion import Dataset  # noqa
