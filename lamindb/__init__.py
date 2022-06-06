651e2d3a563dbd678fa14f434500a0a04ebfa6d8
Alex Wolf <f.alexander.wolf@gmail.com>
2022-06-06 18:37:37 +0200

    :memo: update example

--- lamindb/__init__.py
+++ lamindb/__init__.py
@@ -5,5 +5,6 @@ from . import _version
 __version__ = _version.get_versions()["version"]
 from . import storage  # noqa
 from ._configure import Configure  # noqa
+from ._db._notion import Dataset  # noqa
+from ._db._sqlite import DB  # noqa
 from ._logging import logger  # noqa
-from .db._notion import Dataset  # noqa
