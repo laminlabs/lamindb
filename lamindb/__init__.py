490d25d3debf4cd23516d6c4d3c33c4d0898aa63
Alex Wolf <f.alexander.wolf@gmail.com>
2022-06-06 17:28:26 +0200

    :truck: rename database to db

--- lamindb/__init__.py
+++ lamindb/__init__.py
@@ -6,4 +6,4 @@ __version__ = _version.get_versions()["version"]
 from . import storage  # noqa
 from ._configure import Configure  # noqa
 from ._logging import logger  # noqa
-from .database._notion import Dataset  # noqa
+from .db._notion import Dataset  # noqa
