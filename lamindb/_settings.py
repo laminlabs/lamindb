7c2f5c9f4c3a7f9c95def840ba9c85c49435f50e
Alex Wolf <f.alexander.wolf@gmail.com>
2022-06-07 13:52:04 +0200

    :fire: integrate _configure with settings

--- lamindb/_settings.py
+++ lamindb/_settings.py
@@ -5,6 +5,15 @@ from typing import Union
 class Settings:
     def __init__(self, datasetdir: Union[str, Path] = Path("./data/")):
         self._datasetdir = Path(datasetdir)
+        try:
+            from lamindb._configuration import storage_root
+        except ImportError:
+            raise RuntimeError("Please run: lamin configure")
+        self._storage_root = storage_root
+
+    @property
+    def storage_root(self):
+        return self._storage_root
 
     @property
     def datasetdir(self) -> Path:
