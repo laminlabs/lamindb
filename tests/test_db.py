8bf205c727aaf7d49ba49588e05de44b71ca48a8
Alex Wolf <f.alexander.wolf@gmail.com>
2022-06-10 18:53:30 +0200

    :green_heart: Fix tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,15 +3,15 @@ import lamindb as lndb
 
 def test_create_to_load():
 
-    lndb.db.meta.create()
+    lndb._admin.db.setup()
 
     from lamindb._configuration import user_name
 
-    user_id = lndb.db.insert_if_not_exists.user(user_name)  # type: ignore
+    user_id = lndb._admin.db.insert_if_not_exists.user(user_name)  # type: ignore
     print(f"added user {user_id} ({user_name})")
-    lndb.db.insert.file("test_file.csv", source="83jf")
-    for entity in lndb.db.entities:
-        print(lndb.db.load(entity))
+    lndb._admin.db.insert.file("test_file.csv", source="83jf")
+    for entity in lndb.schema.entities:
+        print(lndb.load(entity))
 
 
 if __name__ == "__main__":
