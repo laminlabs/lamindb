6443e1873b6276fb308c03359ffcf303f936c4cd
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-01 18:01:14 +0200

    :wrench: Update logger config

--- lamindb/dev/_logger.py
+++ lamindb/dev/_logger.py
@@ -2,12 +2,11 @@ import sys
 
 from loguru import logger
 
-# all times in UTC
 logger.configure(
     handlers=[
         dict(
             sink=sys.stdout,
-            format="{time:YYYY-MM-DD HH:mm:ss!UTC} | {message}",
+            format="{message}",
         ),
     ],
 )
