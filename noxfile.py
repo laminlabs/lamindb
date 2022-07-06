e2a087101964e66b3c6ace1e24c17c8fd631e3ac
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-07 01:16:52 +0200

    :pencil2: Fix tests

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 import nox
@@ -16,18 +15,6 @@ def lint(session: nox.Session) -> None:
 @nox.session(python=["3.9"])
 def build(session):
     session.install(".[dev,test]")
-    session.run(
-        "lndb",
-        "setup",
-        # "--notion",
-        # os.environ["NOTION_API_KEY"],
-        "--storage",
-        f"{os.environ['HOME']}/data",
-        "--user",
-        "raspbear@gmx.de",
-        "--secret",
-        "A7X5clpvrjcQJWZBFABYLOdIDskIFujXJ9Trhz0P",
-    )
     session.run(
         "pytest",
         "-s",
