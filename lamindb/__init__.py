2a9f14f2af19fcb72874c7c8897c0adf9099ae26
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-20 11:14:42 +0200

    :truck: Move version check into correct __init__ (#82)

--- lamindb/__init__.py
+++ lamindb/__init__.py
@@ -27,6 +27,12 @@ To retrieve settings after setup via the CLI, use:
    :members:
 
 """
+from nbproject import __version__ as nbproject_version
+from packaging import version
+
+if version.parse(nbproject_version) < version.parse("0.4.3"):
+    raise RuntimeError("lamindb needs nbproject >= 0.4.3")
+del version, nbproject_version
 
 __version__ = "0.1.1"
 from . import _setup  # noqa
