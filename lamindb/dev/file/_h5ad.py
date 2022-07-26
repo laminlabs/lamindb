92425b9ee323c5d1317d0ea365f2caacb521297c
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-26 16:24:39 +0200

    :green_heart: More fixes

--- lamindb/dev/file/_h5ad.py
+++ lamindb/dev/file/_h5ad.py
@@ -5,4 +5,4 @@ from lndb_setup import settings
 
 def h5ad_to_anndata(filekey) -> AnnData:
     """h5ad → AnnData."""
-    return anndata.read(settings.storage.local_filepath(filekey))
+    return anndata.read(settings.instance.storage.local_filepath(filekey))
