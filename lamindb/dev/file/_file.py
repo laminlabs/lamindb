8e9a9e73fd1f9dd3111e864d7b1f830f508759f3
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-26 16:28:53 +0200

    :green_heart: Fix up storage_filepath

--- lamindb/dev/file/_file.py
+++ lamindb/dev/file/_file.py
@@ -3,12 +3,12 @@ from pathlib import Path
 from typing import Union
 
 from cloudpathlib import CloudPath
-from lndb_setup import _settings
+from lndb_setup import settings
 
 
 def store_file(filepath: Union[str, Path], filekey: str):
     """Store arbitrary file."""
-    storage_path = _settings.storage_filepath(filekey)
+    storage_path = settings.instance.storage.key_to_filepath(filekey)
     if isinstance(storage_path, CloudPath):
         storage_path.upload_from(filepath)
     else:
