d5eb1096fece940737246e838892e370d00c90da
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-29 23:09:28 +0200

    :arrow_up: Upgrade deps

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -1,9 +1,13 @@
-from lndb_setup import __version__ as lndb_setup_version
-from nbproject import __version__ as nbproject_version
+from lndb_schema_core import __version__ as lndb_schema_core_v
+from lndb_setup import __version__ as lndb_setup_v
+from nbproject import __version__ as nbproject_v
 from packaging import version
 
-if version.parse(nbproject_version) < version.parse("0.4.3"):
+if version.parse(nbproject_v) < version.parse("0.4.3"):
     raise RuntimeError("lamindb needs nbproject >= 0.4.3")
 
-if version.parse(lndb_setup_version) < version.parse("0.2.0"):
+if version.parse(lndb_setup_v) < version.parse("0.2.0"):
+    raise RuntimeError("lamindb needs lndb_setup >= 0.2.0")
+
+if version.parse(lndb_schema_core_v) < version.parse("0.2.0"):
     raise RuntimeError("lamindb needs lndb_setup >= 0.2.0")
