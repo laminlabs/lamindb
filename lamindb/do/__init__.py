e71e797cef8211e581a5f61584edcc291bcfba50
Frédéric ENARD <frederic.enard@gmail.com>
2022-07-29 11:29:53 +0200

    ✨ Enable sharing dobjects & instances in the hub (#69)
    * create push function
    
    * fix notebook output error
    
    * Add auth to enable row level security
    
    * fix notebook output problem
    
    * connect to hub prod
    
    * Add lnid field to inserted dobject
    
    * Fix notebook output error
    
    * Update test
    
    * Add a function to reverse push
    
    * Update test logic
    
    * Update test logic
    
    * Add docstring
    
    * create push function
    
    * fix notebook output error
    
    * Add auth to enable row level security
    
    * fix notebook output problem
    
    * connect to hub prod
    
    * Add lnid field to inserted dobject
    
    * Fix notebook output error
    
    * Update test
    
    * Add a function to reverse push
    
    * Update test logic
    
    * Update test logic
    
    * Add docstring
    
    * create push function
    
    * Update documentation
    
    * Add tests
    
    * Adapt push functions to new schema
    
    * Update imports
    
    * Update documentation
    
    * Reduce number of auth requests
    
    * Fix missing parameter errors
    
    Co-authored-by: Frédéric ENARD <fenard@kize.immo>

--- lamindb/do/__init__.py
+++ lamindb/do/__init__.py
@@ -21,5 +21,6 @@ Modify data:
 from ._delete import delete  # noqa
 from ._ingest import ingest  # noqa
 from ._load import load  # noqa
+from ._push import push_dobject, push_instance, unpush_instance  # noqa
 from ._query import query  # noqa
 from ._update import update  # noqa
---
+++ lamindb/do/_push.py
@@ -0,0 +1,286 @@
+import uuid
+
+from lndb_setup._hub import connect_hub
+from lndb_setup._setup_instance import (
+    load_or_create_instance_settings,
+    load_or_create_user_settings,
+)
+from supabase import Client
+
+from ._load import load
+
+
+def get_hub_with_authentication():
+    user_settings = load_or_create_user_settings()
+    hub = connect_hub()
+    session = hub.auth.sign_in(
+        email=user_settings.user_email, password=user_settings.user_secret
+    )
+    hub.postgrest.auth(session.access_token)
+    return hub
+
+
+def push_instance():
+    hub = get_hub_with_authentication()
+    dobject_df = load("dobject")
+    for id, v in dobject_df.index:
+        push_dobject(id, v, hub)
+
+
+def push_dobject(id: str, v: str, hub: Client = None):
+    if hub is None:
+        hub = get_hub_with_authentication()
+    instance = get_or_create_instance(hub)
+    dobject = load("dobject").loc[[(id, v)]].reset_index().to_dict("records")[0]
+    if not jupynb_exists(dobject["jupynb_id"], dobject["jupynb_v"], hub):
+        insert_jupynb(dobject["jupynb_id"], dobject["jupynb_v"], instance["id"], hub)
+    if not dobject_exists(id, v, hub):
+        insert_dobject(id, v, hub)
+    if not user_dobject_exists(id, v, hub):
+        insert_user_dobject(id, v, hub)
+
+
+def unpush_instance():
+    hub = get_hub_with_authentication()
+    instance = get_or_create_instance(hub)
+    dobject_df = load("dobject")
+    jupynb_df = load("jupynb")
+    for id, v in dobject_df.index:
+        delete_user_dobject(id, v, hub)
+        delete_dobject(id, v, hub)
+    for id, v in jupynb_df.index:
+        delete_jupynb(id, v, hub)
+    delete_user_instance(instance["id"], hub)
+    delete_instance(hub)
+
+
+# def get_user(user_id):
+#     hub = get_hub_with_authentication()
+#     data = (
+#         hub.table("usermeta")
+#         .select("*")
+#         .eq("lnid", user_id)
+#         .execute()
+#     )
+#     if len(data.data) > 0:
+#         return data.data[0]
+#     return None
+
+# Instance
+
+
+def get_or_create_instance(hub: Client):
+    instance = get_instance(hub)
+    if instance is None:
+        instance = insert_instance(hub)
+    if not user_instance_exists(instance["id"], hub):
+        insert_user_instance(instance["id"], hub)
+    return instance
+
+
+def get_instance(hub: Client):
+    instance_settings = load_or_create_instance_settings()
+    data = (
+        hub.table("instance")
+        .select("*")
+        .eq("name", instance_settings.instance_name)
+        .execute()
+    )
+    if len(data.data) > 0:
+        return data.data[0]
+    return None
+
+
+def delete_instance(hub: Client):
+    instance_settings = load_or_create_instance_settings()
+    (
+        hub.table("instance")
+        .delete()
+        .eq("name", instance_settings.instance_name)
+        .execute()
+    )
+
+
+def insert_instance(hub: Client):
+    instance_settings = load_or_create_instance_settings()
+    data = (
+        hub.table("instance")
+        .insert(
+            {
+                "id": str(uuid.uuid4()),
+                "name": instance_settings.instance_name,
+                "owner_id": hub.auth.session().user.id.hex,
+                "storage_dir": str(instance_settings.storage_dir),
+                "dbconfig": instance_settings._dbconfig,
+                "cache_dir": str(instance_settings.cache_dir),
+                "sqlite_file": str(instance_settings._sqlite_file),
+                "sqlite_file_local": str(instance_settings._sqlite_file_local),
+                "db": instance_settings.db,
+            }
+        )
+        .execute()
+    )
+    assert len(data.data) == 1
+    return data.data[0]
+
+
+# User instance
+
+
+def user_instance_exists(instance_id, hub: Client):
+    data = (
+        hub.table("user_instance")
+        .select("*")
+        .eq("user_id", hub.auth.session().user.id.hex)
+        .eq("instance_id", instance_id)
+        .execute()
+    )
+    if len(data.data) > 0:
+        return True
+    return False
+
+
+def delete_user_instance(instance_id, hub: Client):
+    (
+        hub.table("user_instance")
+        .delete()
+        .eq("user_id", hub.auth.session().user.id.hex)
+        .eq("instance_id", instance_id)
+        .execute()
+    )
+
+
+def insert_user_instance(instance_id, hub: Client):
+    data = (
+        hub.table("user_instance")
+        .insert(
+            {
+                "user_id": hub.auth.session().user.id.hex,
+                "instance_id": instance_id,
+            }
+        )
+        .execute()
+    )
+    assert len(data.data) == 1
+    return data.data[0]
+
+
+# Jupynb
+
+
+def jupynb_exists(id, v, hub: Client):
+    data = hub.table("jupynb").select("*").eq("id", id).eq("v", v).execute()
+    if len(data.data) > 0:
+        return True
+    return False
+
+
+def delete_jupynb(id, v, hub: Client):
+    hub.table("jupynb").delete().eq("id", id).eq("v", v).execute()
+
+
+def insert_jupynb(id, v, instance_id, hub: Client):
+    jupynb = load("jupynb").loc[[(id, v)]].reset_index().to_dict("records")[0]
+    data = (
+        hub.table("jupynb")
+        .insert(
+            {
+                "id": jupynb["id"],
+                "v": jupynb["v"],
+                "name": jupynb["name"],
+                "type": jupynb["type"],
+                "instance_id": instance_id,
+                "owner_id": hub.auth.session().user.id.hex,
+                # We could use the get_user function the hub user id from the
+                # lnid of the original creator of a notebook, but this won't
+                # works because RLS policy. So for the moment the owner_id is
+                # the id of the first user that push a dobject from this
+                # notebook.
+                "time_created": jupynb["time_created"].strftime("%Y-%m-%d %H:%M:%S"),
+                "time_updated": jupynb["time_updated"].strftime("%Y-%m-%d %H:%M:%S"),
+            }
+        )
+        .execute()
+    )
+    assert len(data.data) == 1
+    return data.data[0]
+
+
+# Dobject
+
+
+def dobject_exists(id, v, hub: Client):
+    data = hub.table("dobject").select("*").eq("id", id).eq("v", v).execute()
+    if len(data.data) > 0:
+        return True
+    return False
+
+
+def delete_dobject(id, v, hub: Client):
+    hub.table("dobject").delete().eq("id", id).eq("v", v).execute()
+
+
+def insert_dobject(id, v, hub: Client):
+    dobject = load("dobject").loc[[(id, v)]].reset_index().to_dict("records")[0]
+    data = (
+        hub.table("dobject")
+        .insert(
+            {
+                "id": dobject["id"],
+                "v": dobject["v"],
+                "name": dobject["name"],
+                "file_suffix": dobject["file_suffix"],
+                "jupynb_id": dobject["jupynb_id"],
+                "jupynb_v": dobject["jupynb_v"],
+                "time_created": dobject["time_created"].strftime("%Y-%m-%d %H:%M:%S"),
+                "time_updated": dobject["time_updated"].strftime("%Y-%m-%d %H:%M:%S"),
+            }
+        )
+        .execute()
+    )
+    assert len(data.data) == 1
+    return data.data[0]
+
+
+# User dobject
+
+
+def user_dobject_exists(id, v, hub: Client):
+    data = (
+        hub.table("user_dobject")
+        .select("*")
+        .eq("dobject_id", id)
+        .eq("dobject_v", v)
+        .eq("user_id", hub.auth.session().user.id.hex)
+        .execute()
+    )
+    if len(data.data) > 0:
+        return True
+    return False
+
+
+def delete_user_dobject(id, v, hub: Client):
+    (
+        hub.table("user_dobject")
+        .delete()
+        .eq("dobject_id", id)
+        .eq("dobject_v", v)
+        .eq("user_id", hub.auth.session().user.id.hex)
+        .execute()
+    )
+
+
+def insert_user_dobject(id, v, hub: Client):
+    data = (
+        hub.table("user_dobject")
+        .insert(
+            {
+                "dobject_id": id,
+                "dobject_v": v,
+                "user_id": hub.auth.session().user.id.hex,
+            }
+        )
+        .execute()
+    )
+    assert len(data.data) == 1
+    return data.data[0]
