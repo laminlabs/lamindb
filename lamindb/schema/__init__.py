b3ef12fb37517984661a7ed65b7a6e87a8e48676
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-31 13:33:37 +0200

    :memo: Fix API docs

--- lamindb/schema/__init__.py
+++ lamindb/schema/__init__.py
@@ -6,14 +6,13 @@ For a schematic of relations among models, see :class:`track.schema`.
 
 .. currentmodule:: lamindb.schema
 
-.. autosummary::
-   :toctree: .
+See the docs of the schema modules:
 
-   core
-   bionty
-   wetlab
+- `schema-core <https://lamin.ai/docs/lndb-schema-core>`__
+- `schema-bionty <https://lamin.ai/docs/lndb-schema-bionty>`__
+- `schema-wetlab <https://lamin.ai/docs/lndb-schema-wetlab>`__
 
 """
 import lndb_schema_bionty as bionty
+import lndb_schema_core as core
 import lndb_schema_wetlab as wetlab
-from lamindb_schema import core
