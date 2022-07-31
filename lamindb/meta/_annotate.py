4cd3e4cd3c644403d2ca1b3a3da89a5ec4fbf1fe
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-08-01 00:44:57 +0800

    :sparkles: Annotate metadata

--- lamindb/meta/_annotate.py
+++ lamindb/meta/_annotate.py
@@ -1,23 +1,12 @@
 from typing import Optional  # noqa
 
-# from bioreader import lookup
+from bioreader import lookup
 from tabulate import tabulate  # type: ignore
 
 from .._logger import colors, logger
 from ..dev.db import insert
-
-# from ..do._query import query
-
-
-def anndata_to_df(adata, obs_or_var):
-    """Get a df from AnnData."""
-    if obs_or_var == "var":
-        df = adata.var
-    elif obs_or_var == "obs":
-        df = adata.obs
-    else:
-        raise KeyError("Please specify obs or var!")
-    return df
+from ..do._query import query
+from ..do._update import update
 
 
 class annotate:
@@ -39,7 +28,7 @@ class annotate:
         # use the geneset_id and readout_type_id to create an entry in biometa
         biometa_id = insert.biometa(
             dobject_id=dobject_id,
-            geneset_id=geneset_id,
+            featureset_id=geneset_id,
         )
 
         logs = [[str(geneset_id), str(biometa_id)]]
@@ -55,25 +44,38 @@ class annotate:
             f"Annotated data {dobject_id} with the following features:\n{log_table}",
         )
 
-    # @classmethod
-    # def readout_type(
-    #     cls,
-    #     readout_type: lookup.READOUT_TYPES,
-    #     readout_platform: Optional[lookup.READOUT_PLATFORMS],
-    # ):
-    #     # register the readout if not yet in the database
-    #     readout_results = query.readout_type(
-    #         name=readout_type, platform=readout_platform
-    #     )
-    #     if len(readout_results) == 0:
-    #         readout_type_id = insert.readout_type(
-    #             name=readout_type, platform=readout_platform
-    #         )
-    #     else:
-    #         readout_type_id = readout_results[0].id
+    @classmethod
+    def readout_type(
+        cls,
+        dobject_id,
+        readout_type: lookup.READOUT_TYPES,
+        readout_platform: Optional[lookup.READOUT_PLATFORMS],
+    ):
+        # register the readout if not yet in the database
+        readout_results = query.readout_type(
+            name=readout_type, platform=readout_platform
+        )
+        if len(readout_results) == 0:
+            readout_type_id = insert.readout_type(
+                name=readout_type, platform=readout_platform
+            )
+            logger.success(f"Registered readout_type: {readout_type_id}")
+        else:
+            readout_type_id = readout_results[0].id
 
-    #     # query biometa
+        # query biometa associated with a dobject
+        dobject_biometa = query.dobject_biometa(dobject_id=dobject_id)
+        if len(dobject_biometa) > 0:
+            biometa_ids = [i.biometa_id for i in dobject_biometa]
+        else:
+            raise AssertionError(f"No biometa found for dobject {dobject_id}")
 
-    #     # fill in biometa entries with readout_type_id
+        # fill in biometa entries with readout_type_id
+        for biometa_id in biometa_ids:
+            update.biometa(biometa_id, readout_type_id=readout_type_id)
 
-    #     return readout_type_id
+        logger.success(
+            f"{colors.blue(f'readout_type_id {readout_type_id}')} has been added to"
+            f" {colors.purple(f'biometa entries {biometa_ids}')} associated with"
+            f" {colors.green(f'dobject {dobject_id}')}."
+        )
