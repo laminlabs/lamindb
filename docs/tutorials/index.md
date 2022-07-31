d41870e6ba633f69ebff4c166a3bd2cfa05ffb49
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-08-01 01:17:24 +0800

    ✨ Query and update metadata (#113)
    * :sparkles: Annotate metadata
    
    * :memo: added annotate_metadata nb to the index
    
    * :bug: fixed wetlab imports
    
    * :art: fixed bugs and prettified nb

--- docs/tutorials/index.md
+++ docs/tutorials/index.md
@@ -7,6 +7,7 @@ get-started
 collaborate
 ingest-feature
 ingest-biology
+query-annotate-metadata
 introspect-data
 share
 concepts
--- lamindb/do/_update.py
+++ lamindb/do/_update.py
@@ -1,3 +1,23 @@
-def update():
+from lndb_setup import settings
+from sqlmodel import Session
+
+from .. import schema
+
+
+class update:
     """Update data."""
-    raise NotImplementedError
+
+    @classmethod
+    def biometa(
+        cls,
+        id,
+        **kwargs,
+    ):
+        """Query biometa by its id to update the entry fields."""
+        with Session(settings.instance.db_engine()) as session:
+            biometa = session.get(schema.wetlab.biometa, id)
+            for k, v in kwargs.items():
+                biometa.__setattr__(k, int(v))
+            session.add(biometa)
+            session.commit()
+            session.refresh(biometa)
