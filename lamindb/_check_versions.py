0e5b786d7192885fd581e318790f09bc45ee5c53
Frédéric ENARD <frederic.enard@gmail.com>
2022-08-22 23:27:05 +0200

    Update lndb_hub version checking

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -10,8 +10,8 @@ if version.parse(lnschema_core_v) != version.parse("0.4.0"):
 if version.parse(lndb_setup_v) != version.parse("0.5.4"):
     raise RuntimeError("lamindb needs lndb_setup==0.5.4")
 
-if version.parse(lndb_hub_v) != version.parse("0.4.0"):
-    raise RuntimeError("lamindb needs lndb_hub==0.4.0")
+if version.parse(lndb_hub_v) != version.parse("0.5.0"):
+    raise RuntimeError("lamindb needs lndb_hub==0.5.0")
 
 if version.parse(nbproject_v) < version.parse("0.4.3"):
     raise RuntimeError("lamindb needs nbproject >= 0.4.3")
