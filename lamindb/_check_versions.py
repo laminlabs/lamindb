f6ed3fc2a7dee5b872bacde80c6c917bef081a0e
Alex Wolf <f.alexander.wolf@gmail.com>
2022-08-23 15:53:04 +0200

    :arrow_up: Pin lnbfx version

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -1,3 +1,4 @@
+from lnbfx import __version__ as lnbfx_v
 from lndb_hub import __version__ as lndb_hub_v
 from lndb_setup import __version__ as lndb_setup_v
 from lnschema_core import __version__ as lnschema_core_v
@@ -15,3 +16,6 @@ if version.parse(lndb_hub_v) != version.parse("0.5.0"):
 
 if version.parse(nbproject_v) < version.parse("0.4.3"):
     raise RuntimeError("lamindb needs nbproject >= 0.4.3")
+
+if version.parse(lnbfx_v) != version.parse("0.2.0"):
+    raise RuntimeError("lamindb needs nbproject == 0.2.0")
