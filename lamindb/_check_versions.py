c090b96d587b0e96f5cee77d8f86b742e6491ec7
Alex Wolf <f.alexander.wolf@gmail.com>
2022-08-25 13:55:20 +0200

    :arrow_up: Upgrade wetlab and setup

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -2,14 +2,18 @@ from lnbfx import __version__ as lnbfx_v
 from lndb_hub import __version__ as lndb_hub_v
 from lndb_setup import __version__ as lndb_setup_v
 from lnschema_core import __version__ as lnschema_core_v
+from lnschema_wetlab import __version__ as lnschema_wetlab_v
 from nbproject import __version__ as nbproject_v
 from packaging import version
 
 if version.parse(lnschema_core_v) != version.parse("0.4.1"):
     raise RuntimeError("lamindb needs lnschema_core==0.4.1")
 
-if version.parse(lndb_setup_v) != version.parse("0.5.5"):
-    raise RuntimeError("lamindb needs lndb_setup==0.5.5")
+if version.parse(lnschema_wetlab_v) != version.parse("0.3.0"):
+    raise RuntimeError("lamindb needs lnschema_wetlab==0.3.0")
+
+if version.parse(lndb_setup_v) != version.parse("0.6.0"):
+    raise RuntimeError("lamindb needs lndb_setup==0.6.0")
 
 if version.parse(lndb_hub_v) != version.parse("0.5.1"):
     raise RuntimeError("lamindb needs lndb_hub==0.5.1")
