8c86b96a54d907f9dcc59b8475987f5f24ef337a
Alex Wolf <f.alexander.wolf@gmail.com>
2022-08-29 14:25:38 +0200

    :memo: Fix type

--- lamindb/dev/_core.py
+++ lamindb/dev/_core.py
@@ -19,7 +19,7 @@ def filepath_from_dobject(dobj: dobject):
     return filepath
 
 
-def track_usage(dobject_id, dobject_v, usage_type: type.usage_type):
+def track_usage(dobject_id, dobject_v, usage_type: type.usage):
     with sqm.Session(settings.instance.db_engine()) as session:
         usage = schema.core.usage(
             type=usage_type,
