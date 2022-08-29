a052aaf6bc7d59fb70405821bc1e7e716096c3c5
Alex Wolf <f.alexander.wolf@gmail.com>
2022-08-29 14:17:18 +0200

    :art: Simplify load function

--- lamindb/do/_query.py
+++ lamindb/do/_query.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from lndb_setup import settings
 from sqlmodel import Session, select
 
@@ -38,7 +39,18 @@ def _create_query_func(name: str, schema_module):
 class query:
     """Query literal (semantic) data."""
 
-    pass
+    @classmethod
+    def table_to_df(cls, entity_name) -> pd.DataFrame:
+        """Load metadata table as DataFrame."""
+        engine = settings.instance.db_engine()
+        with engine.connect() as conn:
+            df = pd.read_sql_table(entity_name, conn)
+            if "id" in df.columns:
+                if "v" in df.columns:
+                    df = df.set_index(["id", "v"])
+                else:
+                    df = df.set_index("id")
+        return df
 
 
 def featureset(
