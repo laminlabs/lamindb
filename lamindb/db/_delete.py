96de2a87ad9cb8c0b4c72425f322353d79ece00f
Alex Wolf <f.alexander.wolf@gmail.com>
2022-08-30 12:28:59 +0200

    :memo: Fix docs

--- lamindb/db/_delete.py
+++ lamindb/db/_delete.py
@@ -2,38 +2,41 @@ from lndb_setup import settings
 from sqlmodel import Session
 
 from .._logger import colors, logger
-from ..dev import track_usage
+from ..dev import storage_key_from_dobject, track_usage
+from ..dev.file import delete_file
 from ..schema._schema import alltables
 
 
 def _create_delete_func(name: str, schema_module):
-    def delete_func(cls, key, **kwargs):
+    def delete_func(cls, key):
         with Session(settings.instance.db_engine()) as session:
             entry = session.get(schema_module, key)
-            for k, v in kwargs.items():
-                if isinstance(k, tuple):
-                    k[1] = int(k[1])
-                entry.__setattr__(k, v)
             session.delete(entry)
             session.commit()
+            settings.instance._update_cloud_sqlite_file()
             logger.success(
-                f"Deleted {colors.yellow(f'entry {key}')} in"
-                f" {colors.blue(f'table {name}')}!"
+                f"Deleted {colors.yellow(f'row {key}')} in"
+                f" {colors.blue(f'table {name}')}."
             )
             if name == "dobject":
                 track_usage(entry.id, entry.v, "delete")
-
-        settings.instance._update_cloud_sqlite_file()
+                storage_key = storage_key_from_dobject(entry)
+                delete_file(storage_key)
+                logger.success(f"Deleted {colors.yellow(storage_key)} from storage.")
 
     delete_func.__name__ = name
     return delete_func
 
 
 class delete:
-    """Delete an entry based on its primary identifier.
+    """Delete data by primary key.
+
+    Deletes the row identified by the key in the table that represents the entity.
+    For `dobject`, this also deletes the data object in storage.
 
     Example:
-    >>> delete.{entity}(id=1)
+
+    >>> delete.dobject(key=("MHnsI0TWWNu3VBsedg6gS", "1"))
     """
 
     pass
