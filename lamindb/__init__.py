acb9934608407b3fd0a3d150d3fc513d03a85315
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-09-01 13:39:56 +0200

    :bookmark: Release version 0.3.4

--- lamindb/__init__.py
+++ lamindb/__init__.py
@@ -19,7 +19,7 @@ To retrieve settings, use `lamindb.settings <https://lamin.ai/docs/lndb-setup/ln
 """
 from . import _check_versions
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 from lndb_setup import settings  # noqa
 
 from . import datasets  # noqa
