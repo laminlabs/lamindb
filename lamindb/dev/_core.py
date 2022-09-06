a6a8fd662a875c22aac940f49b9ba7e32abc655e
bpenteado <33378855+bpenteado@users.noreply.github.com>
2022-09-06 11:05:37 +0100

    ♻️ Segregate pipeline ingestion (runs, pipelines, and pipeline-linked dobjects) from non-pipeline ingestion (dobjects) (#183)
    Co-authored-by: Sunny Sun <38218185+sunnyosun@users.noreply.github.com>

--- lamindb/dev/_core.py
+++ lamindb/dev/_core.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import sqlmodel as sqm
 from lndb_setup import settings
 from lnschema_core import dobject, type
@@ -36,24 +34,3 @@ def track_usage(dobject_id, dobject_v, usage_type: type.usage):
     settings.instance._update_cloud_sqlite_file()
 
     return usage.id
-
-
-def format_pipeline_logs(logs):
-    pipeline_dir_logs = {}
-    for log in list(logs):
-        rel_filepath = Path(log[0].split(" ")[0])
-        n_parents = len(rel_filepath.parents) - 1
-        if n_parents > 2:
-            logs.remove(log)
-            top_dir = str(list(rel_filepath.parents)[-4])
-            if top_dir in pipeline_dir_logs.keys():
-                pipeline_dir_logs[top_dir][0] += 1
-            else:
-                jupynb_log = log[1]
-                settings_log = log[2]
-                pipeline_dir_logs[top_dir] = [1, jupynb_log, settings_log]
-
-    for dir, new_logs in pipeline_dir_logs.items():
-        logs.append([f"{new_logs[0]} files in {dir}/", new_logs[1], new_logs[2]])
-
-    return logs
