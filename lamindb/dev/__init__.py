192f97bb262105b46f0e2932465e6c09052b88de
Bernardo Penteado <bernardopbf3@gmail.com>
2022-09-13 10:27:18 +0100

    :truck: moved format_pipeline_logs to dev/_pipeline.py

--- lamindb/dev/__init__.py
+++ lamindb/dev/__init__.py
@@ -21,9 +21,9 @@ from . import db  # noqa
 from . import file, object  # noqa
 from ._core import (
     filepath_from_dobject,
-    format_pipeline_logs,
     storage_key_from_dobject,
     storage_key_from_triple,
     track_usage,
 )
 from ._docs import doc_args  # noqa
+from ._pipeline import format_pipeline_logs
