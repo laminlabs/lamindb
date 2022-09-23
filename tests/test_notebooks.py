5f819a6d7850d4683bc478aeb14c05e7bf52e3b4
Alex Wolf <f.alexander.wolf@gmail.com>
2022-09-23 23:41:48 +0200

    :construction_worker: Improve nb test function

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -7,11 +7,9 @@ from lamindb._logger import logger
 
 def test_notebooks():
     # assuming this is in the tests folder
-    docs_folder = Path(__file__).parents[1] / "docs/"
+    docsdir = Path(__file__).parents[1] / "docs/"
 
-    for check_folder in docs_folder.glob("./**"):
-        # these are the typical notebook testpaths
-        if not str(check_folder).endswith(("faq", "guide")):
-            continue
-        logger.info(f"\n---{check_folder.stem}---")
-        test.execute_notebooks(check_folder, write=True)
+    for subdir in ["guide", "faq"]:
+        checkdir = docsdir / subdir
+        logger.info(f"\n---{checkdir.stem}---")
+        test.execute_notebooks(checkdir, write=True)
