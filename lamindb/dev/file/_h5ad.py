60c2b66556eefd2813dfeede015075103e45d63b
Koncopd <koncopd@gmail.com>
2022-09-25 19:01:16 +0200

    additional args for h5ad

--- lamindb/dev/file/_h5ad.py
+++ lamindb/dev/file/_h5ad.py
@@ -9,7 +9,7 @@ def h5ad_to_anndata(filekey) -> AnnData:
     return anndata.read(settings.instance.storage.local_filepath(filekey))
 
 
-def read_adata_h5ad(filepath) -> AnnData:
+def read_adata_h5ad(filepath, **kwargs) -> AnnData:
     with fsspec.open(filepath, mode="rb") as file:
-        adata = read_h5ad(file, backed=False)
+        adata = read_h5ad(file, backed=False, **kwargs)
         return adata
