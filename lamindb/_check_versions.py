785bd4d019a3a133b901792538fe150577184199
Alex Wolf <f.alexander.wolf@gmail.com>
2022-09-30 15:12:55 +0200

    :pencil2: Fix test

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -8,11 +8,11 @@ from lnschema_wetlab import __version__ as lnschema_wetlab_v
 from nbproject import __version__ as nbproject_v
 from packaging import version
 
-if version.parse(lnschema_core_v) != version.parse("0.8.1"):
-    raise RuntimeError("lamindb needs lnschema_core==0.8.1")
+if version.parse(lnschema_core_v) != version.parse("0.9.0"):
+    raise RuntimeError("lamindb needs lnschema_core==0.9.0")
 
-if version.parse(lnschema_wetlab_v) != version.parse("0.4.3"):
-    raise RuntimeError("lamindb needs lnschema_wetlab==0.4.3")
+if version.parse(lnschema_wetlab_v) != version.parse("0.5.0"):
+    raise RuntimeError("lamindb needs lnschema_wetlab==0.5.0")
 
 if version.parse(lnschema_bionty_v) != version.parse("0.4.2"):
     raise RuntimeError("lamindb needs lnschema_bionty==0.4.2")
@@ -26,8 +26,8 @@ if version.parse(lndb_hub_v) != version.parse("0.5.6"):
 if version.parse(nbproject_v) < version.parse("0.5.3"):
     raise RuntimeError("lamindb needs nbproject>=0.5.3")
 
-if version.parse(lnbfx_v) < version.parse("0.3.10"):
-    raise RuntimeError("lamindb needs lnbfx>=0.3.10")
+if version.parse(lnbfx_v) < version.parse("0.4.0"):
+    raise RuntimeError("lamindb needs lnbfx>=0.4.0")
 
 if version.parse(bioreadout_v) < version.parse("0.1.2"):
     raise RuntimeError("lamindb needs bioreadout>=0.1.2")
