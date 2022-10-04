799103ee0e195b0d317ec665db3c4d54a969d393
Frédéric ENARD <frederic.enard@gmail.com>
2022-10-04 08:28:34 +0200

    Enable setup outside cli (#250)

--- lamindb/__init__.py
+++ lamindb/__init__.py
@@ -17,15 +17,20 @@ Browse the API:
    dev
 
 """
+
 __version__ = "0.5.0"
+
+import warnings
+
 from lndb_setup import settings  # noqa
 from lndb_setup._migrate import check_migrate as _check_migrate
 
 from . import _check_versions  # executes checks during import
 
 if settings.instance.storage_dir is None:
-    raise RuntimeError("Please run `lndb init` to configure an instance.")
-_check_migrate(usettings=settings.user, isettings=settings.instance)
+    warnings.warn("Please run `lndb init` to configure an instance.")
+else:
+    _check_migrate(usettings=settings.user, isettings=settings.instance)
 
 from . import datasets  # noqa
 from . import db  # noqa
