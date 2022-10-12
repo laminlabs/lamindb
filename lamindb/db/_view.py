d4ed948a82bdbe50b09e63d749ed5100e6724141
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-10-13 01:22:52 +0200

    :bug: View only prints existing tables

--- lamindb/db/_view.py
+++ lamindb/db/_view.py
@@ -4,6 +4,7 @@ from IPython.display import display
 
 from .. import schema
 from ..dev.db._select import select
+from ..schema import list_entities
 
 
 def view(n: int = 10, schema_modules: Optional[list] = None):
@@ -24,16 +25,20 @@ def view(n: int = 10, schema_modules: Optional[list] = None):
         ]
         schema_modules = ["core"] + modules
 
+    all_tables = list_entities()
+
     # core tables always display first
     for module in schema_modules:
-        divider = "*" * 5
-        print(f"{divider} {module} {divider}")
         schema_module = getattr(schema, module)
         tables = [
             i
             for i in schema_module.__dir__()
             if getattr(schema_module, i).__class__.__name__ == "SQLModelMetaclass"
         ]
+        if len(set(tables).intersection(all_tables)) == 0:
+            continue
+        divider = "*" * 5
+        print(f"{divider} {module} {divider}")
         for entity in tables:
             df = getattr(select, entity)().df()
             if df.shape[0] > 0:
