e404f2dfed44cacb847b8c87b1ec959413d5cc5a
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-10-12 23:44:31 +0200

    :art: simplified in introspect

--- lamindb/db/_view.py
+++ lamindb/db/_view.py
@@ -1,23 +1,31 @@
+from typing import Optional
+
 from IPython.display import display
 
 from .. import schema
 from ..dev.db._select import select
 
 
-def view(n: int = 10):
+def view(n: int = 10, schema_modules: Optional[list] = None):
     """View the latest edits to the database.
 
     Args:
         n: display the latest n rows of the tables
+        schema_modules: a list of schema modules to view
+            if None: will view all schema modules
     """
-    modules = [
-        i
-        for i in schema.__dir__()
-        if getattr(schema, i).__class__.__name__ == "module" and not i.startswith("_")
-    ]
+    if schema_modules is None:
+        modules = [
+            i
+            for i in schema.__dir__()
+            if getattr(schema, i).__class__.__name__ == "module"
+            and not i.startswith("_")  # noqa
+            and i != "core"  # noqa
+        ]
+        schema_modules = ["core"] + modules
 
     # core tables always display first
-    for module in ["core"] + [i for i in modules if i != "core"]:
+    for module in schema_modules:
         divider = "*" * 5
         print(f"{divider} {module} {divider}")
         schema_module = getattr(schema, module)
