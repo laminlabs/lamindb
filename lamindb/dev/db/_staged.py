f881d13f896bb8ce7281ad25c20e3793f746ef1b
Sergei Rybakov <koncopd@gmail.com>
2022-10-12 12:19:19 +0200

    :sparkles: Add streaming zarr writes and streaming h5ad and zarr reads (#277)

--- lamindb/dev/db/_staged.py
+++ lamindb/dev/db/_staged.py
@@ -1,11 +1,13 @@
 import hashlib
 from pathlib import Path
-from typing import Any
+from sys import getsizeof
+from typing import Any, Optional
 
 import lnschema_core as core
+from lndb_setup import settings
 
 from .._core import get_name_suffix_from_filepath
-from ..file import store_file
+from ..file import store_file, write_adata_zarr
 from ..object import infer_suffix, write_to_file
 from ._insert import insert
 from ._linkstaged import LinkStaged
@@ -22,6 +24,7 @@ class Staged:
         dtransform: The data transform that links to the data source of the data object.
         name: name of the data object, required of an in-memory object is passed
         dobject_id: id of the dobject
+        adata_format: Use `h5ad` or `zarr` to store an `AnnData` object
     """
 
     def __init__(
@@ -29,8 +32,9 @@ class Staged:
         data: Any,
         *,
         dtransform: core.dtransform,
-        name: str = None,
-        dobject_id: str = None,
+        name: Optional[str] = None,
+        dobject_id: Optional[str] = None,
+        adata_format: Optional[str] = None,
     ) -> None:
         self._data = data  # input data object provided by user
         self._dmem = None  # in-memory object
@@ -41,19 +45,22 @@ class Staged:
             name, suffix = get_name_suffix_from_filepath(self.filepath)
         else:
             # if in-memory object is given, return the cache path
+            # if zarr, then filename is just name.zarr
             self._dmem = data
-            suffix = infer_suffix(data)
+            suffix = infer_suffix(data, adata_format)
             if name is None:
                 raise RuntimeError("Provide name if ingesting in-memory data.")
             self._filepath = Path(f"{name}{suffix}")
             # write to file
             # TODO: should this raise an error/warning if filepath exists?
-            if not self.filepath.exists():
+            if suffix != ".zarr" and not self.filepath.exists():
                 write_to_file(self.dmem, self.filepath)  # type: ignore
 
         self._dobject = core.dobject(name=name, suffix=suffix)
         self._dobject.id = dobject_id if dobject_id is not None else self.dobject.id
-        self._dobject.checksum = compute_checksum(self._filepath)
+        # streamed
+        if suffix != ".zarr":
+            self._dobject.checksum = compute_checksum(self._filepath)
 
         # access to the feature model
         self._feature_model = None  # feature model
@@ -118,10 +125,16 @@ class Staged:
 
     def _commit(self) -> None:
         """Store and insert dobject and its linked entries."""
-        # store dobject
         dobject_storage_key = f"{self.dobject.id}{self.dobject.suffix}"
-        size = store_file(self.filepath, dobject_storage_key)
-        self._dobject.size = size  # size is only calculated when storing the file
+
+        if self.dobject.suffix != ".zarr":
+            size = store_file(self.filepath, dobject_storage_key)
+        else:
+            # adata size
+            size = getsizeof(self.dmem)
+            storepath = settings.instance.storage.key_to_filepath(dobject_storage_key)
+            write_adata_zarr(self.dmem, storepath)
+        self._dobject.size = size
 
         # insert dobject first to satisfy foreign key constraints
         insert.dobject_from_dtransform(  # type:ignore
--- lamindb/dev/object/_core.py
+++ lamindb/dev/object/_core.py
@@ -1,10 +1,16 @@
+from typing import Optional
+
 from anndata import AnnData
 from pandas import DataFrame
 
 
-def infer_suffix(dmem):
+def infer_suffix(dmem, adata_format: Optional[str] = None):
     """Infer LaminDB storage file suffix from a data object."""
     if isinstance(dmem, AnnData):
+        if adata_format is not None:
+            if adata_format not in ("h5ad", "zarr"):
+                raise ValueError
+            return "." + adata_format
         return ".h5ad"
     elif isinstance(dmem, DataFrame):
         return ".feather"
