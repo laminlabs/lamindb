fca4fa460cf5f99d1a10d72228c0f683259a791c
Alex Wolf <f.alexander.wolf@gmail.com>
2022-10-13 09:29:18 +0200

    :memo: Update docs

--- lamindb/_nb.py
+++ lamindb/_nb.py
@@ -2,5 +2,8 @@ import nbproject as nb  # noqa
 
 nb.__doc__ = """Manage Jupyter notebooks.
 
-See `nbproject <https://lamin.ai/docs/nbproject>`__.
+You'll typically only need one function in this module: `header()` initializes
+and prints metadata for a Jupyter notebook.
+
+For more details, see `nbproject <https://lamin.ai/docs/nbproject>`__.
 """
