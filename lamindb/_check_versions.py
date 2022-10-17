bc703378462cd133fe28864042ce4e448c57f324
Bernardo Penteado <bernardopbf3@gmail.com>
2022-10-17 15:33:01 +0200

    :arrow_up: upgraded lnbfx to 0.4.3

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -26,8 +26,8 @@ if version.parse(lndb_hub_v) != version.parse("0.5.6"):
 if version.parse(nbproject_v) < version.parse("0.7.0"):
     raise RuntimeError("lamindb needs nbproject>=0.7.0")
 
-if version.parse(lnbfx_v) < version.parse("0.4.2"):
-    raise RuntimeError("lamindb needs lnbfx>=0.4.2")
+if version.parse(lnbfx_v) < version.parse("0.4.3"):
+    raise RuntimeError("lamindb needs lnbfx>=0.4.3")
 
 if version.parse(bioreadout_v) < version.parse("0.2.1"):
     raise RuntimeError("lamindb needs bioreadout>=0.2.1")
