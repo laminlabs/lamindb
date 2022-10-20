740e0b209d68780c93a0cf51d63f6c21d10bc336
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-10-20 12:00:36 +0200

    :arrow_up: updated lndb-setup and lnbfx versions

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -17,8 +17,8 @@ if version.parse(lnschema_wetlab_v) != version.parse("0.7.2"):
 if version.parse(lnschema_bionty_v) != version.parse("0.4.3"):
     raise RuntimeError("lamindb needs lnschema_bionty==0.4.3")
 
-if version.parse(lndb_setup_v) != version.parse("0.12.1"):
-    raise RuntimeError("lamindb needs lndb_setup==0.12.1")
+if version.parse(lndb_setup_v) != version.parse("0.12.2"):
+    raise RuntimeError("lamindb needs lndb_setup==0.12.2")
 
 if version.parse(lndb_hub_v) != version.parse("0.5.6"):
     raise RuntimeError("lamindb needs lndb_hub==0.5.6")
@@ -26,8 +26,8 @@ if version.parse(lndb_hub_v) != version.parse("0.5.6"):
 if version.parse(nbproject_v) < version.parse("0.7.0"):
     raise RuntimeError("lamindb needs nbproject>=0.7.0")
 
-if version.parse(lnbfx_v) < version.parse("0.4.2"):
-    raise RuntimeError("lamindb needs lnbfx>=0.4.2")
+if version.parse(lnbfx_v) < version.parse("0.4.4"):
+    raise RuntimeError("lamindb needs lnbfx>=0.4.4")
 
 if version.parse(bioreadout_v) < version.parse("0.2.1"):
     raise RuntimeError("lamindb needs bioreadout>=0.2.1")
