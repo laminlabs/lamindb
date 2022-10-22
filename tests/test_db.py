44b82dcf535007905d2e7f4fb3c986ab18e68bb0
Alex Wolf <f.alexander.wolf@gmail.com>
2022-10-23 01:40:11 +0200

    :green_heart: Fix

--- tests/test_db.py
+++ tests/test_db.py
@@ -4,12 +4,12 @@ from pathlib import Path
 def test_create_to_load():
     import lndb_setup
 
-    storage = "mydata-test-db"
+    storage_root = "mydata-test-db"
     lndb_setup.login(
         "raspbear@gmx.de",
         password="MmR4YuQEyb0yxu7dAwJZTjLzR1Az2lN4Q4IduDlO",
     )
-    lndb_setup.init(storage=storage)
+    lndb_setup.init(storage=storage_root)
 
     # if importing this at the top of the file lndb_setup will try to
     # create unnecessary tables
@@ -38,7 +38,7 @@ def test_create_to_load():
     table_object = ln.schema._core.get_table_object("dobject")
     ln.schema._core.get_table_metadata_as_dict(table_object)
 
-    (Path(storage) / "mydata-test-db.lndb").unlink()
+    (Path(storage_root) / "mydata-test-db.lndb").unlink()
     # Note that this merely removes database file but doesn't clean out the instance_settings file!  # noqa
     # Hence, we need to also clean that out:
     from lndb_setup._settings_store import current_instance_settings_file
