ebcdbedcbe65b83ac8ff12ff78be6e2664c20d45
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-11-01 09:41:41 +0100

    :bento: Added anndata_mouse_sc_lymph_node

--- lamindb/datasets/__init__.py
+++ lamindb/datasets/__init__.py
@@ -7,12 +7,14 @@
    file_jpg_paradisi05
    dir_scrnaseq_cellranger
    file_mouse_sc_lymph_node
+   anndata_mouse_sc_lymph_node
    schmidt22_crispra_gws_IFNG
    schmidt22_perturbseq
    dir_scrnaseq_cellranger_schmidt22
 
 """
 from ._core import (
+    anndata_mouse_sc_lymph_node,
     dir_scrnaseq_cellranger,
     dir_scrnaseq_cellranger_schmidt22,
     file_fcs,
