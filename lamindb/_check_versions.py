82e7195539b6f501ce478638c263d59fc59d6b67
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-11-11 21:24:26 +0100

    ⬆️ Updated bionty (#348)

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -14,8 +14,8 @@ if version.parse(lndb_setup_v) != version.parse("0.14.1"):
 if version.parse(lnschema_core_v) != version.parse("0.16.1"):
     raise RuntimeError("lamindb needs lnschema_core==0.16.1")
 
-if version.parse(lnschema_bionty_v) != version.parse("0.5.3"):
-    raise RuntimeError("lamindb needs lnschema_bionty==0.5.3")
+if version.parse(lnschema_bionty_v) != version.parse("0.5.4"):
+    raise RuntimeError("lamindb needs lnschema_bionty==0.5.4")
 
 if version.parse(lnschema_wetlab_v) != version.parse("0.8.2"):
     raise RuntimeError("lamindb needs lnschema_wetlab==0.8.2")
@@ -23,8 +23,8 @@ if version.parse(lnschema_wetlab_v) != version.parse("0.8.2"):
 if version.parse(lnbfx_v) < version.parse("0.7.0"):
     raise RuntimeError("lamindb needs lnbfx>=0.7.0")
 
-if version.parse(bionty_v) != version.parse("0.5.1"):
-    raise RuntimeError("lamindb needs bionty==0.5.1")
+if version.parse(bionty_v) != version.parse("0.5.2"):
+    raise RuntimeError("lamindb needs bionty==0.5.2")
 
 if version.parse(nbproject_v) < version.parse("0.7.0"):
     raise RuntimeError("lamindb needs nbproject>=0.7.0")
