d73c418fdc8f28ab43248bab49ed20a197b74da6
Alex Wolf <f.alexander.wolf@gmail.com>
2022-11-12 21:04:05 +0100

    :arrow_up: Upgrade nbproject

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -26,8 +26,8 @@ if version.parse(lnbfx_v) < version.parse("0.7.0"):
 if version.parse(bionty_v) != version.parse("0.5.2"):
     raise RuntimeError("lamindb needs bionty==0.5.2")
 
-if version.parse(nbproject_v) < version.parse("0.7.0"):
-    raise RuntimeError("lamindb needs nbproject>=0.7.0")
+if version.parse(nbproject_v) < version.parse("0.8.0"):
+    raise RuntimeError("lamindb needs nbproject>=0.8.0")
 
 if version.parse(bioreadout_v) < version.parse("0.2.1"):
     raise RuntimeError("lamindb needs bioreadout>=0.2.1")
