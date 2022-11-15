45a107ee5d436f1a6afa106ae90f9617814a0f19
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-11-15 15:32:17 +0800

    :arrow_up: upgrade bionty

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -23,8 +23,8 @@ if version.parse(lnschema_wetlab_v) != version.parse("0.8.2"):
 if version.parse(lnbfx_v) < version.parse("0.7.0"):
     raise RuntimeError("lamindb needs lnbfx>=0.7.0")
 
-if version.parse(bionty_v) != version.parse("0.5.2"):
-    raise RuntimeError("lamindb needs bionty==0.5.2")
+if version.parse(bionty_v) != version.parse("0.5.3"):
+    raise RuntimeError("lamindb needs bionty==0.5.3")
 
 if version.parse(nbproject_v) < version.parse("0.8.0"):
     raise RuntimeError("lamindb needs nbproject>=0.8.0")
