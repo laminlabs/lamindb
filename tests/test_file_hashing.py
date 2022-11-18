8a26a75ec357df04c7fb357d93a8ff718b783873
Alex Wolf <f.alexander.wolf@gmail.com>
2022-11-18 16:41:16 +0100

    :bug: Fix test

--- tests/test_file_hashing.py
+++ tests/test_file_hashing.py
@@ -36,4 +36,5 @@ def test_base64():
     # the following can be commented out over time
     mytest = "test".encode()
     b64_str = to_b64_str(mytest)
-    assert base64.urlsafe_b64decode(b64_str.encode()).hex() == mytest.hexdigest()
+    b64_str_padded = f"{b64_str}=="
+    assert base64.urlsafe_b64decode(b64_str_padded.encode()).hex() == mytest.hexdigest()
