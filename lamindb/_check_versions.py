78f34e907d077c903563d41ed7dce7572d3ec380
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-11-24 11:05:34 +0800

    :green_heart: upgrade bionty

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -10,8 +10,8 @@ if version.parse(lndb_setup_v) != version.parse("0.18.0"):
 if version.parse(lnschema_core_v) != version.parse("0.20.0"):
     raise RuntimeError("lamindb needs lnschema_core==0.20.0")
 
-if version.parse(bionty_v) != version.parse("0.5.4"):
-    raise RuntimeError("lamindb needs bionty==0.5.4")
+if version.parse(bionty_v) != version.parse("0.5.5"):
+    raise RuntimeError("lamindb needs bionty==0.5.5")
 
 if version.parse(nbproject_v) < version.parse("0.8.0"):
     raise RuntimeError("lamindb needs nbproject>=0.8.0")
