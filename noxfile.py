2cc6d3a9b2e3308417a24cec52dc85ea4682becc
Alex Wolf <f.alexander.wolf@gmail.com>
2022-12-08 15:35:46 +0100

    :green_heart: Fixes

--- noxfile.py
+++ noxfile.py
@@ -1,36 +1,25 @@
-from pathlib import Path
-
 import nox
+from lndb_setup.test.nox import (
+    build_docs,
+    install_and_run_pytest,
+    login_testuser1,
+    run_pre_commit,
+)
 
 nox.options.reuse_existing_virtualenvs = True
-nox.options.error_on_external_run = False
-nox.options.default_venv_backend = None
 
 
 @nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
 def lint(session: nox.Session) -> None:
-    session.install("pre-commit")
-    session.run("pre-commit", "install")
-    session.run("pre-commit", "run", "--all-files")
+    run_pre_commit(session)
 
 
 @nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
 def build(session):
-    session.install(".[dev,test]")
-    login_user_1 = "lndb login testuser1@lamin.ai --password cEvcwMJFX4OwbsYVaMt2Os6GxxGgDUlBGILs2RyS"  # noqa
+    login_testuser1(session)
     login_user_2 = "lndb login testuser2@lamin.ai --password goeoNJKE61ygbz1vhaCVynGERaRrlviPBVQsjkhz"  # noqa
-    session.run(*(login_user_1.split(" ")))
     session.run(*(login_user_2.split(" ")))
     test_db = "lndb init --storage mydata-test-db"
     session.run(*test_db.split(" "))
-    session.run(
-        "pytest",
-        "-s",
-        "--cov=lamindb",
-        "--cov-append",
-        "--cov-report=term-missing",
-    )
-    session.run("coverage", "xml")
-    prefix = "." if Path("./lndocs").exists() else ".."
-    session.install(f"{prefix}/lndocs")
-    session.run("lndocs")
+    install_and_run_pytest(session)
+    build_docs(session)
