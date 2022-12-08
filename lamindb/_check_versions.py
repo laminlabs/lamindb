619e1766c5a0164f7aea314d7f3a7f196e5b5d26
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-12-08 22:27:37 +0800

    :arrow_up: Updated bionty setup

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -4,14 +4,14 @@ from lnschema_core import __version__ as lnschema_core_v
 from nbproject import __version__ as nbproject_v
 from packaging import version
 
-if version.parse(lndb_setup_v) != version.parse("0.22.0"):
-    raise RuntimeError("lamindb needs lndb_setup==0.22.0")
+if version.parse(lndb_setup_v) != version.parse("0.24.0"):
+    raise RuntimeError("lamindb needs lndb_setup==0.24.0")
 
 if version.parse(lnschema_core_v) != version.parse("0.21.2"):
     raise RuntimeError("lamindb needs lnschema_core==0.21.2")
 
-if version.parse(bionty_v) != version.parse("0.5.5"):
-    raise RuntimeError("lamindb needs bionty==0.5.5")
+if version.parse(bionty_v) != version.parse("0.5.6"):
+    raise RuntimeError("lamindb needs bionty==0.5.6")
 
 if version.parse(nbproject_v) < version.parse("0.8.0"):
     raise RuntimeError("lamindb needs nbproject>=0.8.0")
