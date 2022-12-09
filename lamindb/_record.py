16fa5efed885547e03d6f85f72414aeb2cc92a09
Alex Wolf <f.alexander.wolf@gmail.com>
2022-12-09 18:00:21 +0100

    :green_heart: fix

--- lamindb/_record.py
+++ lamindb/_record.py
@@ -212,7 +212,7 @@ def get_dobject_kwargs_from_data(
         _memory_rep=memory_rep,
     )
     if features_ref is not None:
-        features = get_features(dobject_privates, features_ref)
+        features = [get_features(dobject_privates, features_ref)]  # has to be list!
     else:
         features = []
     dobject_kwargs = dict(
