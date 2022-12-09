73b353b905f13b1a04a6bb46a68e93862aaab456
Alex Wolf <f.alexander.wolf@gmail.com>
2022-12-09 16:24:22 +0100

    :arrow_up: Upgrade

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -4,11 +4,11 @@ from lnschema_core import __version__ as lnschema_core_v
 from nbproject import __version__ as nbproject_v
 from packaging import version
 
-if version.parse(lndb_setup_v) != version.parse("0.24.3"):
-    raise RuntimeError("lamindb needs lndb_setup==0.24.3")
+if version.parse(lndb_setup_v) != version.parse("0.24.4"):
+    raise RuntimeError("lamindb needs lndb_setup==0.24.4")
 
-if version.parse(lnschema_core_v) != version.parse("0.22.2"):
-    raise RuntimeError("lamindb needs lnschema_core==0.22.2")
+if version.parse(lnschema_core_v) != version.parse("0.22.3"):
+    raise RuntimeError("lamindb needs lnschema_core==0.22.3")
 
 if version.parse(bionty_v) != version.parse("0.5.7"):
     raise RuntimeError("lamindb needs bionty==0.5.7")
