61b9b419d47679899535c1b804b5156e294416fa
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-12-09 22:31:37 +0800

    :arrow_up: updated core version

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -7,8 +7,8 @@ from packaging import version
 if version.parse(lndb_setup_v) != version.parse("0.24.3"):
     raise RuntimeError("lamindb needs lndb_setup==0.24.3")
 
-if version.parse(lnschema_core_v) != version.parse("0.22.1"):
-    raise RuntimeError("lamindb needs lnschema_core==0.22.1")
+if version.parse(lnschema_core_v) != version.parse("0.22.2"):
+    raise RuntimeError("lamindb needs lnschema_core==0.22.2")
 
 if version.parse(bionty_v) != version.parse("0.5.7"):
     raise RuntimeError("lamindb needs bionty==0.5.7")
