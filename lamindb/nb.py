e0b526ab59bdf986140cb18e420fe36b0dd5b3b8
Alex Wolf <f.alexander.wolf@gmail.com>
2022-12-14 08:41:47 +0100

    :children_crossing: Make nb.run public

--- lamindb/nb.py
+++ lamindb/nb.py
@@ -3,9 +3,11 @@
 .. autosummary::
    :toctree: .
 
+   run
    header
    publish
 
 For more background, see `nbproject <https://lamin.ai/docs/nbproject>`__.
 """
+from ._nb import _run as run  # noqa
 from ._nb import header, publish  # noqa
