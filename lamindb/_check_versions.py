a647db40c54d2722f4443041522caba8deace13a
Alex Wolf <f.alexander.wolf@gmail.com>
2022-12-15 23:11:15 +0100

    :arrow_up: Upgrade lndb-setup

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -4,8 +4,8 @@ from lnschema_core import __version__ as lnschema_core_v
 from nbproject import __version__ as nbproject_v
 from packaging import version
 
-if version.parse(lndb_setup_v) != version.parse("0.26.1"):
-    raise RuntimeError("lamindb needs lndb_setup==0.26.1")
+if version.parse(lndb_setup_v) != version.parse("0.26.2"):
+    raise RuntimeError("lamindb needs lndb_setup==0.26.2")
 
 if version.parse(lnschema_core_v) != version.parse("0.23.2"):
     raise RuntimeError("lamindb needs lnschema_core==0.23.2")
