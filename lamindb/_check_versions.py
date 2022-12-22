403b5ad055e5fd240c8f3d111af82fd84d5e75a9
Alex Wolf <f.alexander.wolf@gmail.com>
2022-12-22 20:12:15 +0100

    :bug: Fix bugs in lndb set & info

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -4,8 +4,8 @@ from lnschema_core import __version__ as lnschema_core_v
 from nbproject import __version__ as nbproject_v
 from packaging import version
 
-if version.parse(lndb_setup_v) != version.parse("0.26.3"):
-    raise RuntimeError("lamindb needs lndb_setup==0.26.3")
+if version.parse(lndb_setup_v) != version.parse("0.27.0"):
+    raise RuntimeError("lamindb needs lndb_setup==0.27.0")
 
 if version.parse(lnschema_core_v) != version.parse("0.23.2"):
     raise RuntimeError("lamindb needs lnschema_core==0.23.2")
