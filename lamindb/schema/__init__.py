6f98f564a2076f96d7759d0cb38b17a70a65b049
Alex Wolf <f.alexander.wolf@gmail.com>
2023-01-09 12:59:49 +0100

    :arrow_up: Upgrade lnbfx

--- lamindb/schema/__init__.py
+++ lamindb/schema/__init__.py
@@ -89,7 +89,7 @@ from packaging import version as _v
 _check_v = {
     "bionty": "0.6.5",
     "wetlab": "0.13.0",
-    "bfx": "0.7.0",
+    "bfx": "0.7.1",
 }
 
 for name in _settings.instance.schema:
