98cbc404534fb90fbcef4683dcdad0ae944e10b6
Frédéric ENARD <frederic.enard@gmail.com>
2023-01-23 11:56:17 +0400

    :bug: Fix check version

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -4,8 +4,8 @@ from lnschema_core import __version__ as lnschema_core_v
 from nbproject import __version__ as nbproject_v
 from packaging import version
 
-if version.parse(lndb_setup_v) != version.parse("0.30.7"):
-    raise RuntimeError("Upgrade lndb_setup! pip install lndb_setup==0.30.7")
+if version.parse(lndb_setup_v) != version.parse("0.30.8"):
+    raise RuntimeError("Upgrade lndb_setup! pip install lndb_setup==0.30.8")
 
 if version.parse(lnschema_core_v) != version.parse("0.25.0"):
     raise RuntimeError("lamindb needs lnschema_core==0.25.0")
