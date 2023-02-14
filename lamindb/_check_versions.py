3a27c85d1688e773950daddd579065b84400e04e
Bernardo Penteado <bernardopbf3@gmail.com>
2023-02-14 13:32:12 +0000

    :arrow_up: upgraded bionty to 0.7.0

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -10,8 +10,8 @@ if version.parse(lndb_v) != version.parse("0.32.4"):
 if version.parse(lnschema_core_v) != version.parse("0.26.1"):
     raise RuntimeError("lamindb needs lnschema_core==0.26.1")
 
-if version.parse(bionty_v) != version.parse("0.6.5"):
-    raise RuntimeError("lamindb needs bionty==0.6.5")
+if version.parse(bionty_v) != version.parse("0.7.0"):
+    raise RuntimeError("lamindb needs bionty==0.7.0")
 
 if version.parse(nbproject_v) < version.parse("0.8.0"):
     raise RuntimeError("lamindb needs nbproject>=0.8.0")
