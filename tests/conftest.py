cec55b9113e00f6d2006d1fea6c40cc13a1b60d1
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-17 18:30:14 +0100

    :construction_worker: Move fixtures from nox to conftest

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from subprocess import run
+
+
+def pytest_sessionstart(session):
+    instance_dirs = [
+        d for d in ["./docs/guide/mydata", "./mydata-test-db"] if Path(d).exists()
+    ]
+    for instance_dir in instance_dirs:
+        clean_instance = f"rm -r {instance_dir}"
+        session.run(*clean_instance.split(" "))
+    run("lndb init --storage mydata-test-db", shell=True)
