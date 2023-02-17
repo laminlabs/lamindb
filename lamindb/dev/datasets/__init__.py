38ca6900d8d0460ca4a217c9a55e7153825bc4df
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-02-17 19:39:15 +0800

    :bento: change to file_fastq

--- lamindb/dev/datasets/__init__.py
+++ lamindb/dev/datasets/__init__.py
@@ -5,10 +5,10 @@
 
    file_fcs
    file_jpg_paradisi05
+   file_fastq
    file_bam
    file_mini_csv
    dir_scrnaseq_cellranger
-   dir_scrnaseq_cellranger_out
    anndata_mouse_sc_lymph_node
    anndata_human_immune_cells
    anndata_pbmc68k_reduced
@@ -21,8 +21,8 @@ from ._core import (
     anndata_pbmc3k_processed,
     anndata_pbmc68k_reduced,
     dir_scrnaseq_cellranger,
-    dir_scrnaseq_cellranger_out,
     file_bam,
+    file_fastq,
     file_fcs,
     file_jpg_paradisi05,
     file_mini_csv,
--- lamindb/dev/datasets/_core.py
+++ lamindb/dev/datasets/_core.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+from typing import Union
 from urllib.request import urlretrieve
 
 import anndata as ad
 import pandas as pd
+from lnschema_core.dev import id
 
 
 def file_fcs() -> Path:
@@ -23,6 +25,13 @@ def file_jpg_paradisi05() -> Path:
         "paradisi05_laminopathic_nuclei.jpg",
     )
     return Path(filepath)
+
+
+def file_fastq() -> Path:
+    """Mini mock fastq file."""
+    with open("./input.fastq.gz", "w") as f:
+        f.write("Mock fastq file.")
+    return Path("./input.fastq.gz")
 
 
 def file_bam() -> Path:
@@ -52,20 +61,6 @@ def dir_scrnaseq_cellranger() -> Path:
         zipObj.extractall(path=".")
 
     return Path("cellranger_run_001")
-
-
-def dir_scrnaseq_cellranger_out() -> Path:
-    """Directory with exemplary scrnaseq cellranger output."""
-    filepath, _ = urlretrieve(
-        "https://lamindb-test.s3.amazonaws.com/scrnaseq-cellranger.zip"
-    )
-    from zipfile import ZipFile
-
-    with ZipFile(filepath, "r") as zipObj:
-        # Extract all the contents of zip file in current directory
-        zipObj.extractall(path=".")
-
-    return Path("scrnaseq-cellranger")
 
 
 def anndata_mouse_sc_lymph_node() -> ad.AnnData:
@@ -127,6 +122,54 @@ def anndata_human_immune_cells() -> ad.AnnData:
     """
     filepath, _ = urlretrieve("https://lamindb-test.s3.amazonaws.com/human_immune.h5ad")
     return ad.read(filepath)
+
+
+def generate_cell_ranger_files(sample_name: str, basedir: Union[str, Path] = "./"):
+    """Generate mock cell ranger outputs.
+
+    Args:
+        sample_name: name of the sample
+        basedir: run directory
+
+    """
+    basedir = Path(basedir)
+
+    fastqdir = basedir / "fastq"
+    fastqdir.mkdir(parents=True, exist_ok=True)
+    fastqfile1 = fastqdir / f"{sample_name}_R1_001.fastq.gz"
+    fastqfile1.touch(exist_ok=True)
+    fastqfile2 = fastqdir / f"{sample_name}_R2_001.fastq.gz"
+    fastqfile2.touch(exist_ok=True)
+
+    sampledir = basedir / f"{sample_name}"
+    for folder in ["raw_feature_bc_matrix", "filtered_feature_bc_matrix", "analysis"]:
+        filedir = sampledir / folder
+        filedir.mkdir(parents=True, exist_ok=True)
+
+    for filename in [
+        "web_summary.html",
+        "metrics_summary.csv",
+        "possorted_genome_bam.bam",
+        "possorted_genome_bam.bam.bai",
+        "molecule_info.h5",
+        "cloupe.cloupe",
+        "raw_feature_bc_matrix.h5",
+        "raw_feature_bc_matrix/barcodes.tsv.gz",
+        "raw_feature_bc_matrix/features.tsv.gz",
+        "raw_feature_bc_matrix/matrix.mtx.gz",
+        "filtered_feature_bc_matrix.h5",
+        "filtered_feature_bc_matrix/barcodes.tsv.gz",
+        "filtered_feature_bc_matrix/features.tsv.gz",
+        "filtered_feature_bc_matrix/matrix.mtx.gz",
+        "analysis/analysis.csv",
+    ]:
+        filedir = sampledir / filename
+        filedir.touch(exist_ok=True)
+
+    for filepath in basedir.rglob("*"):
+        if not filepath.is_dir():
+            with open(filepath, "w") as file:
+                file.write(f"{id.base62(n_char=6)}")
 
 
 # def schmidt22_crispra_gws_IFNG() -> Path:
