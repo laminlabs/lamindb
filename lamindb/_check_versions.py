b20bf2908a032c6f36dbc514b45647a9a51977dc
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-21 20:33:00 +0100

    👷 Better CI and better arg validation (#505)

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -4,8 +4,8 @@ from lnschema_core import __version__ as lnschema_core_v
 from nbproject import __version__ as nbproject_v
 from packaging import version
 
-if version.parse(lndb_v) != version.parse("0.35.rc1"):
-    raise RuntimeError("Upgrade lndb! pip install lndb==0.35.rc1")
+if version.parse(lndb_v) != version.parse("0.35.0"):
+    raise RuntimeError("Upgrade lndb! pip install lndb==0.35.0")
 
 if version.parse(lnschema_core_v) != version.parse("0.27.2"):
     raise RuntimeError("lamindb needs lnschema_core==0.27.2")
