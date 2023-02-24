8d19bf8975199fc6814c6776890c0291102836e1
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-25 00:13:50 +0100

    :truck: Replace lndb with lamin in tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -2,16 +2,16 @@ from pathlib import Path
 
 
 def test_create_to_load():
-    import lndb
+    import lamin
 
     storage_root = "mydata-test-db"
-    lndb.login(
+    lamin.login(
         "raspbear@gmx.de",
         password="MmR4YuQEyb0yxu7dAwJZTjLzR1Az2lN4Q4IduDlO",
     )
-    lndb.init(storage=storage_root)
+    lamin.init(storage=storage_root)
 
-    # if importing this at the top of the file lndb will try to
+    # if importing this at the top of the file lamin will try to
     # create unnecessary tables
     import lamindb as ln
 
@@ -19,16 +19,16 @@ def test_create_to_load():
     ln.add(notebook)
     run = ln.schema.Run(notebook_id=notebook.id, notebook_v=notebook.v)
     ln.add(run)
-    ln.select(ln.schema.Storage, root=str(lndb.settings.instance.storage.root)).one()
+    ln.select(ln.schema.Storage, root=str(lamin.settings.instance.storage.root)).one()
 
     ln.schema._core.get_db_metadata_as_dict()
     table_object = ln.schema._core.get_table_object("core.dobject")
     ln.schema._core.get_table_metadata_as_dict(table_object)
 
-    (Path(storage_root) / "mydata-test-db.lndb").unlink()
+    (Path(storage_root) / "mydata-test-db.lamin").unlink()
     # Note that this merely removes database file but doesn't clean out the instance_settings file!  # noqa
     # Hence, we need to also clean that out:
-    from lndb.dev._settings_store import current_instance_settings_file
+    from lamin.dev._settings_store import current_instance_settings_file
 
     current_instance_settings_file().unlink()
 
