c587cae9669e345feaba19512323e36540ea15d5
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-25 01:08:24 +0100

    :green_heart: More fixes

---
+++ lamindb/schema/dev/sqlmodel.py
@@ -0,0 +1,4 @@
+from lnschema_core.dev import sqlmodel as _sqlmodel
+from lnschema_core.dev.sqlmodel import *  # noqa
+
+__doc__ = _sqlmodel.__doc__
---
+++ lamindb/schema/dev/type.py
@@ -0,0 +1,4 @@
+from lnschema_core.dev import type as _type
+from lnschema_core.dev.type import *  # noqa
+
+__doc__ = _type.__doc__
