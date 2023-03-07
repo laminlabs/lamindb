ae7f51c05a3cd98b07d98eded36524dffd55c41b
Alex Wolf <f.alexander.wolf@gmail.com>
2023-03-07 23:01:59 +0100

    :memo: Do not logg knowledge warning for now

--- lamindb/knowledge/__init__.py
+++ lamindb/knowledge/__init__.py
@@ -27,7 +27,8 @@ from lamin_logger import logger
 from ._core import CellMarker, CellType, Disease, Gene, Protein, Species, Tissue
 from ._lookup import lookup
 
-logger.warning(
-    "The lamindb.knowledge API is deprecated.\nPlease, use bionty directly.\nYou can"
-    " replace all occurances of ln.knowledge with bionty without breaking changes!"
-)
+# currently shows up also when initializing ln.DObject, so not emit it yet
+# logger.warning(
+#     "The lamindb.knowledge API is deprecated.\nPlease, use bionty directly.\nYou can"
+#     " replace all occurances of ln.knowledge with bionty without breaking changes!"
+# )
