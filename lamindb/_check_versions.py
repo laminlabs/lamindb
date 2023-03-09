02b0ebd5cae4b63d7e6907ff62881c3cf1e50e4f
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-03-09 13:05:43 +0800

    :heavy_minus_sign: Remove bionty as a dependency

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -1,4 +1,3 @@
-from bionty import __version__ as bionty_v
 from lamin_logger import logger
 from lndb import __version__ as lndb_v
 from lnschema_core import __version__ as lnschema_core_v
@@ -10,9 +9,6 @@ if version.parse(lndb_v) != version.parse("0.37.1"):
 
 if version.parse(lnschema_core_v) != version.parse("0.29.1"):
     raise RuntimeError("lamindb needs lnschema_core==0.29.1")
-
-if version.parse(bionty_v) != version.parse("0.7.0"):
-    raise RuntimeError("lamindb needs bionty==0.7.0")
 
 if version.parse(nbproject_v) < version.parse("0.8.2"):
     raise RuntimeError("lamindb needs nbproject>=0.8.2")
