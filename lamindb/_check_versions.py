ed17e7766853332b7434f4fefd2d20609cb6c472
Alex Wolf <f.alexander.wolf@gmail.com>
2023-03-29 23:14:47 +0200

    :arrow_up: Bump to lnschema-core 0.30.0

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -7,8 +7,8 @@ from packaging import version
 
 # Lamin PINNED packages
 
-if version.parse(lnschema_core_v) != version.parse("0.30rc5"):
-    raise RuntimeError("lamindb needs lnschema_core==0.30rc5")
+if version.parse(lnschema_core_v) != version.parse("0.30.0"):
+    raise RuntimeError("lamindb needs lnschema_core==0.30.0")
 
 if version.parse(lndb_storage_v) != version.parse("0.2rc2"):
     raise RuntimeError("lamindb needs lndb_storage==0.2rc2")
