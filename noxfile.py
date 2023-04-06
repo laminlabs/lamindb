bd2f9285116b815c1160c8a6170b1333c2a95d2b
Alex Wolf <f.alexander.wolf@gmail.com>
2023-04-06 17:39:34 +0200

    :green_heart: Try to clean up ci instance

--- noxfile.py
+++ noxfile.py
@@ -45,9 +45,16 @@ def build(session, package):
     print(f"Done installing: {t_total:.3f}s")
 
     if package == "lamindb":
-        session.run("lamin", "load", "lamindb-ci")
-        session.run("lamin", "delete", "lamindb-ci")
-        run_pytest(session)
+        try:
+            # try to clean up lamindb-ci
+            session.run(
+                "lamin",
+                "load",
+                "lamindb-ci",
+            )
+            session.run("lamin", "delete", "lamindb-ci")
+        finally:
+            run_pytest(session)
     else:
         # navigate into submodule so that lamin-project.yml is correctly read
         os.chdir(f"./sub/{package}")
