a907941c07e9056feca8325aeda4605d12008c8c
Alex Wolf <f.alexander.wolf@gmail.com>
2023-04-10 00:18:38 +0200

    :arrow_up: Bump lndb-storage to 0.2rc3

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -10,8 +10,8 @@ from packaging import version
 if version.parse(lnschema_core_v) != version.parse("0.32.0"):
     raise RuntimeError("lamindb needs lnschema_core==0.32.0")
 
-if version.parse(lndb_storage_v) != version.parse("0.2rc2"):
-    raise RuntimeError("lamindb needs lndb_storage==0.2rc2")
+if version.parse(lndb_storage_v) != version.parse("0.2rc3"):
+    raise RuntimeError("lamindb needs lndb_storage==0.2rc3")
 
 if version.parse(lndb_v) < version.parse("0.41rc1"):
     raise RuntimeError("Upgrade lndb! pip install lndb==0.41rc1")
