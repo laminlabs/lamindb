52950328b1e50ed71befb19a15b07343c6c4dba1
Alex Wolf <f.alexander.wolf@gmail.com>
2023-04-10 12:36:40 +0200

    :green_heart: Bring back full CI

--- noxfile.py
+++ noxfile.py
@@ -1,20 +1,19 @@
 import os
-
-# import shutil
-# from pathlib import Path
+import shutil
+from pathlib import Path
 from time import perf_counter
 
 import nox
-
-# from laminci import move_built_docs_to_docs_slash_project_slug, upload_docs_artifact
-from laminci.nox import (  # build_docs,
+from laminci import move_built_docs_to_docs_slash_project_slug, upload_docs_artifact
+from laminci.nox import (
+    build_docs,
     login_testuser1,
     login_testuser2,
     run_pre_commit,
     run_pytest,
 )
 
-# import lamindb as ln
+import lamindb as ln
 
 nox.options.reuse_existing_virtualenvs = True
 
@@ -46,42 +45,42 @@ def build(session, package):
     print(f"Done installing: {t_total:.3f}s")
 
     if package == "lamindb":
-        run_pytest(session, coverage=False)
+        run_pytest(session)
     else:
         # navigate into submodule so that lamin-project.yml is correctly read
         os.chdir(f"./sub/{package}")
         session.run("pytest", "-s", "./tests", "--ignore", "./tests/test_migrations.py")
 
     if package == "lamindb":
-        # t_start = perf_counter()
+        t_start = perf_counter()
 
-        # # Schemas
-        # ln.setup.load("testuser1/lamin-site-assets", migrate=True)
+        # Schemas
+        ln.setup.load("testuser1/lamin-site-assets", migrate=True)
 
-        # file = ln.select(ln.File, name="lnschema_core_docs").one()
-        # shutil.unpack_archive(file.load(), "lnschema_core_docs")
-        # Path("lnschema_core_docs/guide/0-core-schema.ipynb").rename(
-        #     "docs/guide/lnschema-core.ipynb"
-        # )
-        # Path("lnschema_core_docs/guide/1-data-validation.ipynb").rename(
-        #     "docs/guide/data-validation.ipynb"
-        # )
+        file = ln.select(ln.File, name="lnschema_core_docs").one()
+        shutil.unpack_archive(file.load(), "lnschema_core_docs")
+        Path("lnschema_core_docs/guide/0-core-schema.ipynb").rename(
+            "docs/guide/lnschema-core.ipynb"
+        )
+        Path("lnschema_core_docs/guide/1-data-validation.ipynb").rename(
+            "docs/guide/data-validation.ipynb"
+        )
 
-        # file = ln.select(ln.File, name="lnschema_bionty_docs").one()
-        # shutil.unpack_archive(file.load(), "lnschema_bionty_docs")
-        # Path("lnschema_bionty_docs/guide/orms.ipynb").rename(
-        #     "docs/guide/lnschema-bionty.ipynb"
-        # )
-        # Path("lnschema_bionty_docs/guide/knowledge.ipynb").rename(
-        #     "docs/guide/knowledge.ipynb"
-        # )
+        file = ln.select(ln.File, name="lnschema_bionty_docs").one()
+        shutil.unpack_archive(file.load(), "lnschema_bionty_docs")
+        Path("lnschema_bionty_docs/guide/orms.ipynb").rename(
+            "docs/guide/lnschema-bionty.ipynb"
+        )
+        Path("lnschema_bionty_docs/guide/knowledge.ipynb").rename(
+            "docs/guide/knowledge.ipynb"
+        )
 
-        # t_total = perf_counter() - t_start
-        # print(f"Done pulling artifacts: {t_total:.3f}s")
+        t_total = perf_counter() - t_start
+        print(f"Done pulling artifacts: {t_total:.3f}s")
 
         t_start = perf_counter()
-        # build_docs(session)
-        # login_testuser1(session)
-        # upload_docs_artifact()
-        # move_built_docs_to_docs_slash_project_slug()
+        build_docs(session)
+        login_testuser1(session)
+        upload_docs_artifact()
+        move_built_docs_to_docs_slash_project_slug()
         print(f"Done building docs and uploading: {t_total:.3f}s")
