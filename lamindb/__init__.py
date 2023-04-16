fcc1e83b58868ad8518a3af66c32f47881ae4083
Alex Wolf <f.alexander.wolf@gmail.com>
2023-04-16 23:19:42 +0200

    :bookmark: Release version 0.37.0

--- lamindb/__init__.py
+++ lamindb/__init__.py
@@ -85,7 +85,7 @@ Developer API:
    dev
 """
 
-__version__ = "0.36.4"  # denote a release candidate for 0.1.0 with 0.1rc1
+__version__ = "0.37.0"  # denote a release candidate for 0.1.0 with 0.1rc1
 
 # prints warning of python versions
 from lamin_logger import py_version_warning as _py_version_warning
