a88c4a3498cccd258252225f1669145ca4e7e2a6
Alex Wolf <f.alexander.wolf@gmail.com>
2023-04-22 21:47:06 +0200

    ♻️ Move docstrings to entities & move `Folder.subset` here (#652)

--- lamindb/_folder.py
+++ lamindb/_folder.py
@@ -15,6 +15,16 @@ from ._file import (
 )
 from .dev.db._select import select
 
+Folder.__doc__ = """Folders: collections of files.
+
+Real vs. virtual folders:
+
+- A real LaminDB `Folder` has a 1:1 correspondence to a folder on a file system
+  or in object storage, and a `.key` that is not `None`.
+- A virtual LaminDB `Folder` is a mere way of grouping files. A file can be
+  linked to multiple virtual folders, but only to one real folder.
+"""
+
 
 def get_folder_kwargs_from_data(
     path: Union[Path, UPath, str],
@@ -116,17 +126,18 @@ def tree(
 
 
 # exposed to users as Folder.subset()
-def subset(folder: Folder, *, prefix: str, **fields) -> List[File]:
+def subset(self: Folder, *, prefix: str, **fields) -> List[File]:
     """Get files via relative path to folder."""
     # ensure is actual folder, not virtual one
-    if folder.key is None:
+    if self.key is None:
         raise ValueError(
             ".get() is only defined for real folders, not virtual ones"
             "you can access files via .files or by refining with queries"
         )
     files = (
-        select(File, **fields)
-        .where(File.key.startswith(folder.key + "/" + prefix))
-        .all()
+        select(File, **fields).where(File.key.startswith(self.key + "/" + prefix)).all()
     )
     return files
+
+
+Folder.subset = subset
