1e791abf261c3e343fe6a969b924b44862783dc5
Alex Wolf <f.alexander.wolf@gmail.com>
2023-05-17 12:04:13 +0200

    :memo: Fix type docs

--- lamindb/__init__.py
+++ lamindb/__init__.py
@@ -112,12 +112,12 @@ if _INSTANCE_SETUP:
         Storage,
         Transform,
         User,
-        types,
     )
 
     from . import _check_versions  # executes checks during import
     from . import dev  # noqa
     from . import schema  # noqa
+    from . import link, types  # noqa
     from ._context import context  # noqa
 
     track = context._track  # noqa
