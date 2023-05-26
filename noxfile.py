fbd0c367fcabae886425e35d6f45b0275e07a997
Alex Wolf <f.alexander.wolf@gmail.com>
2023-05-26 15:03:26 +0200

    :green_heart: Fix

--- noxfile.py
+++ noxfile.py
@@ -36,7 +36,7 @@ def build(session, package):
     # run with pypi install on main
     if "GITHUB_EVENT_NAME" in os.environ and os.environ["GITHUB_EVENT_NAME"] != "push":
         # run with submodule install on a PR
-        session.install("./sub/lndb")
+        session.install("./sub/lndb-setup")
         session.install("./sub/lnschema-core")
         session.install("./sub/lnbase-biolab")
         session.install("./sub/lndb-storage[dev,test]")
