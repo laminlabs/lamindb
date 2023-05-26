a9d2b74a296ec1958892b908271cbf61d1513061
Alex Wolf <f.alexander.wolf@gmail.com>
2023-05-26 16:08:02 +0200

    :recycle: Simplify noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,19 +1,13 @@
-import os
+import os  # noqa
 import shutil
 from pathlib import Path
 from time import perf_counter
+from urllib.request import urlretrieve
 
 import nox
 from laminci import move_built_docs_to_docs_slash_project_slug, upload_docs_artifact
-from laminci.nox import (
-    build_docs,
-    login_testuser1,
-    login_testuser2,
-    run_pre_commit,
-    run_pytest,
-)
-
-import lamindb as ln
+from laminci.nox import login_testuser2  # noqa
+from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest  # noqa
 
 nox.options.reuse_existing_virtualenvs = True
 
@@ -26,46 +20,49 @@ def lint(session: nox.Session) -> None:
 @nox.session
 @nox.parametrize("package", ["lamindb", "lndb-storage"])
 def build(session, package):
-    t_start = perf_counter()
-    login_testuser2(session)
-    login_testuser1(session)
-    t_end = perf_counter()
-    print(f"Done logging in: {t_end - t_start:.3f}s")
-    t_start = t_end
+    # t_start = perf_counter()
+    # # run with pypi install on main
+    # if "GITHUB_EVENT_NAME" in os.environ and os.environ["GITHUB_EVENT_NAME"] != "push":  # noqa
+    #     # run with submodule install on a PR
+    #     session.install("./sub/lndb-setup")
+    #     session.install("./sub/lnschema-core")
+    #     session.install("./sub/lnbase-biolab")
+    #     session.install("./sub/lndb-storage[dev,test]")
+    # session.install(".[dev,test]")
+    # t_end = perf_counter()
+    # print(f"Done installing: {t_end - t_start:.3f}s")
+    # t_start = t_end
 
-    # run with pypi install on main
-    if "GITHUB_EVENT_NAME" in os.environ and os.environ["GITHUB_EVENT_NAME"] != "push":
-        # run with submodule install on a PR
-        session.install("./sub/lndb-setup")
-        session.install("./sub/lnschema-core")
-        session.install("./sub/lnbase-biolab")
-        session.install("./sub/lndb-storage[dev,test]")
-    session.install(".[dev,test]")
+    # login_testuser2(session)
+    # login_testuser1(session)
+    # t_end = perf_counter()
+    # print(f"Done logging in: {t_end - t_start:.3f}s")
+    # t_start = t_end
+
+    # if package == "lamindb":
+    #     run_pytest(session)
+    # else:
+    #     # navigate into submodule so that lamin-project.yml is correctly read
+    #     os.chdir(f"./sub/{package}")
+    #     session.run("pytest", "-s", "./tests", "--ignore", "./tests/test_migrations.py")  # noqa
+
     t_end = perf_counter()
-    print(f"Done installing: {t_end - t_start:.3f}s")
+    # print(f"Done running tests: {t_end - t_start:.3f}s")
     t_start = t_end
 
     if package == "lamindb":
-        run_pytest(session)
-    else:
-        # navigate into submodule so that lamin-project.yml is correctly read
-        os.chdir(f"./sub/{package}")
-        session.run("pytest", "-s", "./tests", "--ignore", "./tests/test_migrations.py")
-
-    t_end = perf_counter()
-    print(f"Done running tests: {t_end - t_start:.3f}s")
-    t_start = t_end
-
-    if package == "lamindb":
-        # Schemas
-        ln.setup.load("testuser1/lamin-site-assets", migrate=True)
-
-        file = ln.select(ln.File, key="docs/lndb_storage_docs.zip").one()
-        shutil.unpack_archive(file.stage(), "lndb_storage_docs")
+        filename = "lndb_storage_docs.zip"
+        urlretrieve(
+            f"https://lamin-site-assets.s3.amazonaws.com/docs/{filename}", filename
+        )
+        shutil.unpack_archive(filename, "lndb_storage_docs")
         Path("lndb_storage_docs/guide/stream.ipynb").rename("docs/guide/stream.ipynb")
 
-        file = ln.select(ln.File, key="docs/lnschema_core_docs.zip").one()
-        shutil.unpack_archive(file.stage(), "lnschema_core_docs")
+        filename = "lnschema_core_docs.zip"
+        urlretrieve(
+            f"https://lamin-site-assets.s3.amazonaws.com/docs/{filename}", filename
+        )
+        shutil.unpack_archive(filename, "lnschema_core_docs")
         Path("lnschema_core_docs/guide/0-core-schema.ipynb").rename(
             "docs/guide/lnschema-core.ipynb"
         )
@@ -73,17 +70,18 @@ def build(session, package):
             "docs/guide/data-validation.ipynb"
         )
 
-        file = ln.select(ln.File, key="docs/lnschema_bionty_docs.zip").one()
-        shutil.unpack_archive(file.stage(), "lnschema_bionty_docs")
+        filename = "lnschema_bionty_docs.zip"
+        urlretrieve(
+            f"https://lamin-site-assets.s3.amazonaws.com/docs/{filename}", filename
+        )
+        shutil.unpack_archive(filename, "lnschema_bionty_docs")
         Path("lnschema_bionty_docs/guide/bionty-orms.ipynb").rename(
             "docs/guide/lnschema-bionty.ipynb"
         )
-
         t_end = perf_counter()
         print(f"Done pulling artifacts: {t_end - t_start:.3f}s")
         t_start = t_end
 
-        ln.setup.init(storage="./mydocs")  # instance for building docs
         build_docs(session)
         login_testuser1(session)
         upload_docs_artifact()
