0094d90273057b05c0d6e462831e8c21620fe484
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-05-28 07:22:16 -0400

    ✨ Add `DataFrame` support for `ln.parse` (#688)

--- lamindb/_parse.py
+++ lamindb/_parse.py
@@ -1,4 +1,4 @@
-from typing import Callable, List, TypeVar
+from typing import Dict, List, TypeVar, Union
 
 import numpy as np
 import pandas as pd
@@ -9,51 +9,92 @@ from sqlmodel import SQLModel
 from .dev.db._select import select
 
 ListLike = TypeVar("ListLike", pd.Series, list, np.array)
-SQLModelField = TypeVar("SQLModelField", Callable, InstrumentedAttribute)
 
 
 def parse(
-    iterable: ListLike, field: SQLModelField, from_bionty: bool = True
+    iterable: Union[ListLike, pd.DataFrame],
+    field: Union[InstrumentedAttribute, Dict[str, InstrumentedAttribute]],
+    from_bionty: bool = True,
 ) -> List[SQLModel]:
     """Parse a dataset column based on a SQLModel entity field.
 
     Guide: :doc:`/biology/ontologies`.
 
     Args:
-        iterable: a `ListLike` of values.
-        field: a `SQLModel` field that correspond to the input list.
-        from_bionty: whether to use bionty for auto-completion of the fields.
+        iterable: a `ListLike` of values or a `DataFrame`.
+        field: if iterable is `ListLike`: a `SQLModel` field to parse into.
+               if iterable is `DataFrame`: a dict of {column_name : SQLModel_field}.
+        from_bionty: whether to auto-complete the fields of bionty tables.
+                     only effective if iterable is `ListLike`.
 
     Returns:
         A list of SQLModel records.
     """
+    if isinstance(iterable, pd.DataFrame):
+        if not isinstance(field, dict):
+            raise TypeError(
+                "field must be a dictionary of {column_name : SQLModel_field}!"
+            )
+        column_mapper = {colname: f.name for colname, f in field.items()}
+        class_mapper = {f.name: f.class_ for f in field.values()}
+        if len(set(class_mapper.values())) > 1:
+            raise NotImplementedError("fields must from the same entity!")
+        entity = list(class_mapper.values())[0]
+        df = iterable.rename(columns=column_mapper)
+        df = df.dropna().drop_duplicates()
+        df = df.mask(df == "", None)
+        iterable = df.to_dict(orient="records")
+    else:
+        if not isinstance(field, InstrumentedAttribute):
+            raise TypeError("field must be a SQLModel field!")
+        entity = field.class_
+        iterable = set(iterable)
+
     records = []
-    entity = field.class_  # type:ignore
-    for i in set(iterable):
+    for i in iterable:
         # No entries are made for NAs, '', None
         if pd.isnull(i) or i == "":
             continue
-        kwargs = {field.name: i}  # type:ignore
+        if isinstance(i, dict):
+            kwargs = i
+        else:
+            kwargs = {field.name: i}  # type:ignore
         record = _create_record(entity, **kwargs, from_bionty=from_bionty)
-        records.append(record)
+        records += record
     return records
 
 
 def _create_record(entity: SQLModel, from_bionty: bool = True, **kwargs):
     """Create a record from bionty with configured ontologies."""
-    record = select(entity, **kwargs).one_or_none()
-    if record is None:
+    kwargs = {k: v for k, v in kwargs.items() if k in entity.__fields__}
+
+    if len(kwargs) == 0:
+        return None
+    elif len(kwargs) > 1:
+        # from_bionty is set to false if multiple kwargs are passed
+        from_bionty = False
+
+    # query for existing records in the DB
+    record = select(entity, **kwargs).all()
+    if len(record) > 1:
+        logger.warning(
+            f"{len(record)} records are found in the database with {kwargs}, returning"
+            " all!"
+        )
+    elif len(record) == 0:
         if from_bionty:
             try:
-                record = entity.from_bionty(**kwargs)
-            except AttributeError:
-                record = entity(**kwargs)
+                # bionty tables
+                record = [entity.from_bionty(**kwargs)]
             except (ValueError, KeyError):
                 logger.warning(
                     f"No entry found in bionty with {kwargs}!"
                     " Couldn't populate additional fields..."
                 )
-                record = entity(**kwargs)
+                record = [entity(**kwargs)]
+            except AttributeError:
+                # no-bionty tables
+                record = [entity(**kwargs)]
         else:
-            record = entity(**kwargs)
+            record = [entity(**kwargs)]
     return record
---
+++ tests/test_parse.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from lnschema_bionty import CellType
+
+from lamindb._parse import parse
+
+df = pd.DataFrame(
+    (["T cell", "CL:0000084"], ["hepatocyte", "CL:0000182"], ["my new cell type", ""]),
+    columns=["cell_type", "cell_type_id"],
+)
+
+
+def test_parse_name():
+    result = parse(df.cell_type, CellType.name)
+    ids = [i.ontology_id for i in result]
+    assert len(result) == 3
+    assert set(ids) == set(["CL:0000182", None, "CL:0000084"])
+
+
+def test_parse_ontology_id():
+    result = parse(df.cell_type_id, CellType.ontology_id)
+    names = [i.name for i in result]
+    assert len(result) == 2
+    assert set(names) == set(["T cell", "hepatocyte"])
+
+
+def test_parse_df():
+    result = parse(
+        df, {"cell_type": CellType.name, "cell_type_id": CellType.ontology_id}
+    )
+    names = [i.name for i in result]
+    ids = [i.ontology_id for i in result]
+    extras = [i.definition for i in result]
+    assert len(result) == 3
+    assert set(names) == set(["T cell", "hepatocyte", "my new cell type"])
+    # converts empty string to None
+    assert set(ids) == set(["CL:0000084", "CL:0000182", None])
+    # additional fields are not populated
+    assert set(extras) == set([None, None, None])
