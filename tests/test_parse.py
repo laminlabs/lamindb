2d496fb56ac6c2ad6002171932ffba046ed4a8d0
Alex Wolf <f.alexander.wolf@gmail.com>
2023-05-28 15:17:55 +0200

    :construction_worker: Add fixtures to test_parse

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,30 +1,45 @@
 import pandas as pd
+import pytest
 from lnschema_bionty import CellType
 
-from lamindb._parse import parse
-
-df = pd.DataFrame(
-    (["T cell", "CL:0000084"], ["hepatocyte", "CL:0000182"], ["my new cell type", ""]),
-    columns=["cell_type", "cell_type_id"],
-)
+import lamindb as ln
 
 
-def test_parse_name():
-    result = parse(df.cell_type, CellType.name)
+@pytest.fixture(scope="module")
+def instance():
+    ln.setup.init(storage="test_parse", schema="bionty")
+    yield
+    ln.setup.delete("test_parse")
+
+
+@pytest.fixture(scope="module")
+def df():
+    return pd.DataFrame(
+        (
+            ["T cell", "CL:0000084"],
+            ["hepatocyte", "CL:0000182"],
+            ["my new cell type", ""],
+        ),
+        columns=["cell_type", "cell_type_id"],
+    )
+
+
+def test_parse_name(instance, df):
+    result = ln.parse(df.cell_type, CellType.name)
     ids = [i.ontology_id for i in result]
     assert len(result) == 3
     assert set(ids) == set(["CL:0000182", None, "CL:0000084"])
 
 
-def test_parse_ontology_id():
-    result = parse(df.cell_type_id, CellType.ontology_id)
+def test_parse_ontology_id(instance, df):
+    result = ln.parse(df.cell_type_id, CellType.ontology_id)
     names = [i.name for i in result]
     assert len(result) == 2
     assert set(names) == set(["T cell", "hepatocyte"])
 
 
-def test_parse_df():
-    result = parse(
+def test_parse_df(instance, df):
+    result = ln.parse(
         df, {"cell_type": CellType.name, "cell_type_id": CellType.ontology_id}
     )
     names = [i.name for i in result]
