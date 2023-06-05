578c6cac1a46267d6e6b802dea0fcf07385ac179
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-06-05 13:29:29 +0200

    :bookmark: Release version 0.42a2

--- lamindb/__init__.py
+++ lamindb/__init__.py
@@ -81,7 +81,7 @@ Developer API:
    dev
 """
 
-__version__ = "0.42a1"  # denote a release candidate for 0.1.0 with 0.1rc1
+__version__ = "0.42a2"  # denote a release candidate for 0.1.0 with 0.1rc1
 
 import lamindb_setup as _lamindb_setup
 
