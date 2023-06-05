37f7f0a3d409521ef1c686b2a136f8b919d724f4
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-05 15:02:59 +0200

    :arrow_up: Update lamindb-setup & lnschema-bionty

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -1,5 +1,6 @@
 import os
 
+from lamin_logger import logger
 from lamindb_setup import __version__ as lamindb_setup_v
 from lnschema_core import __version__ as lnschema_core_v
 from packaging import version
@@ -8,16 +9,16 @@ if os.getenv("GITHUB_ACTIONS") is None:
     # Lamin PINNED packages
 
     if version.parse(lnschema_core_v) != version.parse("0.35a5"):
-        raise RuntimeError("lamindb needs lnschema_core==0.35a5")
+        logger.warning("lamindb needs lnschema_core==0.35a5")
 
-    if version.parse(lamindb_setup_v) < version.parse("0.46a3"):
-        raise RuntimeError("Upgrade lamindb_setup! pip install lamindb_setup==0.46a3")
+    if version.parse(lamindb_setup_v) < version.parse("0.46a4"):
+        logger.warning("Upgrade lamindb_setup! pip install lamindb_setup==0.46a4")
 
     # Lamin GREATEREQ packages
     try:
         from nbproject import __version__ as nbproject_v
 
         if version.parse(nbproject_v) < version.parse("0.8.7"):
-            raise RuntimeError("lamindb needs nbproject>=0.8.7")
+            logger.warning("lamindb needs nbproject>=0.8.7")
     except ImportError:
         pass
