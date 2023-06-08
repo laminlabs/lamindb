a3cd9473fc7f77d3b7e5063c344484bbda2549a4
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-06-08 15:59:05 +0200

    :recycle: Refactored features code

--- lamindb/_features.py
+++ lamindb/_features.py
@@ -1,71 +1,50 @@
-from typing import Any
+from typing import Optional
 
-import pandas as pd
+from lamin_logger import logger
 from lnschema_core import Featureset
 
 from lamindb._select import select
 from lamindb.dev.hashing import hash_set
 
-from ._parse import Field, ListLike, get_or_create_records
-
-
-def parse_features(
-    df: pd.DataFrame, bionty_object: Any, field: Field, **map_kwargs
-) -> None:
-    """Link features to a knowledge table.
-
-    Args:
-        df: a DataFrame
-        bionty_object: Features reference class, bionty.{entity}()
-    """
-    map_kwargs["reference_id"] = field.field.name
-    df_curated = bionty_object.curate(df=df, **map_kwargs)
-    # ._parsing_id only exist and set to be the reference_id after `.curate`` is called
-    parsing_id = bionty_object._parsing_id
-
-    # logging of curation
-    n = df_curated["__curated__"].count()
-    n_mapped = df_curated["__curated__"].sum()
-    log = {  # noqa  TODO: store this somewhere in the db
-        "feature": parsing_id,
-        "n_mapped": n_mapped,
-        "percent_mapped": round(n_mapped / n * 100, 1),
-        "unmapped": df_curated.index[~df_curated["__curated__"]],
-    }
-
-    features_hash = hash_set(set(df_curated.index))
-
-    features_type = bionty_object._entity
-
-    features = select(
-        Featureset,
-        id=features_hash,
-        type=features_type,
-    ).one_or_none()
-    if features is not None:
-        return features  # features already exists!
-    else:
-        records = get_or_create_records(
-            iterable=df_curated.index,
-            field=field,
-            species=bionty_object.species,
-        )
-
-        key = f"{bionty_object._entity}s"
-        featureset = Featureset(id=features_hash, type=features_type, **{key: records})
-
-    return featureset
+from ._parse import Field, ListLike, get_or_create_records, not_null_iterable
 
 
 # expose to user via ln.Features
 def parse_features_from_iterable(
     iterable: ListLike,
     field: Field,
-    species: str = None,
+    species: Optional[str] = None,
 ):
-    # No entries are made for NAs, '', None
-    iterable = [i for i in set(iterable) if not (pd.isnull(i) or i == "" or i == " ")]
-    entity = field.field.model
-    reference = entity.bionty(species=species)
-    df = pd.DataFrame(index=list(iterable))
-    return parse_features(df, reference, field=field)
+    # get related_name of the field class from Featureset class
+    model = field.field.model
+    related_name = [
+        i.related_name
+        for i in Featureset._meta.related_objects
+        if i.related_model == model
+    ]
+    if len(related_name) == 0:
+        raise AssertionError(
+            f"Can't create featuresets from {model.__name__}! Check your schema!"
+        )
+    else:
+        related_name = related_name[0]
+
+    iterable_set = not_null_iterable(iterable)
+
+    features_hash = hash_set(iterable_set)
+
+    featureset = select(
+        Featureset,
+        id=features_hash,
+        type=related_name,
+    ).one_or_none()
+    if featureset is not None:
+        logger.info("Returning an existing featureset")
+    else:
+        records = get_or_create_records(
+            iterable=iterable_set, field=field, species=species
+        )
+        featureset = Featureset(
+            id=features_hash, type=related_name, **{related_name: records}
+        )
+    return featureset
--- lamindb/_parse.py
+++ lamindb/_parse.py
@@ -1,4 +1,4 @@
-from typing import Dict, Iterable, List, TypeVar, Union
+from typing import Any, Dict, Iterable, List, Optional, Tuple, TypeVar, Union
 
 import numpy as np
 import pandas as pd
@@ -6,7 +6,6 @@ from django.db.models import Model
 from django.db.models.query_utils import DeferredAttribute as Field
 from lamin_logger import logger
 
-from ._save import save
 from ._select import select
 
 ListLike = TypeVar("ListLike", pd.Series, list, np.array)
@@ -57,75 +56,97 @@ def parse(
         return get_or_create_records(iterable=iterable, field=field, species=species)
 
 
+def not_null_iterable(iterable: Iterable) -> set:
+    # No entries are made for NAs, '', None
+    return set([i for i in iterable if (not pd.isnull(i)) and (i != "")])
+
+
 def get_or_create_records(
     iterable: Iterable,
     field: Field,
-    species: str = None,
+    species: Optional[str] = None,
 ) -> List:
-    # No entries are made for NAs, '', None
-    iterable = set([i for i in iterable if (not pd.isnull(i)) and (i != "")])
+    """Get or create records from iterables."""
+    iterable = not_null_iterable(iterable)
     model = field.field.model  # is DeferredAttribute
-    model_field_names = {i.name for i in model._meta.fields}
-    parsing_id = field.field.name
+    field_name = field.field.name
 
-    # get all existing records in the db
-    # if species is specified, only pull species-specific records
-    condition = {f"{parsing_id}__in": iterable}
+    kwargs_species, condition_species, bionty_df = _preprocess_species(
+        species=species, model=model
+    )
 
-    # for bionty records, will add species if needed
-    additional_kwargs: Dict = {}
-    reference_df = pd.DataFrame()
-    if model.__module__.startswith("lnschema_bionty."):
-        import bionty as bt
-
-        bionty_object = getattr(bt, model.__name__)(species=species)
-
-        # insert species entry if not exists
-        if bionty_object.species != "all":
-            from lnschema_bionty import Species
-
-            species = select(Species, name=bionty_object.species).one_or_none()
-            if species is None:
-                species = save(Species.from_bionty(name=bionty_object.species))
-
-            condition["species__name"] = species.name  # type:ignore
-            additional_kwargs.update({"species": species})
-            logger.info(
-                f"Returned records with species='{species.name}'."  # type:ignore
-            )
-
-        reference_df = bionty_object.df().reset_index().set_index(parsing_id)
-        reference_df = reference_df.loc[:, reference_df.columns.isin(model_field_names)]
-
+    # get all existing records in the db for a species
+    condition = {f"{field_name}__in": iterable}
+    condition.update(condition_species)
     stmt = select(model, **condition)
+
     records = stmt.list()
     if len(records) > 0:
-        logger.hint(f"Returned {len(records)} existing records.")
+        logger.hint(f"Returned {len(records)} existing {model.__name__} records.")
 
-    existing_ids = iterable.intersection(stmt.df()[parsing_id])
+    existing_values = iterable.intersection(stmt.values_list(field_name, flat=True))
 
-    # new records to be appended
-    new_ids = iterable.difference(existing_ids)
-    if len(new_ids) > 0:
-        # mapped new_ids will fetch fields values from bionty
-        mapped_id = reference_df.index.intersection(new_ids)
-        if len(mapped_id) > 0:
-            logger.hint(f"Mapped {len(mapped_id)} records from bionty")
-            new_ids_kwargs = _bulk_create_dicts_from_df(
-                keys=mapped_id, column_name=parsing_id, df=reference_df
+    # new records to be created based on new values
+    new_values = iterable.difference(existing_values)
+    if len(new_values) > 0:
+        # first try to populate additional fields from bionty
+        mapped_values = bionty_df[field_name].intersection(new_values)
+        if len(mapped_values) > 0:
+            new_values_kwargs = _bulk_create_dicts_from_df(
+                keys=mapped_values, column_name=field_name, df=bionty_df
             )
-            for kwargs in new_ids_kwargs:
-                kwargs.update(additional_kwargs)
+            for kwargs in new_values_kwargs:
+                kwargs.update(kwargs_species)
                 records.append(model(**kwargs))
-        # unmapped new_ids will only create records with parsing_id and species
-        unmapped = set(new_ids).difference(mapped_id)
-        if len(unmapped) > 0:
-            logger.hint(f"Created {len(unmapped)} new records")
-            for i in unmapped:
-                kwargs = {parsing_id: i}
-                kwargs.update(additional_kwargs)
+            logger.hint(
+                f"Created {len(mapped_values)} {model.__name__} records from bionty"
+                f" with {bionty_df.shape[1]} fields"
+            )
+        # unmapped new_ids will only create records with field and species
+        unmapped_values = new_values.difference(mapped_values)
+        if len(unmapped_values) > 0:
+            for i in unmapped_values:
+                kwargs = {field_name: i}
+                kwargs.update(kwargs_species)
                 records.append(model(**kwargs))
+            logger.hint(
+                f"Created {len(unmapped_values)} {model.__name__} records with a single"
+                f" field '{field_name}'"
+            )
     return records
+
+
+def _preprocess_species(
+    model: Model, species: Optional[str] = None
+) -> Tuple[dict, dict, pd.DataFrame]:
+    kwargs_species: Dict = {}
+    condition_species: Dict = {}
+    from lnschema_bionty._bionty import create_or_get_species_record, get_bionty_object
+
+    bionty_object = get_bionty_object(model=model, species=species)
+
+    if bionty_object is not None:
+        species_record = create_or_get_species_record(species=bionty_object.species)
+
+        # if species is specified, only pull species-specific records
+        condition_species["species__name"] = species_record.name
+        # add species to the record
+        kwargs_species["species"] = species_record
+        logger.info(
+            f"Returning records with species='{species_record.name}'."  # type:ignore
+        )
+    bionty_df = _filter_bionty_df_columns(model=model, bionty_object=bionty_object)
+
+    return kwargs_species, condition_species, bionty_df
+
+
+def _filter_bionty_df_columns(model: Model, bionty_object: Any) -> pd.DataFrame:
+    bionty_df = pd.DataFrame()
+    if bionty_object is not None:
+        model_field_names = {i.name for i in model._meta.fields}
+        bionty_df = bionty_object.df().reset_index()
+        bionty_df = bionty_df.loc[:, bionty_df.columns.isin(model_field_names)]
+    return bionty_df
 
 
 def _bulk_create_dicts_from_df(keys: list, column_name: str, df: pd.DataFrame) -> dict:
