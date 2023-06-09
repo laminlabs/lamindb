f15ffb4025925aada7f3d8dff1e92cecc156dbf5
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-06-09 17:23:03 -0400

    ✨ Populate `bionty_version` in `ln.parse` (#739)

--- lamindb/_check_versions.py
+++ lamindb/_check_versions.py
@@ -11,8 +11,8 @@ if os.getenv("GITHUB_ACTIONS") is None:
     if version.parse(lnschema_core_v) != version.parse("0.35.2"):
         logger.warning("lamindb needs lnschema_core==0.35.2")
 
-    if version.parse(lamindb_setup_v) < version.parse("0.47.0"):
-        logger.warning("Upgrade lamindb_setup! pip install lamindb_setup==0.47.0")
+    if version.parse(lamindb_setup_v) < version.parse("0.47.1"):
+        logger.warning("Upgrade lamindb_setup! pip install lamindb_setup==0.47.1")
 
     # Lamin GREATEREQ packages
     try:
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -22,7 +22,9 @@ def test_parse_name(df):
     result = ln.parse(df.cell_type, CellType.name)
     ids = [i.ontology_id for i in result]
     assert len(result) == 3
-    assert ids == ["CL:0000182", "CL:0000084", None]
+    assert ids == ["CL:0000084", "CL:0000182", None]
+    assert result[0].bionty_source.entity == "CellType"
+    assert result[2].bionty_source is None
 
 
 def test_parse_ontology_id(df):
@@ -32,6 +34,7 @@ def test_parse_ontology_id(df):
     names = [i.name for i in result]
     assert len(result) == 2
     assert names == ["T cell", "hepatocyte"]
+    assert result[0].bionty_source.entity == "CellType"
 
 
 def test_parse_df(df):
