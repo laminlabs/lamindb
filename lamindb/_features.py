19da68b9ffc56967373b3853235c66ea0c02a493
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-06-10 13:43:50 +0200

    :truck: Rename Featureset to FeatureSet

--- lamindb/_features.py
+++ lamindb/_features.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from lamin_logger import logger
-from lnschema_core import Featureset
+from lnschema_core import FeatureSet
 
 from lamindb._select import select
 from lamindb.dev.hashing import hash_set
@@ -9,17 +9,17 @@ from lamindb.dev.hashing import hash_set
 from ._parse import Field, ListLike, get_or_create_records, index_iterable
 
 
-# expose to user via ln.Featureset
+# expose to user via ln.FeatureSet
 def parse_features_from_iterable(
     iterable: ListLike,
     field: Field,
     species: Optional[str] = None,
 ):
-    # get related_name of the field class from Featureset class
+    # get related_name of the field class from FeatureSet class
     model = field.field.model
     related_name = [
         i.related_name
-        for i in Featureset._meta.related_objects
+        for i in FeatureSet._meta.related_objects
         if i.related_model == model
     ]
     if len(related_name) == 0:
@@ -34,7 +34,7 @@ def parse_features_from_iterable(
     features_hash = hash_set(set(iterable_idx))
 
     featureset = select(
-        Featureset,
+        FeatureSet,
         id=features_hash,
         type=related_name,
     ).one_or_none()
@@ -44,7 +44,7 @@ def parse_features_from_iterable(
         records = get_or_create_records(
             iterable=iterable_idx, field=field, species=species
         )
-        featureset = Featureset(
+        featureset = FeatureSet(
             id=features_hash, type=related_name, **{related_name: records}
         )
     return featureset
