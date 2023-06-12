81ba580a8ab1bdb164df036bf225584ffe48b7d8
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-12 08:38:20 +0200

    :recycle: Move init method here completely

--- lamindb/_file.py
+++ lamindb/_file.py
@@ -266,6 +266,18 @@ def log_storage_hint(
 
 
 def init_file(file: File, *args, **kwargs):
+    # Below checks for the Django-internal call in from_db()
+    # it'd be better if we could avoid this, but not being able to create a File
+    # from data with the default constructor renders the central class of the API
+    # essentially useless
+    # The danger below is not that a user might pass as many args (12 of it), but rather
+    # that at some point the Django API might change; on the other hand, this
+    # condition of for calling the constructor based on kwargs should always
+    # stay robust
+    if len(args) == len(file._meta.concrete_fields):
+        super(File, file).__init__(*args, **kwargs)
+        return None
+    # now we proceed with the user-facing constructor
     if len(args) > 1:
         raise ValueError("Only one non-keyword arg allowed: data")
     data: Union[PathLike, DataLike] = kwargs["data"] if len(args) == 0 else args[0]
@@ -308,4 +320,4 @@ def init_file(file: File, *args, **kwargs):
         file._memory_rep = privates["memory_rep"]
         file._to_store = not privates["check_path_in_storage"]
 
-    return kwargs
+    super(File, file).__init__(**kwargs)
--- lamindb/types.py
+++ lamindb/types.py
@@ -7,14 +7,4 @@
    DataLike
    TransformType
 """
-from pathlib import Path
-from typing import Any, TypeVar
-
-from upath import UPath
-
-PathLike = TypeVar("PathLike", str, Path, UPath)
-# statically typing the following is hard because these are all heavy
-# dependencies, even DataFrame is heavy & slow to import
-DataLike = Any
-
-from lnschema_core.types import TransformType  # noqa
+from lnschema_core.types import DataLike, PathLike, TransformType  # noqa
