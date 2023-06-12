e3858e1390c41396efaeae4611a141a71383f0c0
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-12 06:54:22 +0200

    :recycle: Refactor tests

--- tests/test_file_class.py
+++ tests/test_file_class.py
@@ -9,49 +9,36 @@ from lnschema_core.models import File
 # set in conftest.py
 
 
-@pytest.fixture(scope="module")
-def create_dummy_files():
-    # create a file within the default storage
-    dir1 = Path("./default_storage") / Path("my_project")
-    dir1.mkdir(parents=True)
-    filepath1 = dir1 / "my_file1.csv"
-    filepath1.write_text("a")
-    # create a file outside the default storage
-    dir2 = Path("./outside_storage") / Path("my_project")
-    dir2.mkdir(parents=True)
-    filepath2 = dir2 / "my_file2.csv"
-    filepath2.write_text("b")
-    yield
-    shutil.rmtree(dir1)
-    shutil.rmtree(dir2)
+@pytest.fixture(
+    scope="module", params=[(True, "./default_storage/"), (False, "./outside_storage/")]
+)
+def get_test_filepaths(request):
+    isin_default_storage: bool = request.param[0]
+    root_dir: Path = Path(request.param[1])
+    test_folder = root_dir / "my_project/"
+    test_folder.mkdir(parents=True)
+    test_filepath = test_folder / "my_file.csv"
+    test_filepath.write_text("a")
+    # return a boolean indicating whether test filepath is in default storage
+    # and the test filepath
+    yield (isin_default_storage, test_filepath)
+    shutil.rmtree(test_folder)
 
 
-def test_init_from_filepath(create_dummy_files):
-    # file within storage, key=None, name=None
-    file = File("./default_storage/my_project/my_file1.csv")
-    assert file.name == "my_file1.csv"
+# this tests the basic (non-provenance-related) metadata
+@pytest.mark.parametrize("name", [None, "my name"])
+def test_init_from_filepath_basic_fields(get_test_filepaths, name):
+    isin_default_storage = get_test_filepaths[0]
+    test_filepath = get_test_filepaths[1]
+    file = File(test_filepath, name=name)
+    if name is None:
+        assert file.name == test_filepath.name
+    else:
+        assert file.name == name
     assert file.suffix == ".csv"
-    assert file.key == "my_project/my_file1.csv"
+    if isin_default_storage:
+        assert file.key == "my_project/my_file.csv"
+    else:
+        assert file.key is None
     assert file.storage.root == Path("./default_storage").resolve().as_posix()
     assert file.hash == "DMF1ucDxtqgxw5niaXcmYQ"
-    # file outside storage, key=None, name=None
-    file = File("./outside_storage/my_project/my_file2.csv")
-    assert file.name == "my_file2.csv"
-    assert file.suffix == ".csv"
-    assert file.key is None
-    assert file.storage.root == Path("./default_storage").resolve().as_posix()
-    assert file.hash == "kutf_uauL-w61xx3dTFXjw"
-    # file within storage, key=None, name!=None
-    file = File("./default_storage/my_project/my_file1.csv", name="hello")
-    assert file.name == "hello"
-    assert file.suffix == ".csv"
-    assert file.key == "my_project/my_file1.csv"
-    assert file.storage.root == Path("./default_storage").resolve().as_posix()
-    assert file.hash == "DMF1ucDxtqgxw5niaXcmYQ"
-    # file outside storage, key=None, name!=None
-    file = File("./outside_storage/my_project/my_file2.csv", name="hello")
-    assert file.name == "hello"
-    assert file.suffix == ".csv"
-    assert file.key is None
-    assert file.storage.root == Path("./default_storage").resolve().as_posix()
-    assert file.hash == "kutf_uauL-w61xx3dTFXjw"
