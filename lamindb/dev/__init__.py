9a7bb9723fd1c467d344dd9127823cdc47714c94
Sergei Rybakov <koncopd@gmail.com>
2023-06-12 21:26:58 +0200

    🔥 Remove stream and lazy (#760)

--- lamindb/dev/__init__.py
+++ lamindb/dev/__init__.py
@@ -6,12 +6,9 @@
    BaseORM
    QuerySet
    datasets
-   LazyDataFrame
 """
 
 from lnschema_core._queryset import QuerySet
 from lnschema_core.models import BaseORM
-
-from lamindb.dev.storage.object._lazy_field import Lazy as LazyDataFrame
 
 from . import datasets  # noqa
