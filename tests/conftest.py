d86a0cb1c3af50e485a61374c2064396fb623323
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-20 18:16:24 +0200

    👷 Switch unit tests to postgres

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,22 @@
 import shutil
+from subprocess import run
 
 import lamindb_setup
 import pytest
+from laminci.db import setup_local_test_postgres
 
 
 def pytest_sessionstart(session: pytest.Session):
+    pgurl = setup_local_test_postgres()
     lamindb_setup.init(
-        storage="./default_storage", schema="bionty", name="lamindb-unit-tests"
+        storage="./default_storage",
+        schema="bionty",
+        name="lamindb-unit-tests",
+        db=pgurl,
     )
 
 
 def pytest_sessionfinish(session: pytest.Session):
     lamindb_setup.delete("lamindb-unit-tests")
     shutil.rmtree("./default_storage")
+    run("docker stop pgtest && docker rm pgtest", shell=True)
