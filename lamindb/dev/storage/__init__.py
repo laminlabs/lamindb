ec0847aa02249ac6c97bc17f102450410c0ef085
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-21 12:51:22 +0200

    🔥 Delete images

--- lamindb/dev/storage/__init__.py
+++ lamindb/dev/storage/__init__.py
@@ -11,6 +11,5 @@ from lamindb_setup.dev.upath import UPath
 from lamindb_setup.dev.upath import infer_filesystem as _infer_filesystem
 
 from ._file import delete_storage, load_to_memory, store_object
-from ._images import store_png
 from ._zarr import read_adata_zarr, write_adata_zarr
 from .object._backed_access import AnnDataAccessor
