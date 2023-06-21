e46018bc7c1d5fc1b19b9ad8c90f5a9e5cc1864e
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-21 12:19:05 +0200

    🔥 Remove h5ad_to_anndata

--- lamindb/dev/storage/__init__.py
+++ lamindb/dev/storage/__init__.py
@@ -11,7 +11,6 @@ from lamindb_setup.dev.upath import UPath
 from lamindb_setup.dev.upath import infer_filesystem as _infer_filesystem
 
 from ._file import delete_storage, load_to_memory, store_object
-from ._h5ad import h5ad_to_anndata
 from ._images import store_png
 from ._zarr import read_adata_zarr, write_adata_zarr
 from .object._backed_access import AnnDataAccessor
--- lamindb/dev/storage/_h5ad.py
+++ lamindb/dev/storage/_h5ad.py
@@ -1,12 +1,6 @@
 import anndata
 from anndata import AnnData
-from lamindb_setup import settings
 from lamindb_setup.dev.upath import infer_filesystem
-
-
-def h5ad_to_anndata(filekey) -> AnnData:
-    """h5ad → AnnData."""
-    return anndata.read(settings.instance.storage.local_filepath(filekey))
 
 
 def read_adata_h5ad(filepath, **kwargs) -> AnnData:
