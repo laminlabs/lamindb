89e5e831db82596e29c3fbd4e5a7f36ec10ce1b0
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-22 12:47:55 +0200

    🔇 Fix logging

--- lamindb/_file.py
+++ lamindb/_file.py
@@ -117,8 +117,10 @@ def get_run(run: Optional[Run]) -> Optional[Run]:
 
         run = context.run
         if run is None:
-            logger.info("No run & transform get linked to this file")
-            logger.hint("Consider using the `run` argument or ln.track()")
+            logger.hint(
+                "no run & transform get linked, consider passing a `run` or calling"
+                " ln.track()"
+            )
     return run
 
 
@@ -367,13 +369,16 @@ def from_dir(
     # TODO: UPath doesn't list the first level files and dirs with "*"
     pattern = "" if isinstance(folderpath, UPath) else "*"
 
+    # silence fine-grained logging
+    verbosity = settings.verbosity
+    settings.verbosity = 3
     files = []
     for filepath in folderpath.rglob(pattern):
         if filepath.is_file():
             relative_path = get_relative_path_to_directory(filepath, folderpath)
             file_key = folder_key + "/" + relative_path.as_posix()
             files.append(File(filepath, run=run, key=file_key))
-
+    settings.verbosity = verbosity
     logger.info(f"→ {len(files)} files")
     return files
 
