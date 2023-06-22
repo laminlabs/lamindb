700433c77db070071cc3e15f211d4a89e5c0198e
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-22 12:08:36 +0200

    ✅ Add test for required field

--- tests/test_baseorm.py
+++ tests/test_baseorm.py
@@ -6,3 +6,9 @@ import lamindb as ln
 def test_init_with_args():
     with pytest.raises(ValueError):
         ln.Tag()
+
+
+def test_validate_required_fields():
+    # project has a required name
+    with pytest.raises(TypeError):
+        ln.Project(external_id="test")
