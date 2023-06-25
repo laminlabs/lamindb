fb79ef18a8de8b59d24064d94a7f1957780d3264
Koncopd <koncopd@gmail.com>
2023-06-25 15:11:42 +0200

    test with local

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -6,16 +6,18 @@ from lamindb.dev.storage._zarr import read_adata_zarr, write_adata_zarr
 
 
 def test_anndata_io():
-    adata = read_adata_h5ad("s3://lamindb-ci/lndb-storage/pbmc68k.h5ad")
+    test_files = Path("./test-files")
+
+    adata = read_adata_h5ad(test_files / "pbmc68k.h5ad")
 
     def callback(*args, **kwargs):
         pass
 
-    zarr_path = Path("./pbmc68k.zarr")
+    zarr_path = test_files / "pbmc68k.zarr"
     write_adata_zarr(adata, zarr_path, callback)
 
     adata = read_adata_zarr(zarr_path)
 
-    assert adata.shape == (70, 765)
+    assert adata.shape == (30, 765)
 
     delete_storage(zarr_path)
