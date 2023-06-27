d77fa6da774d0e8e5995a891e8beb6e800ecbb2b
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-28 00:11:49 +0200

    🎉 Init staging 0.46

--- lamindb/__init__.py
+++ lamindb/__init__.py
@@ -54,7 +54,7 @@ Developer API:
 
 """
 
-__version__ = "0.45a1"  # denote a release candidate for 0.1.0 with 0.1rc1
+__version__ = "0.46.dev1"  # denote a release candidate for 0.1.0 with 0.1rc1
 
 import lamindb_setup as _lamindb_setup
 
