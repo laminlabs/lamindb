63d1a09c244d3a4dfc3bb1a21519a206ba4fc439
Alex Wolf <f.alexander.wolf@gmail.com>
2023-07-01 17:48:46 +0200

    ♻️ Refactor

--- lamindb/_feature_set.py
+++ lamindb/_feature_set.py
@@ -1,97 +1,95 @@
-from typing import Dict, Optional, Union
+from typing import List, Optional
 
 from lamin_logger import logger
-from lnschema_core import Feature, FeatureSet
+from lnschema_core import ORM, Feature, FeatureSet
 
-from lamindb._select import select
 from lamindb.dev.hashing import hash_set
 
 from ._from_values import Field, ListLike, get_or_create_records, index_iterable
 
 
-# expose to user via ln.FeatureSet
-def parse_features_from_iterable(
-    iterable: ListLike,
-    field: Field,
-    species: Optional[str] = None,
-):
-    # get related_name of the field class from FeatureSet class
-    model = field.field.model
-    related_name = [
-        i.related_name
-        for i in FeatureSet._meta.related_objects
-        if i.related_model == model
+def get_related_name(features_type: ORM):
+    candidates = [
+        field.related_name
+        for field in FeatureSet._meta.related_objects
+        if field.related_model == features_type
     ]
-    if len(related_name) == 0:
-        raise AssertionError(
-            f"Can't create featuresets from {model.__name__}! Check your schema!"
+    if not candidates:
+        raise ValueError(
+            f"Can't create feature sets from {features_type.__name__} because it's not"
+            " related to it!\nYou need to create a link model between FeatureSet and"
+            " your ORM in your custom schema.\nTo do so, add a line:\nfeature_sets ="
+            " models.ManyToMany(FeatureSet, related_name='mythings')\n"
         )
-    else:
-        related_name = related_name[0]
+    return candidates[0]
 
-    iterable_idx = index_iterable(iterable)
 
-    features_hash = hash_set(set(iterable_idx))
+def validate_features(features: List[ORM]) -> ORM:
+    """Validate and return feature type."""
+    if not isinstance(features, ListLike):
+        raise TypeError("features has to be list-like")
+    if not isinstance(features[0], ORM):
+        raise TypeError(
+            "features has to store feature records! use .from_values() otherwise"
+        )
+    feature_types = set([feature.model for feature in features])
+    if len(feature_types) > 1:
+        raise ValueError("feature_set can only contain a single type")
+    return next(iter(feature_types))  # return value in set of cardinality 1
 
-    feature_set = select(
-        FeatureSet,
-        id=features_hash,
-        type=related_name,
-    ).one_or_none()
 
-    from_bionty = (
-        True if field.field.model.__module__.startswith("lnschema_bionty.") else False
+def __init__(self, *args, **kwargs):
+    if len(args) == len(self._meta.concrete_fields):
+        super(FeatureSet, self).__init__(*args, **kwargs)
+        return None
+    # now we proceed with the user-facing constructor
+    if len(args) > 1:
+        raise ValueError("Only one non-keyword arg allowed: features")
+    features: List[ORM] = kwargs.pop("features") if len(args) == 0 else args[0]
+    assert len(kwargs) == 0
+    features_type = validate_features(features)
+    related_name = get_related_name(features_type)
+    features_hash = hash_set({feature.id for feature in features})
+    self._features = (related_name, features)
+    field_name = "id"
+    super(FeatureSet, self).__init__(
+        id=features_hash, type=features_type.__name__, field=field_name
     )
-    if feature_set is not None:
-        logger.info("Returning an existing feature_set")
-    else:
-        if species is not None:
-            kwargs = dict(species=species)
-        else:
-            kwargs = dict()
-        records = get_or_create_records(
-            iterable=iterable_idx, field=field, from_bionty=from_bionty, **kwargs
-        )
-        feature_set = FeatureSet(
-            id=features_hash, type=related_name, **{related_name: records}
-        )
-    return feature_set
 
 
-def __init__(self, *args, **kwargs):  # type: ignore
-    related_names = [i.related_name for i in self.__class__._meta.related_objects]
-
-    relationships: Dict = {}
-    for related_name in related_names:
-        if related_name in kwargs:
-            relationships[related_name] = kwargs.pop(related_name)
-    self._relationships = relationships
-
-    super(FeatureSet, self).__init__(*args, **kwargs)
-
-
-def save(feature_set, *args, **kwargs):
-    super(FeatureSet, feature_set).save(*args, **kwargs)
-    for key, records in feature_set._relationships.items():
-        [r.save() for r in records]
-        getattr(feature_set, key).set(records)
+def save(self, *args, **kwargs):
+    super(FeatureSet, self).save(*args, **kwargs)
+    if hasattr(self, "_features"):
+        related_name, records = self._features
+        getattr(self, related_name).set(records)
 
 
 @classmethod  # type:ignore
 def from_values(
-    cls, values: ListLike, field: Union[Field, str] = Feature.name, **kwargs
-):
-    if isinstance(field, str):
-        field = getattr(cls, field)
-    if not isinstance(field, Field):  # field is DeferredAttribute
-        raise TypeError(
-            "field must be a string or an ORM field, e.g., `CellType.name`!"
+    cls, values: ListLike, field: Field = Feature.name, species: Optional[str] = None
+) -> FeatureSet:
+    if not isinstance(field, Field):
+        raise TypeError("Argument `field` must be an ORM field, e.g., `Feature.name`")
+    orm = field.field.model
+    iterable_idx = index_iterable(values)
+    features_hash = hash_set(set(iterable_idx))
+    feature_set = FeatureSet.select(id=features_hash, type=orm.__name__).one_or_none()
+    if feature_set is not None:
+        logger.info("Returning an existing feature_set")
+    else:
+        from_bionty = orm.__module__.startswith("lnschema_bionty.")
+        records = get_or_create_records(
+            iterable=iterable_idx,
+            field=field,
+            from_bionty=from_bionty,
+            species=species,
         )
-    feature_set = parse_features_from_iterable(
-        iterable=values,
-        field=field,
-        species=kwargs.get("species"),
-    )
+        feature_set = FeatureSet(
+            id=features_hash,
+            type=orm.__name__,
+            field=field.field_name,
+            features=records,
+        )
     return feature_set
 
 
