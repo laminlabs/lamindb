a7eb8f7d743fb08b5d11a46fad6230c9860b3b94
Alex Wolf <f.alexander.wolf@gmail.com>
2023-07-03 14:07:04 +0200

    🚸 Better title errors

--- lamindb/_context.py
+++ lamindb/_context.py
@@ -21,7 +21,15 @@ msg_path_failed = (
 )
 
 
-class NonInteractiveEditorError(Exception):
+class InitWithNonInteractiveEditorError(Exception):
+    pass
+
+
+class NotebookNotSavedError(Exception):
+    pass
+
+
+class NoTitleError(Exception):
     pass
 
 
@@ -177,14 +185,10 @@ class context:
                             " notebook!\nConsider installing nbproject for automatic"
                             " name, title & id tracking."
                         )
-                    elif isinstance(e, NonInteractiveEditorError):
+                    elif isinstance(e, InitWithNonInteractiveEditorError):
                         raise e
-                    elif isinstance(e, IndexError):
-                        logger.warning(
-                            "The notebook is not saved, please save the notebook and"
-                            " rerun ln.track()."
-                        )
-                        return None
+                    elif isinstance(e, NotebookNotSavedError):
+                        raise e
                     else:
                         logger.warning(f"Automatic tracking of notebook failed: {e}")
                     is_tracked_notebook = False
@@ -322,11 +326,11 @@ class context:
             if _env in ("lab", "notebook"):
                 cls._notebook_meta = metadata  # type: ignore
             else:
-                msg_init_noninteractive = (
+                msg = (
                     "Please attach metadata to the notebook by running the CLI: "
                     f"lamin track {notebook_path}"
                 )
-                raise NonInteractiveEditorError(msg_init_noninteractive)
+                raise InitWithNonInteractiveEditorError(msg)
 
         if _env in ("lab", "notebook"):
             # save the notebook in case that title was updated
@@ -337,7 +341,17 @@ class context:
             id = metadata["id"]
             version = metadata["version"]
             filestem = Path(_filepath).stem
-            title = nbproject.meta.live.title
+            try:
+                title = nbproject.meta.live.title
+            except IndexError:
+                raise NotebookNotSavedError(
+                    "The notebook is not saved, please save the notebook and"
+                    " rerun `ln.track()`"
+                )
+            if title is None:
+                raise NoTitleError(
+                    "Please add a title to your notebook in a markdown cell: # Title"
+                )
         else:
             version = "0"
             title = None
