7dbe3f0471cf0c36bf434be7b0397a7d5b9b51dd
Alex Wolf <f.alexander.wolf@gmail.com>
2023-07-05 09:29:29 +0200

    🔖 Release 0.46a3

--- lamindb/__init__.py
+++ lamindb/__init__.py
@@ -55,7 +55,7 @@ Developer API:
 
 """
 
-__version__ = "0.46a2"  # denote a release candidate for 0.1.0 with 0.1rc1
+__version__ = "0.46a3"  # denote a release candidate for 0.1.0 with 0.1rc1
 
 import os as _os
 
