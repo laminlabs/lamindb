63a8e5aa7da01be8836303d09273de53045a355e
Alex Wolf <f.alexander.wolf@gmail.com>
2023-07-06 15:22:19 +0200

    🚸 select only returns queryset

--- lamindb/_select.py
+++ lamindb/_select.py
@@ -1,30 +1,14 @@
-from typing import Union
+from typing import Type
 
-from django.db.models import Manager
 from lnschema_core import ORM
 
 from lamindb._queryset import QuerySet
 
 
-def select(*ORM: ORM, **expressions) -> Union[QuerySet, Manager]:
-    """Query records.
-
-    Guide: :doc:`/guide/select`.
-
-    Args:
-        ORM: An ORM class.
-        expressions: Fields and values passed as Django query expressions.
-
-    Returns:
-        A `QuerySet` or Django `Manager`.
-    """
-    if len(ORM) > 1:
-        raise NotImplementedError(
-            "Currently no longer implemented with Django, but we hope to have it back."
-        )
-    manager = QuerySet.as_manager()
-    manager.model = ORM[0]
+def select(ORM: Type[ORM], **expressions) -> QuerySet:
+    """See :meth:`~lamindb.dev.ORM.select`."""
+    qs = QuerySet(model=ORM)
     if len(expressions) > 0:
-        return manager.filter(**expressions)
+        return qs.filter(**expressions)
     else:
-        return manager
+        return qs
