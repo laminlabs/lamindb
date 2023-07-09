4a07e1b8e0430854ca27a4018a9d80b80c7a15eb
Alex Wolf <f.alexander.wolf@gmail.com>
2023-07-09 16:59:52 +0200

    ✅ Fix bug and add test for multi-parents track

---
+++ tests/test_context.py
@@ -0,0 +1,12 @@
+import lamindb as ln
+
+
+def test_track_with_multi_parents():
+    parent1 = ln.Transform(name="parent 1")
+    parent1.save()
+    parent2 = ln.Transform(name="parent 2")
+    parent2.save()
+    child = ln.Transform(name="Child")
+    child.save()
+    child.parents.set([parent1, parent2])
+    ln.track(child)
