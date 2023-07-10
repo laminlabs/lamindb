8ff57164b2a7247d90805a31217db8da6c3159a9
Alex Wolf <f.alexander.wolf@gmail.com>
2023-07-10 18:29:17 +0200

    🚸 Fixed user dialogue

--- lamindb/_context.py
+++ lamindb/_context.py
@@ -84,11 +84,8 @@ def reinitialize_notebook(
     if response == "y":
         new_id = lnschema_core.ids.base62_12()
     else:
-        response = input(
-            "Do you want to set a new version (e.g. '1.1')? Type 'n' for"
-            " 'no'. (version/n)"
-        )
-        if response != "n":
+        response = input("Do you want to set a new version (e.g. '1.1')? (y/n)")
+        if response == "y":
             new_version = input("Please type the version: ")
 
     nb = None
