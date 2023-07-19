bd322d8ef3329de30e1020ac3a22eabd3860dedc
Alex Wolf <f.alexander.wolf@gmail.com>
2023-07-19 23:04:00 +0200

    💄 Polish tutorial (#899)

--- lamindb/_view.py
+++ lamindb/_view.py
@@ -1,6 +1,6 @@
 import importlib
 import inspect
-from typing import Optional
+from typing import List, Optional
 
 from IPython.display import display
 from lamin_logger import colors
@@ -11,13 +11,15 @@ from lnschema_core import ORM
 from ._select import select
 
 
-def view(n: int = 10, schema: Optional[str] = None):
+def view(n: int = 10, schema: Optional[str] = None, orms: Optional[List[str]] = None):
     """View data.
 
     Args:
-        n: Display the latest n rows of the tables.
-        schema: Schema module to view. Default's to `None` and
-            displays all schema modules.
+        n: ``int = 10`` Display the last `n` rows of a table.
+        schema: ``Optional[str] = None`` Schema module to view. Default's to
+            `None` and displays all schema modules.
+        orms: ``Optional[List[str]] = None`` List of ORM names. Defaults to
+            `None` and lists all ORMs.
 
     Examples:
         >>> ln.view()
@@ -30,18 +32,22 @@ def view(n: int = 10, schema: Optional[str] = None):
     for schema_name in schema_names:
         schema_module = importlib.import_module(get_schema_module_name(schema_name))
 
-        orms = {
-            i
-            for i in schema_module.__dict__.values()
-            if inspect.isclass(i) and issubclass(i, ORM) and i.__name__ != "ORM"
+        all_orms = {
+            orm
+            for orm in schema_module.__dict__.values()
+            if inspect.isclass(orm) and issubclass(orm, ORM) and orm.__name__ != "ORM"
         }
+        if orms is not None:
+            filtered_orms = {orm for orm in all_orms if orm.__name__ in orms}
+        else:
+            filtered_orms = all_orms
         if len(schema_names) > 1:
             section = f"* module: {colors.green(colors.bold(schema_name))} *"
             section_no_color = f"* module: {schema_name} *"
             print("*" * len(section_no_color))
             print(section)
             print("*" * len(section_no_color))
-        for orm in orms:
+        for orm in sorted(filtered_orms, key=lambda x: x.__name__):
             if hasattr(orm, "updated_at"):
                 df = select(orm).order_by("-updated_at")[:n].df()
             else:
