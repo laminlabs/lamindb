3aed576010a85ba98454c55fc230131a34ab0988
Alex Wolf <f.alexander.wolf@gmail.com>
2023-07-21 09:34:55 +0200

    🚸 Improve feature categories

--- lamindb/_category.py
+++ lamindb/_category.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 
 from lamindb_setup.dev._docs import doc_args
 from lnschema_core import Category, Feature
@@ -12,15 +12,21 @@ from ._from_values import get_or_create_records, index_iterable
 
 @classmethod  # type:ignore
 @doc_args(Category.from_values.__doc__)
-def from_values(cls, values: ListLike, feature: Feature, **kwargs) -> List["Category"]:
+def from_values(
+    cls, values: ListLike, feature: Union[Feature, str], **kwargs
+) -> List["Category"]:
     """{}"""
     iterable_idx = index_iterable(values)
+    if isinstance(feature, str):
+        feature_id = Feature.select(name=feature).one().id
+    else:
+        feature_id = feature.id
     records = get_or_create_records(
         iterable=iterable_idx,
         field=Category.name,
         # here, feature_id is a kwarg, which is an additional condition
         # in queries for potentially existing records
-        feature_id=feature.id,
+        feature_id=feature_id,
     )
     return records
 
