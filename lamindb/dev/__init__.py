8326f9352fc152812104308677f8285cf394df3d
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-07-25 22:47:11 +0200

    🚚 Rename context to run_context

--- lamindb/dev/__init__.py
+++ lamindb/dev/__init__.py
@@ -11,6 +11,7 @@
    hashing
    storage
    Settings
+   run_context
 """
 
 from lnschema_core.models import ORM
@@ -19,5 +20,6 @@ from lamindb._feature_manager import FeatureManager
 from lamindb._manager import Manager
 from lamindb._queryset import QuerySet
 
+from .._context import run_context
 from . import datasets  # noqa
 from ._settings import Settings
