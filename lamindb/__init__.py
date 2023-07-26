4f0359afe197ee547494e746f3d6994086211769
Alex Wolf <f.alexander.wolf@gmail.com>
2023-07-26 13:24:46 +0200

    🔖 Release 0.48.0

--- lamindb/__init__.py
+++ lamindb/__init__.py
@@ -47,7 +47,7 @@ Static classes & modules:
 
 """
 
-__version__ = "0.48a3"  # denote a release candidate for 0.1.0 with 0.1rc1
+__version__ = "0.48.0"  # denote a release candidate for 0.1.0 with 0.1rc1
 
 import os as _os
 
