0ad515a077751f0e047757ad08d4c92763da0aca
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-08-15 13:20:06 +0200

    add tests for add/remove synonyms

--- tests/test_synonym.py
+++ tests/test_synonym.py
@@ -1,4 +1,5 @@
 import lnschema_bionty as lb
+import pytest
 
 import lamindb as ln  # noqa
 
@@ -21,3 +22,31 @@ def test_map_synonyms():
     mapper = lb.Gene.map_synonyms(["ABC1", "LMN1"], return_mapper=True)
     assert mapper == {"LMN1": "LMNA", "ABC1": "HEATR6"}
     assert lb.Gene.map_synonyms(["LMNA"]) == ["LMNA"]
+
+
+def test_add_remove_synonym():
+    bionty_source = lb.BiontySource.filter(species="human").first()
+    with pytest.raises(NotImplementedError):
+        bionty_source.add_synonym("syn")
+
+    cell_types = lb.CellType.from_values(["T cell", "B cell"], "name")
+    ln.save(cell_types)
+    tcell = lb.CellType.filter(name="T cell").one()
+    bcell = lb.CellType.filter(name="B cell").one()
+    tcell.add_synonym(["my cell type"])
+    tcell.add_synonym("")
+    tcell.add_synonym([])
+    assert "my cell type" in tcell.synonyms
+    with pytest.raises(SystemExit) as excinfo:
+        bcell.add_synonym("my cell type")
+    assert excinfo.value.code == AssertionError
+    with pytest.raises(AssertionError):
+        tcell.add_synonym("my|celltype")
+
+    tcell.remove_synonym("my cell type")
+    assert "my cell type" not in tcell.synonyms
+
+    tcell.synonyms = None
+    tcell.save()
+    tcell.add_synonym(["my cell type"])
+    tcell.synonyms == "my cell type"
