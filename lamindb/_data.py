afd49d5b64e5432e52262afb4fd043ddcb9d6dec
Alex Wolf <f.alexander.wolf@gmail.com>
2023-08-22 17:46:17 +0200

    ♻️ Simplify transform passing

--- lamindb/_data.py
+++ lamindb/_data.py
@@ -18,15 +18,5 @@ def get_run(run: Optional[Run]) -> Optional[Run]:
 
 
 def add_transform_to_kwargs(kwargs: Dict[str, Any], run: Run):
-    # transform cannot be directly passed, just via run
-    # it's directly stored in the file table to avoid another join
-    # mediate by the run table
     if run is not None:
-        if run.transform_id is not None:
-            kwargs["transform_id"] = run.transform_id
-        else:
-            # accessing the relationship should always be possible if
-            # the above if clause was false as then, we should have a fresh
-            # Transform object that is not queried from the DB
-            assert run.transform is not None
-            kwargs["transform"] = run.transform
+        kwargs["transform"] = run.transform
