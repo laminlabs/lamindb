dcf2fe0f49303484e0ba0d163b4ede1f85cd36aa
Sergei Rybakov <koncopd@gmail.com>
2023-08-29 19:27:34 +0200

    ⚡️ Delete cached files for local instances on File.save() (#1109)

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 
 import lamindb as ln
@@ -6,17 +8,41 @@ from lamindb.dev.storage.file import read_adata_h5ad
 
 # https://stackoverflow.com/questions/22627659/run-code-before-and-after-each-test-in-py-test
 # switch to cloud storage and back
-# needed to isolate this test from the others
-@pytest.fixture(autouse=True)
+@pytest.fixture
 def switch_storage():
-    ln.settings.storage = "s3://lamindb-ci"
+    cloud_storage = "s3://lamindb-ci"
 
-    yield
+    ln.settings.storage = cloud_storage
+
+    yield cloud_storage
 
     ln.settings.storage = "./default_storage"
 
 
-def test_cache():
+def test_local_cache():
+    # check that we have local storage
+    local_storage = Path("./default_storage").resolve().as_posix()
+    assert ln.setup.settings.storage.root_as_str == local_storage
+
+    test_file = ln.dev.datasets.anndata_file_pbmc68k_test()
+    adata = read_adata_h5ad(test_file)
+
+    file = ln.File(adata, key="test_cache.h5ad")
+    temp_path = file._local_filepath.resolve()
+    assert temp_path.exists()
+    assert ln.setup.settings.storage.cache_dir in temp_path.parents
+
+    file.save()
+    assert file.path.exists()
+    assert not temp_path.exists()
+
+    file.delete(storage=True)
+
+
+def test_cloud_cache(switch_storage):
+    # check that we have cloud storage
+    assert ln.setup.settings.storage.root_as_str == switch_storage
+
     cache_dir = ln.setup.settings.storage.cache_dir
     assert cache_dir is not None
 
