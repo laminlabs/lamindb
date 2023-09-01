27c9ec7784cc1ec8b047715bbf1fbcca4fde2b03
Alex Wolf <f.alexander.wolf@gmail.com>
2023-09-01 17:13:09 +0200

    ♻️ Better treatment of priors

--- lamindb/_priors.py
+++ lamindb/_priors.py
@@ -2,13 +2,24 @@ from functools import cached_property
 
 from lnschema_core import Modality, User
 
-Modality(name="meta").save()
+from . import settings
+from ._save import save
+
+MODALITIES = ["meta"]
 
 
-# might be a bad design idea & dispear again
+# might be a bad design idea & disappear again
 class Priors:
     @cached_property
     def modalities(self):
+        all_defaults = Modality.filter(modality__in=MODALITIES).all()
+        # update the registry
+        if len(all_defaults) != len(MODALITIES):
+            upon_create_search_names = settings.upon_create_search_names
+            settings.upon_create_search_names = False
+            modalities = [Modality(name=name) for name in MODALITIES]
+            settings.upon_create_search_names = upon_create_search_names
+            save(modalities)
         return Modality.lookup()
 
     @cached_property
