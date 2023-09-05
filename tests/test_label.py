f6a457b23bc234da88ce66e660a73606c1e9c813
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-09-05 12:19:20 +0200

    🧪 Tests for label

--- tests/test_label.py
+++ tests/test_label.py
@@ -4,5 +4,10 @@ import lamindb as ln
 
 
 def test_label():
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as error:
         ln.Label(x=1)
+    error.exconly() == "ValueError: Only name & description are valid keyword arguments"
+
+    with pytest.raises(ValueError) as error:
+        ln.Label(1)
+    error.exconly() == "ValueError: Only one non-keyword arg allowed"
