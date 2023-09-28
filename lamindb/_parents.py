12e05aa85ca798a9d11b879ccd9e8847bd01e20d
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-09-28 07:53:22 -0400

    🎨 Show both file and dataset in view_flow (#1209)

--- lamindb/_parents.py
+++ lamindb/_parents.py
@@ -80,17 +80,6 @@ def view_flow(data: Union[File, Dataset], with_children: bool = True) -> None:
 
     data_label = _label_data_run_transform(data)
 
-    u = graphviz.Digraph(
-        data.id,
-        node_attr={
-            "fillcolor": GREEN_FILL,
-            "color": LAMIN_GREEN_DARKER,
-            "fontname": "Helvetica",
-            "fontsize": "10",
-        },
-        edge_attr={"arrowsize": "0.5"},
-    )
-
     def add_node(
         record: Union[Run, File, Dataset],
         node_id: str,
@@ -109,11 +98,18 @@ def view_flow(data: Union[File, Dataset], with_children: bool = True) -> None:
             fillcolor=fillcolor,
         )
 
+    u = graphviz.Digraph(
+        f"{data._meta.model_name}_{data.id}",
+        node_attr={
+            "fillcolor": GREEN_FILL,
+            "color": LAMIN_GREEN_DARKER,
+            "fontname": "Helvetica",
+            "fontsize": "10",
+        },
+        edge_attr={"arrowsize": "0.5"},
+    )
+
     for _, row in df_edges.iterrows():
-        if (row["source"] == data.id) & (row["source_record"] != data):
-            continue
-        if (row["target"] == data.id) & (row["target_record"] != data):
-            continue
         add_node(row["source_record"], row["source"], row["source_label"], u)
         if row["target_record"] not in df_edges["source_record"]:
             add_node(row["target_record"], row["target"], row["target_label"], u)
@@ -121,7 +117,7 @@ def view_flow(data: Union[File, Dataset], with_children: bool = True) -> None:
         u.edge(row["source"], row["target"], color="dimgrey")
     # label the searched file
     u.node(
-        data.id,
+        f"{data._meta.model_name}_{data.id}",
         label=data_label,
         style="rounded,filled",
         fillcolor=LAMIN_GREEN_LIGHTER,
@@ -269,17 +265,21 @@ def _add_emoji(record: Registry, label: str):
 
 def _get_all_parent_runs(data: Union[File, Dataset]) -> List:
     """Get all input file/dataset runs recursively."""
+    name = data._meta.model_name
     run_inputs_outputs = []
 
     runs = [data.run] if data.run is not None else []
     while len(runs) > 0:
         inputs = []
         for r in runs:
-            inputs_run = r.input_files.list() + r.input_datasets.list()
+            inputs_run = r.__getattribute__(f"input_{name}s").list()
+            if name == "file":
+                inputs_run += r.input_datasets.list()
             run_inputs_outputs += [(inputs_run, r)]
-            run_inputs_outputs += [
-                (r, r.output_files.list() + r.output_datasets.list())
-            ]
+            outputs_run = r.__getattribute__(f"output_{name}s").list()
+            if name == "file":
+                outputs_run += r.output_datasets.list()
+            run_inputs_outputs += [(r, outputs_run)]
             inputs += inputs_run
         runs = [f.run for f in inputs if f.run is not None]
     return run_inputs_outputs
@@ -287,24 +287,36 @@ def _get_all_parent_runs(data: Union[File, Dataset]) -> List:
 
 def _get_all_child_runs(data: Union[File, Dataset]) -> List:
     """Get all output file/dataset runs recursively."""
+    name = data._meta.model_name
     all_runs: Set[Run] = set()
     run_inputs_outputs = []
 
-    runs = {f.run for f in data.run.output_files.all()}
-    runs.update({f.run for f in data.run.output_datasets.all()})
+    runs = {f.run for f in data.run.__getattribute__(f"output_{name}s").all()}
+    if name == "file":
+        runs.update({f.run for f in data.run.output_datasets.all()})
     while runs.difference(all_runs):
         all_runs.update(runs)
         child_runs: Set[Run] = set()
         for r in runs:
-            run_inputs_outputs += [(r.input_files.list() + r.input_datasets.list(), r)]
-            run_outputs = r.output_files
-            run_inputs_outputs += [(r, run_outputs.list())]
+            inputs_run = r.__getattribute__(f"input_{name}s").list()
+            if name == "file":
+                inputs_run += r.input_datasets.list()
+            run_inputs_outputs += [(inputs_run, r)]
+            outputs_run = r.__getattribute__(f"output_{name}s").list()
+            if name == "file":
+                outputs_run += r.output_datasets.list()
+            run_inputs_outputs += [(r, outputs_run)]
             child_runs.update(
-                Run.filter(input_files__id__in=r.output_files.list("id")).list()
+                Run.filter(
+                    **{f"input_{name}s__id__in": [i.id for i in outputs_run]}
+                ).list()
             )
-            child_runs.update(
-                Run.filter(input_datasets__id__in=r.output_datasets.list("id")).list()
-            )
+            if name == "file":
+                child_runs.update(
+                    Run.filter(
+                        input_datasets__id__in=[i.id for i in outputs_run]
+                    ).list()
+                )
         runs = child_runs
     return run_inputs_outputs
 
@@ -349,8 +361,8 @@ def _df_edges_from_runs(df_values: List):
     df = df.explode("source_record")
     df = df.explode("target_record")
     df = df.drop_duplicates().dropna()
-    df["source"] = [i.id for i in df["source_record"]]
-    df["target"] = [i.id for i in df["target_record"]]
+    df["source"] = [f"{i._meta.model_name}_{i.id}" for i in df["source_record"]]
+    df["target"] = [f"{i._meta.model_name}_{i.id}" for i in df["target_record"]]
     df["source_label"] = df["source_record"].apply(_label_data_run_transform)
     df["target_label"] = df["target_record"].apply(_label_data_run_transform)
     return df
