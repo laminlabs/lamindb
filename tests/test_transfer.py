95463f0119db5b4fed352e5d387dbc931b08f78e
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-10-18 21:05:04 +0200

    💚 Fix

--- tests/test_transfer.py
+++ tests/test_transfer.py
@@ -19,7 +19,7 @@ def test_transfer():
     ).one()
     file.save()
 
-    assert file.species.get(name="human").id == 2
+    assert file.species.get(name="human").id == lb.settings.species.id
     assert file.ulabels.get(name="Tabula Sapiens").id == 1
     ulabel = file.ulabels.get(name="Tabula Sapiens")
     # mimic we have an existing ulabel with a different uid but same name
@@ -33,5 +33,12 @@ def test_transfer():
     ).one()
     file.save()
 
-    assert file.species.get(name="human").id == 2
+    assert file.species.get(name="human").id == lb.settings.species.id
     assert file.ulabels.get(name="Tabula Sapiens").id == 1
+
+    lb.Gene.filter().delete()
+    lb.Species.filter().delete()
+    ln.ULabel.filter().delete()
+    lb.Disease.filter().delete()
+    lb.CellLine.filter().delete()
+    ln.CellType.filter().delete()
