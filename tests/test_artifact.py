344801449a720eb66ab0997c873665baee905b40
Alex Wolf <f.alexander.wolf@gmail.com>
2023-12-12 22:11:52 +0100

    📝 Update tests

--- tests/test_artifact.py
+++ tests/test_artifact.py
@@ -474,7 +474,9 @@ def test_from_dir_single_artifact(get_test_filepaths, key):
 
 
 def test_from_dir_s3():
-    study0_data = ln.Artifact(f"s3://lamindb-dev-datasets/iris_studies/study0_raw_images")
+    study0_data = ln.Artifact(
+        f"s3://lamindb-dev-datasets/iris_studies/study0_raw_images"
+    )
     study0_data.hash = "d8_SjrP3V5tGetN8LQZC7w"
     study0_data.hash_type = "md5-d"
     study0_data.n_objects = 51
