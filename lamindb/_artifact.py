12f0dfddf0f8607558e384d83d6b04ed3c83e78f
Alex Wolf <f.alexander.wolf@gmail.com>
2023-12-13 07:52:20 +0100

    ♻️ Refactor

--- lamindb/_artifact.py
+++ lamindb/_artifact.py
@@ -1,5 +1,5 @@
 from pathlib import Path, PurePath, PurePosixPath
-from typing import Any, List, Optional, Tuple, Union
+from typing import Any, List, Optional, Tuple, Union, Dict
 
 import anndata as ad
 import fsspec
@@ -151,6 +151,66 @@ def process_data(
     return memory_rep, path, suffix, storage, use_existing_storage_key
 
 
+def get_stat_file_cloud(stat: Dict) -> Tuple[int, str, str]:
+    size = stat["size"]
+    # small files
+    if "-" not in stat["ETag"]:
+        # only store hash for non-multipart uploads
+        # we can't rapidly validate multi-part uploaded files client-side
+        # we can add more logic later down-the-road
+        hash = b16_to_b64(stat["ETag"])
+        hash_type = "md5"
+    else:
+        stripped_etag, suffix = stat["ETag"].split("-")
+        suffix = suffix.strip('"')
+        hash = f"{b16_to_b64(stripped_etag)}-{suffix}"
+        hash_type = "md5-n"  # this is the S3 chunk-hashing strategy
+    return size, hash, hash_type    
+
+
+def get_stat_dir_s3(path: UPath) -> Tuple[int, str, str, int]:
+    import boto3
+    from lamindb_setup.dev.upath import AWS_CREDENTIALS_PRESENT
+
+    if not AWS_CREDENTIALS_PRESENT:
+        # passing the following param directly to Session() doesn't
+        # work, unfortunately: botocore_session=path.fs.session
+        from botocore import UNSIGNED
+        from botocore.config import Config
+
+        config = Config(signature_version=UNSIGNED)
+        s3 = boto3.session.Session().resource("s3", config=config)
+    else:
+        s3 = boto3.session.Session().resource("s3")
+    bucket, key, _ = path.fs.split_path(path.as_posix())
+    # assuming this here is the fastest way of querying for many objects
+    objects = s3.Bucket(bucket).objects.filter(Prefix=key)
+    size = sum([object.size for object in objects])
+    md5s = [
+        # skip leading and trailing quotes
+        object.e_tag[1:-1]
+        for object in objects
+    ]
+    n_objects = len(md5s)
+    hash, hash_type = hash_md5s_from_dir(md5s)
+    return size, hash, hash_type, n_objects
+
+
+def get_stat_dir_gs(path: UPath) -> Tuple[int, str, str, int]:
+    import google.cloud.storage as gc_storage
+    bucket, key, _ = path.fs.split_path(path.as_posix())
+    # assuming this here is the fastest way of querying for many objects
+    client = gc_storage.Client.create_anonymous_client()
+    objects = client.Bucket(bucket).list_blobs(prefix=key)
+    sizes, md5s = [], []
+    for object in objects:
+        sizes.append(object.size)
+        md5s.append(object.md5_hash)
+    n_objects = len(md5s)
+    hash, hash_type = hash_md5s_from_dir(md5s)
+    return sum(sizes), hash, hash_type, n_objects
+
+
 def get_stat_or_artifact(
     path: UPath,
     suffix: str,
@@ -167,49 +227,17 @@ def get_stat_or_artifact(
     ):
         size = size_adata(memory_rep)
         return size, None, None, n_objects
-    stat = path.stat()
+    stat = path.stat()  # one network request
     if not isinstance(path, LocalPathClasses):
         if stat is not None:
-            if "ETag" in stat:
-                size = stat["size"]
-                # small files
-                if "-" not in stat["ETag"]:
-                    # only store hash for non-multipart uploads
-                    # we can't rapidly validate multi-part uploaded files client-side
-                    # we can add more logic later down-the-road
-                    hash = b16_to_b64(stat["ETag"])
-                    hash_type = "md5"
-                else:
-                    stripped_etag, suffix = stat["ETag"].split("-")
-                    suffix = suffix.strip('"')
-                    hash = f"{b16_to_b64(stripped_etag)}-{suffix}"
-                    hash_type = "md5-n"  # this is the S3 chunk-hashing strategy
-            else:
-                assert path.is_dir() and path.protocol == "s3"
-                import boto3
-                from lamindb_setup.dev.upath import AWS_CREDENTIALS_PRESENT
-
-                if not AWS_CREDENTIALS_PRESENT:
-                    # passing the following param directly to Session() doesn't
-                    # work, unfortunately: botocore_session=path.fs.session
-                    from botocore import UNSIGNED
-                    from botocore.config import Config
-
-                    config = Config(signature_version=UNSIGNED)
-                    s3 = boto3.session.Session().resource("s3", config=config)
-                else:
-                    s3 = boto3.session.Session().resource("s3")
-                bucket, key, _ = path.fs.split_path(path.as_posix())
-                # assuming this here is the fastest way of querying for many objects
-                objects = s3.Bucket(bucket).objects.filter(Prefix=key)
-                size = sum([object.size for object in objects])
-                md5s = [
-                    # skip leading and trailing quotes
-                    object.e_tag[1:-1]
-                    for object in objects
-                ]
-                n_objects = len(md5s)
-                hash, hash_type = hash_md5s_from_dir(md5s)
+            if "ETag" in stat:  # is file
+                size, hash, hash_type = get_stat_file_cloud(stat)
+            else:  # is directory
+                assert path.is_dir()  # try to remove this network request
+                if path.protocol == "s3":
+                    size, hash, hash_type, n_objects = get_stat_dir_s3(path)
+                elif path.protocol == "gs":
+                    size, hash, hash_type, n_objects = get_stat_dir_gs(path)
         else:
             logger.warning(f"did not add hash for {path}")
             return size, None, None, n_objects
