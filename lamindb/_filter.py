1f380e9521758d4fde13a9f973a5549f9fc8c621
Bernardo Penteado <bernardopbf3@gmail.com>
2023-12-20 10:21:05 -0300

    :sparkles: Enable to filter by latest version when relevant

--- lamindb/_filter.py
+++ lamindb/_filter.py
@@ -1,5 +1,6 @@
 from typing import Type
 
+from django.db.models import OuterRef, Subquery
 from lnschema_core import Artifact, Dataset, Registry
 from lnschema_core.types import VisibilityChoice
 
@@ -23,6 +24,37 @@ def filter(Registry: Type[Registry], **expressions) -> QuerySet:
                 expressions.pop(visibility)
     qs = QuerySet(model=Registry)
     if len(expressions) > 0:
-        return qs.filter(**expressions)
+        if expressions.get("version") == "latest" and hasattr(
+            qs.model, "initial_version"
+        ):
+            versionless_expressions = expressions.copy()
+            versionless_expressions.pop("version")
+            qs = qs.filter(**versionless_expressions)
+            qs = _filter_query_set_by_latest_version(qs)
+            return qs
+        else:
+            return qs.filter(**expressions)
     else:
         return qs
+
+
+def _filter_query_set_by_latest_version(query_set):
+    # Get records with the most recent created_at for each initial_version id
+    subquery = query_set.filter(initial_version=OuterRef("initial_version")).order_by(
+        "-created_at"
+    )
+    queryset1 = query_set.filter(
+        initial_version__isnull=False,
+        created_at=Subquery(subquery.values("created_at")[:1]),
+    )
+
+    # Get records where initial_version is null and exclude those records whose
+    # id is an initial_version in queryset2
+    queryset2 = query_set.filter(initial_version__isnull=True).exclude(
+        id__in=queryset1.values_list("initial_version", flat=True)
+    )
+
+    # Combine the querysets
+    final_queryset = queryset2.union(queryset1)
+
+    return final_queryset
