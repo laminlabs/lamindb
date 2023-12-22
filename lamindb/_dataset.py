737abd6cffff2d8fb506773ef1b3deb6a243188e
Alex Wolf <f.alexander.wolf@gmail.com>
2023-12-23 00:25:45 +0100

    🎨 Center versioning around stem_uid

--- lamindb/_dataset.py
+++ lamindb/_dataset.py
@@ -11,7 +11,7 @@ from lnschema_core.types import AnnDataLike, DataLike, FieldAttr, VisibilityChoi
 from lamindb._utils import attach_func_to_class_method
 from lamindb.dev._data import _track_run_input
 from lamindb.dev._mapped_dataset import MappedDataset
-from lamindb.dev.versioning import get_ids_from_old_version, init_uid
+from lamindb.dev.versioning import get_uid_from_old_version, init_uid
 
 from . import _TESTING, Artifact, Run
 from ._artifact import parse_feature_sets_from_anndata
@@ -57,9 +57,6 @@ def __init__(
     is_new_version_of: Optional[Dataset] = (
         kwargs.pop("is_new_version_of") if "is_new_version_of" in kwargs else None
     )
-    initial_version_id: Optional[int] = (
-        kwargs.pop("initial_version_id") if "initial_version_id" in kwargs else None
-    )
     version: Optional[str] = kwargs.pop("version") if "version" in kwargs else None
     visibility: Optional[int] = (
         kwargs.pop("visibility")
@@ -79,18 +76,11 @@ def __init__(
     else:
         if not isinstance(is_new_version_of, Dataset):
             raise TypeError("is_new_version_of has to be of type ln.Dataset")
-        provisional_uid, initial_version_id, version = get_ids_from_old_version(
+        provisional_uid, version = get_uid_from_old_version(
             is_new_version_of, version, n_full_id=20
         )
         if name is None:
             name = is_new_version_of.name
-    if version is not None:
-        if initial_version_id is None:
-            logger.info(
-                "initializing versioning for this dataset! create future versions of it"
-                " using ln.Dataset(..., is_new_version_of=old_dataset)"
-            )
-
     run = get_run(run)
     data_init_complete = False
     artifact = None
@@ -170,7 +160,6 @@ def __init__(
             hash=hash,
             run=run,
             version=version,
-            initial_version_id=initial_version_id,
             visibility=visibility,
             **kwargs,
         )
--- lamindb/dev/_run_context.py
+++ lamindb/dev/_run_context.py
@@ -13,7 +13,7 @@ from lnschema_core import Run, Transform, ids
 from lnschema_core.types import TransformType
 from lnschema_core.users import current_user_id
 
-from lamindb.dev.versioning import get_ids_from_old_version
+from lamindb.dev.versioning import get_uid_from_old_version
 
 from .hashing import to_b64_str
 
@@ -85,18 +85,18 @@ def update_notebook_metadata(
     uid_prefix = notebook.metadata["nbproject"]["id"]
     version = notebook.metadata["nbproject"]["version"]
 
-    updated, new_uid_prefix, new_version = update_transform_source_metadata(
+    updated, new_stem_uid, new_version = update_transform_source_metadata(
         notebook, _filepath, bump_version=bump_version, run_from_cli=False
     )
 
     if version != new_version:
         notebook.metadata["nbproject"]["version"] = new_version
-        new_uid, _, _ = get_ids_from_old_version(
-            is_new_version_of=transform, version=new_version, n_full_id=14
+        new_uid, _ = get_uid_from_old_version(
+            is_new_version_of=transform, version=new_version, n_full_id=16
         )
     else:
         notebook.metadata["nbproject"]["id"] = uid_prefix
-        new_uid = new_uid_prefix + ids.base62(n_char=2)
+        new_uid = new_stem_uid + ids.base62(n_char=4)
 
     # here we check that responses to both inputs (for new id and version) were not 'n'
     if updated:
@@ -123,20 +123,21 @@ def get_notebook_name_colab() -> str:
     return name.rstrip(".ipynb")
 
 
-def get_transform_kwargs_from_uid_prefix(
+def get_transform_kwargs_from_stem_uid(
     nbproject_id: str,
     nbproject_version: str,
-) -> Tuple[Optional[Transform], str, str, Optional[Transform]]:
-    id_ext = to_b64_str(hashlib.md5(nbproject_version.encode()).digest())[:2]
-    uid = nbproject_id + id_ext
-    version = nbproject_version
+) -> Tuple[Optional[Transform], str, str]:
+    from lamin_utils._base62 import encodebytes
+
+    # zero-pad the nbproject version such that the base62 encoding is at
+    # least 4 characters long
+    uid_ext = encodebytes(nbproject_version.zfill(4).encode())[:4]
+    new_uid = nbproject_id + uid_ext
+    assert len(new_uid) == 16
     transform = Transform.filter(
-        uid__startswith=nbproject_id, version=version
+        uid__startswith=nbproject_id, version=nbproject_version
     ).one_or_none()
-    old_version_of = None
-    if transform is None:
-        old_version_of = Transform.filter(uid__startswith=nbproject_id).first()
-    return transform, uid, version, old_version_of
+    return transform, new_uid, nbproject_version
 
 
 class run_context:
@@ -254,8 +255,7 @@ class run_context:
                         transform,
                         uid,
                         version,
-                        old_version_of,
-                    ) = get_transform_kwargs_from_uid_prefix(
+                    ) = get_transform_kwargs_from_stem_uid(
                         module.__transform_uid_prefix__,
                         module.__version__,  # type: ignore
                     )
@@ -265,7 +265,6 @@ class run_context:
                         version=version,
                         name=name,
                         reference=reference,
-                        is_new_version_of=old_version_of,
                         transform_type=TransformType.pipeline,
                         short_name=short_name,
                         is_interactive=False,
@@ -472,7 +471,6 @@ class run_context:
             transform = Transform.filter(uid=uid).one_or_none()
             name = filestem
             short_name = None
-            old_version_of = None
         # nbproject parsing successful
         elif nbproject_id is not None:
             name = nbproject_title
@@ -480,21 +478,18 @@ class run_context:
                 transform,
                 uid,
                 version,
-                old_version_of,
-            ) = get_transform_kwargs_from_uid_prefix(nbproject_id, nbproject_version)
+            ) = get_transform_kwargs_from_stem_uid(nbproject_id, nbproject_version)
             short_name = filestem
         cls._create_or_load_transform(
             uid=uid,
             version=version,
             name=name,
             reference=reference,
-            is_new_version_of=old_version_of,
             transform_type=TransformType.notebook,
             short_name=short_name,
             is_interactive=is_interactive,
             filepath=notebook_path,
             transform=transform,
-            metadata=metadata,
         )
 
     @classmethod
@@ -537,13 +532,11 @@ class run_context:
         version: Optional[str],
         name: str,
         reference: Optional[str],
-        is_new_version_of: Optional[Transform],
         short_name: Optional[str],
         transform_type: TransformType,
         is_interactive: bool,
         filepath: str,
         transform: Optional[Transform] = None,
-        metadata: Optional[Dict] = None,
     ) -> bool:
         # make a new transform record
         if transform is None:
@@ -553,7 +546,6 @@ class run_context:
                 name=name,
                 short_name=short_name,
                 reference=reference,
-                is_new_version_of=is_new_version_of,
                 type=transform_type,
             )
             transform.save()
