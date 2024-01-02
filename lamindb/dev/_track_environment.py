4e25fd6f3c8f8518be3057bc40799b84ac9bc652
Alex Wolf <f.alexander.wolf@gmail.com>
2024-01-02 18:44:34 +0100

    ♻️ Except memory error

--- lamindb/dev/_track_environment.py
+++ lamindb/dev/_track_environment.py
@@ -9,6 +9,10 @@ def track_environment(run: Run) -> None:
     filepath = ln_setup.settings.storage.cache_dir / f"run_env_pip_{run.uid}"
     # create a requirements.txt
     # we don't create a conda environment.yml mostly for its slowness
-    result = subprocess.run(f"pip freeze > {str(filepath)}", shell=True)
-    if result.returncode == 0:
+    try:
+        result = subprocess.run(f"pip freeze > {str(filepath)}", shell=True)
+    except OSError as e:
+        result = None
+        logger.warning(f"could not run pip freeze with error {e}")
+    if result is not None and result.returncode == 0:
         logger.info(f"tracked pip freeze > {str(filepath)}")
