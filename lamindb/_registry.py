3cdc46f32def2f75a8b1bf6586ce116de936996e
Alex Wolf <f.alexander.wolf@gmail.com>
2024-01-27 09:46:58 +0100

    💚 Fix import

--- lamindb/_registry.py
+++ lamindb/_registry.py
@@ -15,11 +15,11 @@ from lamindb_setup._init_instance import InstanceSettings
 from lamindb_setup._load_instance import get_owner_name_from_identifier
 from lamindb_setup.dev._docs import doc_args
 from lamindb_setup.dev._hub_core import load_instance
+from lamindb_setup.dev._settings_storage import StorageSettings
 from lnschema_core import Registry
 from lnschema_core.types import ListLike, StrField
 
 from lamindb._utils import attach_func_to_class_method
-from lamindb.dev._settings_storage import StorageSettings
 
 from . import _TESTING
 from ._from_values import get_or_create_records
