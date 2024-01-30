89e9496e9613d57d8e428e657aeba4fd15f9d7cc
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-01-30 17:38:59 +0800

    💚 Fix

--- noxfile.py
+++ noxfile.py
@@ -78,7 +78,7 @@ def install(session, group):
         if "bionty" in extras:
             session.run(*"pip install --no-deps ./sub/bionty".split())
             session.run(*"pip install --no-deps ./sub/lnschema-bionty".split())
-            session.run(*"pip install --no-deps ./sub/bionty-base".split())
+            session.run(*"pip install --no-deps bionty-base".split())
     session.run(*f"pip install -e .[dev,{extras}]".split())
 
 
