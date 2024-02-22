d42015dd3f39053a237446d9bc815aca5a4fe0ce
Koncopd <koncopd@gmail.com>
2024-02-22 14:16:37 +0100

    try improving parallel

--- lamindb/dev/_mapped_collection.py
+++ lamindb/dev/_mapped_collection.py
@@ -69,6 +69,7 @@ class MappedCollection:
         self.storages = []  # type: ignore
         self.conns = []  # type: ignore
         self.parallel = parallel
+        self._path_list = path_list
         self._make_connections(path_list, parallel)
 
         self.n_obs_list = []
@@ -102,9 +103,11 @@ class MappedCollection:
         self._dtype = dtype
         self._closed = False
 
-    def _make_connections(self, path_list: list, parallel: bool):
+    def _make_connections(
+        self, path_list: list, parallel: bool, skip_instance_cache: bool = False
+    ):
         for path in path_list:
-            path = UPath(path)
+            path = UPath(path, skip_instance_cache=skip_instance_cache)
             if path.exists() and path.is_file():  # type: ignore
                 if parallel:
                     conn, storage = None, path
@@ -368,3 +371,13 @@ class MappedCollection:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
+
+    @staticmethod
+    def torch_worker_init_fn(worker_id):
+        from torch.utils.data import get_worker_info
+
+        mapped = get_worker_info().dataset
+        mapped.parallel = False
+        mapped._make_connections(
+            mapped._path_list, parallel=False, skip_instance_cache=True
+        )
