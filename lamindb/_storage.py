d7ad21fb4a6160313cd3eeac97282cfdf235b3ee
Alex Wolf <f.alexander.wolf@gmail.com>
2024-02-29 21:33:17 +0100

    ♻️ Add ability to pass through access_token (#1468)

--- lamindb/_storage.py
+++ lamindb/_storage.py
@@ -4,14 +4,16 @@ from lnschema_core import Storage
 
 
 def root_as_path(self) -> UPath:
-    return create_path(self.root)
+    access_token = self._access_token if hasattr(self, "_access_token") else None
+    return create_path(self.root, access_token=access_token)
 
 
 @property  # type: ignore
 @doc_args(Storage.path.__doc__)
 def path(self) -> UPath:
     """{}."""
-    return create_path(self.root)
+    access_token = self._access_token if hasattr(self, "_access_token") else None
+    return create_path(self.root, access_token=access_token)
 
 
 Storage.root_as_path = root_as_path
