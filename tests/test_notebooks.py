3ac9e4404179d51b0bd44b1d98788a567c590b15
Alex Wolf <f.alexander.wolf@gmail.com>
2024-03-03 17:33:16 +0100

    ✅ Add missing tests

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -7,3 +7,11 @@ notebook_dir = Path(__file__).parent / "notebooks/"
 
 def test_no_title():
     nbproject_test.execute_notebooks(notebook_dir / "no-title.ipynb")
+
+
+def test_not_initialized():
+    nbproject_test.execute_notebooks(notebook_dir / "not-initialized.ipynb")
+
+
+def test_initialized_with_nbproject():
+    nbproject_test.execute_notebooks(notebook_dir / "initialized-with-nbproject.ipynb")
