51c22cc40a29610369fb52236063cc4c8ebe43bc
Alex Wolf <f.alexander.wolf@gmail.com>
2024-03-05 11:25:14 +0100

    💚 Try to fix

--- lamindb/setup/__init__.py
+++ lamindb/setup/__init__.py
@@ -2,7 +2,7 @@ import lamindb_setup as _lamindb_setup
 from lamindb_setup import *  # noqa: F403
 from lamindb_setup import settings
 
-from . import dev
+from . import core
 
 __doc__ = _lamindb_setup.__doc__.replace("lamindb_setup", "lamindb.setup")
 settings.__doc__ = settings.__doc__.replace("lamindb_setup", "lamindb.setup")
