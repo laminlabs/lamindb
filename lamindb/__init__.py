ae96fe5a61d7f15e8c36e1ef2dae368ad3746d2e
Alex Wolf <f.alexander.wolf@gmail.com>
2024-03-18 00:02:10 +0100

    🔖 Release 0.69.0

--- lamindb/__init__.py
+++ lamindb/__init__.py
@@ -56,7 +56,7 @@ Modules & settings:
 
 """
 
-__version__ = "0.68.2"  # denote a release candidate for 0.1.0 with 0.1rc1
+__version__ = "0.69.0"  # denote a release candidate for 0.1.0 with 0.1rc1
 
 import os as _os
 
