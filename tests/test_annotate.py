bdedcf0b5a36464ebbc5ba3e0546a498a8f598b3
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-03-31 22:41:12 +0200

    💚 Fix tests

--- tests/test_annotate.py
+++ tests/test_annotate.py
@@ -63,8 +63,8 @@ def test_annotator(df, categoricals):
     df["cell_type"] = df["cell_type"].replace(
         {"cerebral pyramidal neuron": cell_types.cerebral_cortex_pyramidal_neuron.name}
     )
-    annotate.add_validated("all")
-    annotate.add_new("donor")
+    annotate.add_validated_from("all")
+    annotate.add_new_from("donor")
     validated = annotate.validate()
     assert validated is True
 
