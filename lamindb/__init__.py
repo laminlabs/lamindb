eb05b2be7f4fc9f3d7607760588c9148ef819b39
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-04-02 12:26:10 +0200

    🔖 Release 0.69.6

--- lamindb/__init__.py
+++ lamindb/__init__.py
@@ -40,7 +40,7 @@ Modules & settings:
 
 """
 
-__version__ = "0.69.5"  # denote a release candidate for 0.1.0 with 0.1rc1
+__version__ = "0.69.6"  # denote a release candidate for 0.1.0 with 0.1rc1
 
 import os as _os
 
