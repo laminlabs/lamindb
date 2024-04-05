8d29a7136ac17216ba72728b48587476cecd0984
Alex Wolf <f.alexander.wolf@gmail.com>
2024-04-05 15:44:03 +0200

    💚 Fix

--- lamindb/core/storage/file.py
+++ lamindb/core/storage/file.py
@@ -136,10 +136,10 @@ def delete_storage_using_key(
 
 def delete_storage(storagepath: Path):
     """Delete arbitrary artifact."""
-    from lamindb._artifact import check_path_is_child_of_root
-
-    # is_relative_to is not available in 3.8 and deprecated since 3.12
-    if not check_path_is_child_of_root(storagepath, settings.storage):
+    # TODO is_relative_to is not available in 3.8 and deprecated since 3.12
+    # replace with check_path_is_child_of_root but this needs to first be debugged
+    # if not check_path_is_child_of_root(storagepath, settings.storage):
+    if not storagepath.is_relative_to(settings.storage):  # type: ignore
         logger.warning("couldn't delete files outside of default storage")
         return "did-not-delete"
     # only delete files in the default storage
