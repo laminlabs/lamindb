f8f3fb99f53204de2b5494d51c20c8e83f35c320
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-04-10 14:05:38 +0200

    💚 Fix

--- noxfile.py
+++ noxfile.py
@@ -74,12 +74,12 @@ def install(session, group):
         session.run(*"uv pip install --system --no-deps wetlab".split())
     elif group == "docs":
         extras += "bionty"
+        session.run(*"uv pip install --system mudata".split())
     elif group == "cli":
         extras += "jupyter,aws,bionty"
     if os.getenv("GITHUB_EVENT_NAME") != "push" and "bionty" in extras:
         session.run(*"uv pip install --system --no-deps ./sub/bionty".split())
         session.run(*"uv pip install --system --no-deps ./sub/lnschema-bionty".split())
-        session.run(*"uv pip install mudata".split())
     session.run(*f"uv pip install --system -e .[dev,{extras}]".split())
 
 
