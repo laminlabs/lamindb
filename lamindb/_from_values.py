46ad78658643db62a5e9abb4f27b0ca3d3c2b84e
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-04-12 11:52:49 +0200

    🎨 Polish from_values

--- lamindb/_from_values.py
+++ lamindb/_from_values.py
@@ -26,9 +26,10 @@ def get_or_create_records(
     upon_create_search_names = settings.upon_create_search_names
     settings.upon_create_search_names = False
     feature: Feature = None
+    organism = _get_organism_record(field, organism)
     kwargs: dict = {}
     if organism is not None:
-        kwargs["organism"] = _get_organism_record(field, organism)
+        kwargs["organism"] = organism
     if public_source is not None:
         kwargs["public_source"] = public_source
     try:
