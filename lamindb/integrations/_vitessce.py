6d1b40d25e2ae7f5f3742d714a7922a1d8588a11
Alex Wolf <f.alexander.wolf@gmail.com>
2024-04-19 11:34:37 +0200

    ♻️ Refactor Vitessce integration

--- lamindb/integrations/_vitessce.py
+++ lamindb/integrations/_vitessce.py
@@ -20,31 +20,18 @@ def save_vitessce_config(vitessce_config: VitessceConfig, description: str) -> A
     Args:
         vitessce_config (``VitessceConfig``): A VitessceConfig object.
         description: A description for the artifact.
-    """
-    # can't assume vitessce is installed
-    from vitessce import VitessceConfig
 
-    # create a local _data export_ in a folder
-    timestamp = datetime.now(timezone.utc).isoformat().split(".")[0]
-    vitesse_export_folder = f"./vitessce_export_{timestamp}.vitessce"
-    vitessce_config.export(to="files", base_url="", out_dir=vitesse_export_folder)
-    logger.important(f"local export: {vitesse_export_folder}")
-    # create an artifact and store the local export in th cloud
-    artifact = Artifact(vitesse_export_folder, description=description)
-    artifact.save()
+    .. versionchanged:: 0.70.2
+        This function no longer saves the dataset. It only saves the VitessceConfig object.
+    """
     # create a JSON export that points to the data in the cloud
-    config_dict = vitessce_config.to_dict(base_url=artifact.path.to_url())
-    logger.important(f"base url: {artifact.path.to_url()}")
-    # manually place that JSON export into the local data export folder
-    config_filename = "vitessce_config.json"
-    config_file_local_path = f"{vitesse_export_folder}/{config_filename}"
+    config_file_local_path = (
+        ln_setup.settings.storage.cache_dir / "config.vitessce.json"
+    )
     with open(config_file_local_path, "w") as file:
-        json.dump(config_dict, file)
-    # manually place that JSON export into the previously registered artifact folder
-    config_file_path = artifact.path / config_filename
-    config_file_path.upload_from(config_file_local_path)
-    # log the the URLs
-    logger.important(f"config url: {config_file_path.to_url()}")
+        json.dump(vitessce_config.to_dict(), file)
+    artifact = Artifact(config_file_local_path, description=description)
+    artifact.save()
     slug = ln_setup.settings.instance.slug
     logger.important(f"go to: https://lamin.ai/{slug}/artifact/{artifact.uid}")
     return artifact
