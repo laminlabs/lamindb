8f21aad6c88d9b253071a999cdab544190e7ea97
Alex Wolf <f.alexander.wolf@gmail.com>
2024-05-29 18:23:16 +0200

    🚧 Debug docs

--- noxfile.py
+++ noxfile.py
@@ -142,15 +142,15 @@ def build(session, group):
 @nox.session
 def docs(session):
     # move artifacts into right place
-    for group in ["tutorial", "guide", "biology", "faq", "storage"]:
-        if Path(f"./docs-{group}").exists():
-            if Path(f"./docs/{group}").exists():
-                shutil.rmtree(f"./docs/{group}")
-            Path(f"./docs-{group}").rename(f"./docs/{group}")
-        # move back to root level
-        if group in {"tutorial", "guide", "biology"}:
-            for path in Path(f"./docs/{group}").glob("*"):
-                path.rename(f"./docs/{path.name}")
+    # for group in ["tutorial", "guide", "biology", "faq", "storage"]:
+    #     if Path(f"./docs-{group}").exists():
+    #         if Path(f"./docs/{group}").exists():
+    #             shutil.rmtree(f"./docs/{group}")
+    #         Path(f"./docs-{group}").rename(f"./docs/{group}")
+    #     # move back to root level
+    #     if group in {"tutorial", "guide", "biology"}:
+    #         for path in Path(f"./docs/{group}").glob("*"):
+    #             path.rename(f"./docs/{path.name}")
     run(session, "lamin init --storage ./docsbuild --schema bionty,wetlab")
 
     def generate_cli_docs():
