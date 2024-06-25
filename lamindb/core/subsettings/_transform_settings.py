6ccd4fbca68486e2ff8c3c4808d7e666c8ec188a
Alex Wolf <f.alexander.wolf@gmail.com>
2024-06-25 22:52:53 +0200

    ♻️ Add a name field to TransformSettings

--- lamindb/core/subsettings/_transform_settings.py
+++ lamindb/core/subsettings/_transform_settings.py
@@ -2,8 +2,20 @@ from __future__ import annotations
 
 
 class TransformSettings:
-    stem_uid: None | None = None
-    version: None | None = None
+    stem_uid: str | None = None
+    """Defines the version family of the transform.
+
+    For example, all notebooks of the same family have a uid that starts with
+    `"FPnfDtJz8qbE"`.
+
+    The full uids of the notebooks in this family are of form
+    `"{stem_uid}{suffix_uid}"` where the `suffix_uid` encodes the semantic
+    `version`.
+    """
+    version: str | None = None
+    """The version."""
+    name: str | None = None
+    """A name like a notebook or script title."""
 
 
 transform_settings = TransformSettings()
