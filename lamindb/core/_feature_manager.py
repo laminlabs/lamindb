76c11f4b5c64e27dfc29ddc4f5e86ead2a4c0d39
Alex Wolf <f.alexander.wolf@gmail.com>
2024-07-15 16:30:09 +0200

    🚸 Cleaner API (#1723)

--- lamindb/core/_feature_manager.py
+++ lamindb/core/_feature_manager.py
@@ -118,7 +118,9 @@ def get_feature_set_links(host: Artifact | Collection) -> QuerySet:
 
 def get_link_attr(link: LinkORM | type[LinkORM], data: HasFeatures) -> str:
     link_model_name = link.__class__.__name__
-    if link_model_name == "ModelBase":  # we passed the type of the link
+    if (
+        link_model_name == "ModelBase" or link_model_name == "RegistryMeta"
+    ):  # we passed the type of the link
         link_model_name = link.__name__
     link_attr = link_model_name.replace(data.__class__.__name__, "")
     if link_attr == "ExperimentalFactor":
@@ -589,7 +591,7 @@ def _add_values(
         links = [
             LinkORM(
                 **{
-                    f"{self._host.__get_name_with_schema__().lower()}_id": self._host.id,
+                    f"{self._host.__class__.__get_name_with_schema__().lower()}_id": self._host.id,
                     valuefield_id: feature_value.id,
                 }
             )
