608f40ead17e5126c017ab534d56576ff8f8bab1
Sergei Rybakov <koncopd@gmail.com>
2024-07-15 18:49:35 +0200

    ✨ Artifact.backed() for tiledbsoma stores (#1728)

--- lamindb/core/storage/_backed_access.py
+++ lamindb/core/storage/_backed_access.py
@@ -28,6 +28,8 @@ if TYPE_CHECKING:
     from pathlib import Path
 
     from fsspec.core import OpenFile
+    from tiledbsoma import Collection as SOMACollection
+    from tiledbsoma import Experiment as SOMAExperiment
 
 anndata_version_parse = version.parse(anndata_version)
 
@@ -734,24 +736,62 @@ class BackedAccessor:
 
 def backed_access(
     artifact_or_filepath: Artifact | Path, using_key: str | None = None
-) -> AnnDataAccessor | BackedAccessor:
+) -> AnnDataAccessor | BackedAccessor | SOMACollection | SOMAExperiment:
     if isinstance(artifact_or_filepath, Artifact):
         filepath = filepath_from_artifact(artifact_or_filepath, using_key=using_key)
     else:
         filepath = artifact_or_filepath
     name = filepath.name
+    suffix = filepath.suffix
 
-    if filepath.suffix in (".h5", ".hdf5", ".h5ad"):
+    if name == "soma" or suffix == ".tiledbsoma":
+        try:
+            import tiledbsoma as soma
+        except ImportError as e:
+            raise ImportError(
+                "Please install tiledbsoma: pip install tiledbsoma"
+            ) from e
+        filepath_str = filepath.as_posix()
+        if filepath.protocol == "s3":
+            from lamindb_setup.core._settings_storage import get_storage_region
+
+            region = get_storage_region(filepath_str)
+            tiledb_config = {"vfs.s3.region": region}
+            storage_options = filepath.storage_options
+            if "key" in storage_options:
+                tiledb_config["vfs.s3.aws_access_key_id"] = storage_options["key"]
+            if "secret" in storage_options:
+                tiledb_config["vfs.s3.aws_secret_access_key"] = storage_options[
+                    "secret"
+                ]
+            if "token" in storage_options:
+                tiledb_config["vfs.s3.aws_session_token"] = storage_options["token"]
+            ctx = soma.SOMATileDBContext(tiledb_config=tiledb_config)
+            # this is a strange bug
+            # for some reason iterdir futher gives incorrect results
+            # if cache is not invalidated
+            # instead of obs and ms it gives ms and ms in the list of names
+            filepath.fs.invalidate_cache()
+        else:
+            ctx = None
+
+        soma_objects = [obj.name for obj in filepath.iterdir()]
+        if "obs" in soma_objects and "ms" in soma_objects:
+            SOMAType = soma.Experiment
+        else:
+            SOMAType = soma.Collection
+        return SOMAType.open(filepath_str, context=ctx)
+    elif suffix in {".h5", ".hdf5", ".h5ad"}:
         conn, storage = registry.open("h5py", filepath)
-    elif filepath.suffix == ".zarr":
+    elif suffix == ".zarr":
         conn, storage = registry.open("zarr", filepath)
     else:
         raise ValueError(
-            "object should have .h5, .hdf5, .h5ad, .zarr suffix, not"
-            f" {filepath.suffix}."
+            "object should have .h5, .hdf5, .h5ad, .zarr, .tiledbsoma suffix, not"
+            f" {suffix}."
         )
 
-    if filepath.suffix == ".h5ad":
+    if suffix == ".h5ad":
         return AnnDataAccessor(conn, storage, name)
     else:
         if get_spec(storage).encoding_type == "anndata":
