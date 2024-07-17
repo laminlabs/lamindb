163012a9fa0706e8262929375734e6ee1a2e27ff
Alex Wolf <f.alexander.wolf@gmail.com>
2024-07-17 10:09:38 +0200

    ⏪️ Bring back commit hash integrity check

--- lamindb/core/_sync_git.py
+++ lamindb/core/_sync_git.py
@@ -65,6 +65,9 @@ def get_git_commit_hash(blob_hash: str, repo_dir: Path | None = None) -> str | N
     if commit_hash == "" or result.returncode == 1:
         return None
     else:
+        assert (  # noqa: S101
+            len(commit_hash) == 40
+        ), f"commit hash |{commit_hash}| is not 40 characters long"
         return commit_hash
 
 
