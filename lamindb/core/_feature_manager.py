5520d1dc94238cb7d927cfe87c8366afbc1f6687
Alex Wolf <f.alexander.wolf@gmail.com>
2024-08-05 14:59:00 +0200

    💚 Fix

--- lamindb/core/_feature_manager.py
+++ lamindb/core/_feature_manager.py
@@ -118,9 +118,7 @@ def get_feature_set_links(host: Artifact | Collection) -> QuerySet:
 
 def get_link_attr(link: LinkORM | type[LinkORM], data: HasFeatures) -> str:
     link_model_name = link.__class__.__name__
-    if (
-        link_model_name == "ModelBase" or link_model_name == "RecordMeta"
-    ):  # we passed the type of the link
+    if link_model_name in {"Registry", "ModelBase"}:  # we passed the type of the link
         link_model_name = link.__name__
     return link_model_name.replace(data.__class__.__name__, "").lower()
 
