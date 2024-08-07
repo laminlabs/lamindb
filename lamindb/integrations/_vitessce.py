20adca8913350b4ee0f329c499da6fa453a47c68
Alex Wolf <f.alexander.wolf@gmail.com>
2024-08-07 18:21:08 +0200

    🎨 Add config.vitessce.json as an action to a dataset

--- lamindb/integrations/_vitessce.py
+++ lamindb/integrations/_vitessce.py
@@ -30,46 +30,57 @@ def save_vitessce_config(vitessce_config: VitessceConfig, description: str) -> A
     """
     vc_dict = vitessce_config.to_dict()
     # validate
-    datasets = vc_dict["datasets"]
-    input_artifacts = []
-    for dataset in datasets:
-        if "files" not in dataset:
-            raise ValueError("Each dataset must have a 'files' key.")
-        for file in dataset["files"]:
+    dataset_artifacts = []
+    for vitessce_dataset in vc_dict["datasets"]:
+        if "files" not in vitessce_dataset:
+            raise ValueError("Each vitessce_dataset must have a 'files' key.")
+        for file in vitessce_dataset["files"]:
             if "url" not in file:
                 raise ValueError("Each file must have a 'url' key.")
-            filename = file["url"].split("/")[-1]
-            if not filename.endswith((".anndata.zarr", ".zarr", ".ome.zarr")):
+            s3_path_last_element = file["url"].split("/")[-1]
+            if not s3_path_last_element.endswith(
+                (".anndata.zarr", ".zarr", ".ome.zarr")
+            ):
                 logger.warning(
                     "filename should end with '.anndata.zarr', '.zarr', or '.ome.zarr'."
                 )
-            filestem = (
-                filename.replace(".anndata.zarr", "")
-                .replace(".spatialdata.zarr", "")
+            stem_uid = (
+                s3_path_last_element.replace(".anndata.zarr", "")
                 .replace(".ome.zarr", "")
+                .replace(".zarr", "")  # needs to come last
             )
-            artifact = Artifact.filter(uid__startswith=filestem).one_or_none()
+            assert "." not in stem_uid  # noqa: S101 successfully stripped suffix
+            artifact = Artifact.filter(uid__startswith=stem_uid).one_or_none()
             if artifact is None:
                 logger.warning(
-                    f"could not find dataset '{filestem}' in lamindb: {dataset}"
+                    f"could not find dataset '{stem_uid}' in lamindb: {vitessce_dataset}"
                 )
             else:
-                input_artifacts.append(artifact)
+                dataset_artifacts.append(artifact)
     # link inputs
     with logger.mute():
         transform = Transform(name="save_vitessce_config", type="function", version="1")
         transform.save()
     run = Run(transform=transform)
     run.save()
-    run.input_artifacts.set(input_artifacts)
+    if len(dataset_artifacts) > 1:
+        # if we have more datasets, we should create a collection
+        # and attach an action to the collection
+        raise NotImplementedError
+    run.dataset_artifacts.set(dataset_artifacts)
     # create a JSON export
     config_file_local_path = (
         ln_setup.settings.storage.cache_dir / "config.vitessce.json"
     )
     with open(config_file_local_path, "w") as file:
         json.dump(vc_dict, file)
-    artifact = Artifact(config_file_local_path, description=description, run=run)
-    artifact.save()
+    vitessce_config_artifact = Artifact(
+        config_file_local_path, description=description, run=run
+    ).save()
+    # we have one and only one dataset artifact, hence the following line is OK
+    dataset_artifacts[0]._actions.add(vitessce_config_artifact)
     slug = ln_setup.settings.instance.slug
-    logger.important(f"go to: https://lamin.ai/{slug}/artifact/{artifact.uid}")
-    return artifact
+    logger.important(
+        f"go to: https://lamin.ai/{slug}/artifact/{vitessce_config_artifact.uid}"
+    )
+    return vitessce_config_artifact
