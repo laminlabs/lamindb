608b4e6c5ca0305a3cdae968419c9bfca577d138
Alex Wolf <f.alexander.wolf@gmail.com>
2024-08-08 14:32:27 +0200

    💚 Fix

--- lamindb/core/_label_manager.py
+++ lamindb/core/_label_manager.py
@@ -40,6 +40,7 @@ def get_labels_as_dict(self: HasFeatures, links: bool = False):
         "previous_runs",
         "_feature_values",
         "_action_targets",
+        "_lnschema_core_collection__actions_+",  # something seems off with this one
         "_actions",
     }
     labels = {}  # type: ignore
