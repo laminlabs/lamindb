e5ed4910bb2291bbca1c080b56de1f2c38bd55e8
Koncopd <koncopd@gmail.com>
2024-08-26 17:52:18 +0200

    add durations

--- noxfile.py
+++ noxfile.py
@@ -152,9 +152,9 @@ def build(session, group):
     run(session, "lamin set private-django-api true")
     coverage_args = "--cov=lamindb --cov-config=pyproject.toml --cov-append --cov-report=term-missing"
     if group == "unit-core":
-        run(session, f"pytest {coverage_args} ./tests/core")
+        run(session, f"pytest {coverage_args} ./tests/core --durations=200")
     elif group == "unit-storage":
-        run(session, f"pytest {coverage_args} ./tests/storage")
+        run(session, f"pytest {coverage_args} ./tests/storage --durations=200")
     elif group == "tutorial":
         run(session, "lamin logout")
         run(
