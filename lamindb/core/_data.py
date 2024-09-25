4a36473628abe5f04e5cbf5d435fa03480e0e2e9
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-09-25 16:21:06 +0200

    🎨 Update

--- lamindb/core/_data.py
+++ lamindb/core/_data.py
@@ -140,8 +140,7 @@ def describe(self: Artifact, print_types: bool = False):
         ]
         if isinstance(self, (Collection, Artifact)):
             many_to_many_fields.append("input_of_runs")
-        if isinstance(self, Artifact):
-            many_to_many_fields.append("feature_sets")
+
         self = (
             self.__class__.objects.using(self._state.db)
             .prefetch_related(*many_to_many_fields)
