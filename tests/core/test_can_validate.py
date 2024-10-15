a75db67d14247556747d4335f57e0029971b826d
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-10-15 15:52:50 +0200

    💚 Fix

--- tests/core/test_can_validate.py
+++ tests/core/test_can_validate.py
@@ -118,5 +118,5 @@ def test_set_abbr():
 
 
 def test_validate_int():
-    result = ln.User.validate([1, 2], field=bt.User.id)
+    result = ln.User.validate([1, 2], field=ln.User.id)
     assert result.sum() == 1
