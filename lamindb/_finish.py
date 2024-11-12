ea980195ae3c0cb08ed0fae57a7b376ac8a9ba58
zethson <lukas.heumos@posteo.net>
2024-11-12 20:31:52 +0100

    :art: Improve runtime formatting code
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- lamindb/_finish.py
+++ lamindb/_finish.py
@@ -233,10 +233,11 @@ def save_context_core(
 
     # finalize
     if not from_cli:
-        run_time = str(run.finished_at - run.started_at).split(".")[
-            0
-        ]  # Discard digits after seconds
-        formatted_run_time = f"{int(run_time.split(':')[0])}h {int(run_time.split(':')[1])}m {int(run_time.split(':')[2])}s"
+        run_time = run.finished_at - run.started_at
+        hours, remainder = divmod(run_time.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        formatted_run_time = f"{hours}h {minutes}m {seconds}s"
+
         logger.important(
             f"finished Run('{run.uid[:8]}') after {formatted_run_time} at {format_field_value(run.finished_at)}"
         )
