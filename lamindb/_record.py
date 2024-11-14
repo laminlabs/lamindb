4607883f87fddfcf0782a2402dd7b6dbffd2e2ff
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-11-14 22:38:53 +0100

    🎨 Do not make network requests

--- lamindb/_record.py
+++ lamindb/_record.py
@@ -176,9 +176,9 @@ def __init__(record: Record, *args, **kwargs):
         super(Record, record).__init__(**kwargs)
         # this will trigger validation against django validators
         if hasattr(record, "full_clean"):
-            record.full_clean(validate_unique=False)
+            record.full_clean(validate_unique=False, validate_constraints=False)
         else:
-            record._full__clean(validate_unique=False)
+            record._full__clean(validate_unique=False, validate_constraints=False)
     elif len(args) != len(record._meta.concrete_fields):
         raise ValueError("please provide keyword arguments, not plain arguments")
     else:
