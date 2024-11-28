fdc1f21b911f4ed1a876702ffa82db15002f716e
Alex Wolf <f.alexander.wolf@gmail.com>
2024-11-28 18:33:50 +0100

    💚 Final fix?

--- tests/core/test_collection.py
+++ tests/core/test_collection.py
@@ -144,7 +144,7 @@ def test_from_inconsistent_artifacts(df, adata):
 
 
 def test_from_consistent_artifacts(adata, adata2):
-    if feature := ln.Feature.filter(name="feat1").one_or_none() is not None:
+    if (feature := ln.Feature.filter(name="feat1").one_or_none()) is not None:
         feature.delete()
     ln.Feature(name="feat1", dtype="num").save()
     curator = ln.Curator.from_anndata(adata, var_index=bt.Gene.symbol, organism="human")
