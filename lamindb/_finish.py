48619409efec8637e93724fd36f9c2e46a37e789
Alex Wolf <f.alexander.wolf@gmail.com>
2024-12-16 18:06:46 +0100

    🚸 Improve feedback on not-up-to-date notebook content (#2284)

--- lamindb/_finish.py
+++ lamindb/_finish.py
@@ -18,8 +18,8 @@ if TYPE_CHECKING:
     from ._query_set import QuerySet
 
 
-def get_r_save_notebook_message() -> str:
-    return f"Please save the notebook in RStudio (shortcut `{get_shortcut()}`) within 2 sec before calling `db$finish()`"
+def get_save_notebook_message() -> str:
+    return f"Please save the notebook in your editor (shortcut `{get_shortcut()}`) within 2 sec before calling `finish()`"
 
 
 def get_shortcut() -> str:
@@ -114,7 +114,7 @@ def clean_r_notebook_html(file_path: Path) -> tuple[str | None, Path]:
         cleaned_content = re.sub(pattern_title, "", cleaned_content)
         cleaned_content = re.sub(pattern_h1, "", cleaned_content)
     cleaned_content = cleaned_content.replace(
-        f"NotebookNotSaved: {get_r_save_notebook_message()}", ""
+        f"NotebookNotSaved: {get_save_notebook_message()}", ""
     )
     cleaned_path = file_path.parent / (f"{file_path.stem}.cleaned{file_path.suffix}")
     cleaned_path.write_text(cleaned_content)
@@ -189,6 +189,10 @@ def save_context_core(
             logger.warning(
                 f"no {filepath.with_suffix('.nb.html')} found, save your manually rendered .html report via the CLI: lamin save {filepath}"
             )
+    if report_path is not None and not from_cli:
+        if get_seconds_since_modified(report_path) > 2 and not ln_setup._TESTING:
+            # this can happen when auto-knitting an html with RStudio
+            raise NotebookNotSaved(get_save_notebook_message())
     ln.settings.creation.artifact_silence_missing_run_warning = True
     # track source code
     hash, _ = hash_file(source_code_path)  # ignore hash_type for now
@@ -249,10 +253,6 @@ def save_context_core(
 
     # track report and set is_consecutive
     if report_path is not None:
-        if not from_cli:
-            if get_seconds_since_modified(report_path) > 2 and not ln_setup._TESTING:
-                # this can happen when auto-knitting an html with RStudio
-                raise NotebookNotSaved(get_r_save_notebook_message())
         if is_r_notebook:
             title_text, report_path = clean_r_notebook_html(report_path)
             if title_text is not None:
