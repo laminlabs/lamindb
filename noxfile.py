9bef4381593128147650b4c2cd2c115f32b8483a
zethson <lukas.heumos@posteo.net>
2024-12-17 12:11:52 +0100

    :art: Fix naming
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- noxfile.py
+++ noxfile.py
@@ -124,14 +124,14 @@ def install_ci(session, group):
         extras += "aws,zarr,bionty,jupyter"
         run(
             session,
-            "uv pip install --system --no-deps ./sub/lamin_spatial",
+            "uv pip install --system --no-deps ./sub/lamin-spatial",
         )
     elif group == "docs":
         extras += "bionty,zarr"
         run(session, "uv pip install --system mudata")
         run(
             session,
-            "uv pip install --system --no-deps ./sub/wetlab ./sub/findrefs ./sub/clinicore ./sub/omop ./sub/cellregistry ./sub/ourprojects .sub/lamin_spatial",
+            "uv pip install --system --no-deps ./sub/wetlab ./sub/findrefs ./sub/clinicore ./sub/omop ./sub/cellregistry ./sub/ourprojects .sub/lamin-spatial",
         )
     elif group == "cli":
         extras += "jupyter,aws,bionty"
@@ -199,7 +199,7 @@ def build(session, group):
     elif group == "storage":
         run(session, f"pytest -s {coverage_args} ./docs/storage")
     elif group == "spatial":
-        run(session, f"pytest {coverage_args} ./sub/lamin_spatial/tests --durations=50")
+        run(session, f"pytest {coverage_args} ./sub/lamin-spatial/tests --durations=50")
     elif group == "cli":
         run(session, f"pytest {coverage_args} ./sub/lamin-cli/tests --durations=50")
     # move artifacts into right place
