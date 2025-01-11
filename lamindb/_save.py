986632fe889169d5793b4c3f4a7ae8b8ac5f3150
Alex Wolf <f.alexander.wolf@gmail.com>
2025-01-11 13:46:00 +0100

    📝 Fix docs

--- lamindb/_save.py
+++ lamindb/_save.py
@@ -63,7 +63,7 @@ def save(records: Iterable[Record], ignore_conflicts: bool | None = False) -> No
         Update a single existing record:
 
         >>> transform = ln.Transform.get("0Cb86EZj")
-        >>> transform.description = "New name"
+        >>> transform.description = "New description"
         >>> transform.save()
 
     """
