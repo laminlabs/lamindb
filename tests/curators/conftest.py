c4225fc5ae52b35f94d6df8e82dbdf36a2eee6f8
Alex Wolf <f.alexander.wolf@gmail.com>
2025-01-13 22:01:10 +0100

    💚 Fix

--- tests/curators/conftest.py
+++ tests/curators/conftest.py
@@ -1,3 +1,5 @@
+import shutil
+
 import lamindb_setup as ln_setup
 import pytest
 
@@ -7,4 +9,5 @@ def pytest_sessionstart():
 
 
 def pytest_sessionfinish(session: pytest.Session):
+    shutil.rmtree("./testdb")
     ln_setup.delete("testdb", force=True)
