60d4ff42930d215aa102612f43cf30f0f009d881
Alex Wolf <f.alexander.wolf@gmail.com>
2025-01-20 13:59:55 +0800

    🔖 Release 1.0.2

--- lamindb/__init__.py
+++ lamindb/__init__.py
@@ -53,7 +53,7 @@ Modules and settings.
 """
 
 # denote a release candidate for 0.1.0 with 0.1rc1, 0.1a1, 0.1b1, etc.
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 from lamindb_setup._check_setup import InstanceNotSetupError as _InstanceNotSetupError
 from lamindb_setup._check_setup import _check_instance_setup
