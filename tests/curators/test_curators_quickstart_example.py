a2b826783cbc63b7ab7d81aefaefdc4c87aa311c
Alex Wolf <f.alexander.wolf@gmail.com>
2025-02-02 22:58:49 +0800

    ♻️ Prettify

--- tests/curators/test_curators_quickstart_example.py
+++ tests/curators/test_curators_quickstart_example.py
@@ -9,7 +9,7 @@ from lamindb.core import datasets
 
 
 @pytest.fixture(scope="module")
-def small_dataset1_features():
+def small_dataset1_schema():
     # Labels
     ln.ULabel.from_values(["DMSO", "IFNG"], create=True).save()
     ln.ULabel.from_values(
@@ -19,20 +19,37 @@ def small_dataset1_features():
 
     # Features
     # observation-level
-    ln.Feature(name="cell_medium", dtype="cat[ULabel]").save()
-    ln.Feature(name="sample_note", dtype="str").save()
-    ln.Feature(name="cell_type_by_expert", dtype="cat[bionty.CellType]").save()
-    ln.Feature(name="cell_type_by_model", dtype="cat[bionty.CellType]").save()
+    cell_medium = ln.Feature(name="cell_medium", dtype="cat[ULabel]").save()
+    sample_note = ln.Feature(name="sample_note", dtype="str").save()
+    cell_type_by_expert = ln.Feature(
+        name="cell_type_by_expert", dtype="cat[bionty.CellType]"
+    ).save()
+    cell_type_by_model = ln.Feature(
+        name="cell_type_by_model", dtype="cat[bionty.CellType]"
+    ).save()
     # dataset-level
     ln.Feature(name="temperature", dtype="float").save()
     ln.Feature(name="study", dtype="cat[ULabel]").save()
     ln.Feature(name="date_of_study", dtype="date").save()
     ln.Feature(name="study_note", dtype="str").save()
 
-    yield
+    # Schema
+    schema = ln.Schema(
+        name="small_dataset1",
+        otype="DataFrame",
+        features=[
+            cell_medium,
+            sample_note,
+            cell_type_by_expert,
+            cell_type_by_model,
+        ],
+        coerce_dtype=True,
+    ).save()
+
+    yield schema
 
     # Cleanup
-    ln.Schema.filter().delete()
+    schema.delete()
     ln.Feature.filter().delete()
     bt.Gene.filter().delete()
     ln.ULabel.filter().delete()
@@ -52,26 +69,11 @@ def curator_params():
     }
 
 
-def test_dataframe_curator(small_dataset1_features, curator_params):
+def test_dataframe_curator(small_dataset1_schema):
     """Test DataFrame curator implementation."""
 
-    features = ln.Feature.lookup()
-
-    # Schema
-    schema = ln.Schema(
-        name="small_dataset1",
-        otype="DataFrame",
-        features=[
-            features.cell_medium,
-            features.sample_note,
-            features.cell_type_by_expert,
-            features.cell_type_by_model,
-        ],
-        coerce_dtype=True,
-    ).save()
-
     df, _ = datasets.small_dataset1(format="df")
-    curator = ln.curators.DataFrameCurator(df, schema)
+    curator = ln.curators.DataFrameCurator(df, small_dataset1_schema)
     artifact = curator.save_artifact(key="example_datasets/dataset1.parquet")
 
     assert set(artifact.features.get_values()["cell_type_by_expert"]) == {
@@ -85,7 +87,7 @@ def test_dataframe_curator(small_dataset1_features, curator_params):
     artifact.delete(permanent=True)
 
 
-def test_anndata_curator(small_dataset1_features, curator_params):
+def test_anndata_curator(small_dataset1_schema, curator_params):
     """Test AnnData curator implementation."""
     adata = datasets.small_dataset1(format="anndata")
     curator = ln.Curator.from_anndata(adata, var_index=bt.Gene.symbol, **curator_params)
@@ -104,7 +106,7 @@ def test_anndata_curator(small_dataset1_features, curator_params):
     artifact.delete(permanent=True)
 
 
-def test_soma_curator(small_dataset1_features, curator_params):
+def test_soma_curator(small_dataset1_schema, curator_params):
     """Test SOMA curator implementation."""
     adata = datasets.small_dataset1(format="anndata")
     tiledbsoma.io.from_anndata(
