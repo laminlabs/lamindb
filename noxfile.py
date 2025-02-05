6bb9436d5439a8c39c4493a48e9c1852d8e6873c
Koncopd <koncopd@gmail.com>
2025-02-05 15:57:25 +0100

    point to main

--- noxfile.py
+++ noxfile.py
@@ -140,11 +140,6 @@ def install_ci(session, group):
     elif group == "cli":
         extras += "jupyter,bionty"
     run(session, f"uv pip install --system -e .[dev,{extras}]")
-
-    run(session, "uv pip install --system s3fs==2025.2.0")
-    run(session, "uv pip install --system gcsfs==2025.2.0")
-    run(session, "uv pip install --system universal_pathlib==0.2.6")
-
     # on the release branch, do not use submodules but run with pypi install
     # only exception is the docs group which should always use the submodule
     # to push docs fixes fast
