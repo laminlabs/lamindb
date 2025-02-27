dd07e936bf53d1aef9993f3552da1950f41bd93a
Koncopd <koncopd@gmail.com>
2025-02-27 16:59:57 +0100

    add hubmodule to schema

--- tests/permissions/scripts/setup_instance.py
+++ tests/permissions/scripts/setup_instance.py
@@ -3,9 +3,14 @@ from hubmodule._setup import _install_db_module
 from laminci.db import setup_local_test_postgres
 
 pgurl = setup_local_test_postgres()
+
 ln_setup.init(
     storage="./default_storage_permissions",
     name="lamindb-test-permissions",
     db=pgurl,
 )
+
 _install_db_module(pgurl)
+
+ln_setup.settings.instance._schema_str = "hubmodule"
+ln_setup.settings.instance._persist()
--- tests/permissions/test_permissions.py
+++ tests/permissions/test_permissions.py
@@ -7,3 +7,5 @@ jwt_db_url = _create_jwt_user(pgurl)
 
 def test_fine_grained_permissions():
     ln.connect("lamindb-test-permissions", _db=jwt_db_url)
+
+    assert ln.setup.settings.instance.modules == {"hubmodule"}
