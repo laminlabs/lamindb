562a37ce939580c0282e93534bed02b3f2193f26
Lukas Heumos <lukas.heumos@posteo.net>
2025-02-27 17:08:17 +0100

    🎨 Fix writes

--- lamindb/_artifact.py
+++ lamindb/_artifact.py
@@ -173,11 +173,7 @@ def process_data(
     if key is not None:
         key_suffix = extract_suffix_from_path(PurePosixPath(key), arg_name="key")
         # use suffix as the (adata) format if the format is not provided
-        if (
-            isinstance(data, (AnnData, SpatialData))
-            and format is None
-            and len(key_suffix) > 0
-        ):
+        if isinstance(data, AnnData) and format is None and len(key_suffix) > 0:
             format = key_suffix[1:]
     else:
         key_suffix = None
