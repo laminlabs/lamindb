528be13ffb9f2a36dab9bbb216ea74cf75448286
Koncopd <koncopd@gmail.com>
2025-02-28 12:46:46 +0100

    check

--- tests/permissions/test_permissions.py
+++ tests/permissions/test_permissions.py
@@ -1,14 +1,10 @@
 import lamindb as ln
-from utils import _set_token, _sign_jwt
+from utils import _sign_jwt
 
 pgurl = "postgresql://postgres:pwd@0.0.0.0:5432/pgtest"  # admin db connection url
 token = _sign_jwt(pgurl, {"account_id": ln.setup.settings.user._uuid.hex})
-_set_token(token)
+# _set_token(token)
 
 
 def test_fine_grained_permissions():
-    # that is just to check that everything was setup properly
-    # this doesn't check the real permissions functionality
-    assert ln.setup.settings.instance.modules == {"hubmodule"}
-
     assert ln.ULabel.filter().count() == 2
