c3496e12c6008b64706e02e37382ec5ce3c60689
Koncopd <koncopd@gmail.com>
2025-03-10 21:00:09 +0100

    fix and test

--- tests/core/test_collection.py
+++ tests/core/test_collection.py
@@ -293,7 +293,11 @@ def test_collection_mapped(adata, adata2):
     assert ls_ds.original_shapes[0] == (2, 3) and ls_ds.original_shapes[1] == (2, 3)
     ls_ds.close()
     # test with QuerySet
-    with collection.artifacts.all().mapped() as ls_ds:
+    with collection.artifacts.all().artifacts_mapped() as ls_ds:
+        assert ls_ds.shape == (4, 3)
+    with collection.artifacts.order_by("created_at").artifacts_mapped(
+        stream=True
+    ) as ls_ds:
         assert ls_ds.shape == (4, 3)
 
     with collection.mapped(obs_keys="feat1", stream=True) as ls_ds:
