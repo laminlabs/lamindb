8c03be5148e572202edf29cd46bc51f3643c2bce
Lukas Heumos <lukas.heumos@posteo.net>
2025-03-12 13:49:42 +0100

    🎨 Upgrade tiledbsoma

--- noxfile.py
+++ noxfile.py
@@ -127,7 +127,7 @@ def install_ci(session, group):
             session,
             "uv pip install --system -U spatialdata",
         )
-        run(session, "uv pip install --system tiledbsoma==1.15.0rc3")
+        run(session, "uv pip install --system tiledbsoma>=1.16.0")
     elif group == "docs":
         extras += "bionty,zarr"
         run(
