132064d87476fc3a910ce006be45fe0bf9b66fe2
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2025-03-17 14:53:09 +0100

    🔥 Move more tests

--- tests/core/test_has_parents.py
+++ tests/core/test_has_parents.py
@@ -39,42 +39,6 @@ def test_add_emoji():
     transform.delete()
 
 
-def test_add_ontology_from_df():
-    import bionty as bt
-
-    record = bt.Ethnicity.from_source(ontology_id="HANCESTRO:0005").save()
-    parent = bt.Ethnicity.get(ontology_id="HANCESTRO:0004")
-    assert parent in record.parents.list()
-    parent.delete()
-
-    bt.Ethnicity.import_source()
-    parent = bt.Ethnicity.get(ontology_id="HANCESTRO:0004")
-    assert parent in record.parents.list()
-    record = bt.Ethnicity.get("7RNCY3yC")
-    assert record.parents.all().one().name == "South East Asian"
-    # the source.in_db should be set to True since we imported all the records
-    assert record.source.in_db is True
-
-
-def test_add_ontology_from_values():
-    import bionty as bt
-
-    bt.Ethnicity.filter().delete()
-    ln.save(
-        bt.Ethnicity.from_values(
-            [
-                "HANCESTRO:0597",
-                "HANCESTRO:0006",
-            ],
-            field=bt.Ethnicity.ontology_id,
-        )
-    )
-    record = bt.Ethnicity.get("7RNCY3yC")
-    assert record.parents.all().one().name == "South East Asian"
-    # the source.in_db should be set back to False since we deleted all records
-    assert record.source.in_db is False
-
-
 def test_view_lineage_circular():
     import pandas as pd
 
