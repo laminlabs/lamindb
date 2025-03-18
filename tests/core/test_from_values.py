019ae7971a40421d7c07accfa1a9b40e5f3870a0
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2025-03-18 12:29:11 +0100

    🔥 Move organism tests into bionty

--- tests/core/test_from_values.py
+++ tests/core/test_from_values.py
@@ -45,37 +45,6 @@ def test_from_values_multiple_match():
     assert len(records) == 3
 
 
-def test_from_values_organism():
-    from bionty import Gene, settings
-    from bionty._bionty import OrganismNotSet
-
-    settings._organism = None
-    with pytest.raises(OrganismNotSet):
-        Gene.from_values(["ABC1"], Gene.symbol)
-    # no organism is needed if the values are ensembl gene ids
-    result = Gene.from_values(["ENSG00000068097"], Gene.ensembl_gene_id)
-    assert len(result) == 1
-    result = Gene.from_values(
-        ["ENSMUSG00000102862", "ENSMUSG00000084826"], field=Gene.ensembl_gene_id
-    )
-    assert len(result) == 2
-    result = Gene.from_values(
-        ["HRA1", "ETS1-1"], field=Gene.stable_id, organism="saccharomyces cerevisiae"
-    )
-    assert len(result) == 2
-
-    settings.organism = "human"
-    values = ["ABC1"]
-    standardized_values = Gene.public().standardize(values)
-    records = Gene.from_values(standardized_values, Gene.symbol)
-    assert records[0].ensembl_gene_id == "ENSG00000068097"
-
-    # TODO: Gene.public() should raise error if organism is not provided
-    standardized_values = Gene.public(organism="mouse").standardize(values)
-    records = Gene.from_values(standardized_values, Gene.symbol, organism="mouse")
-    assert records[0].ensembl_gene_id == "ENSMUSG00000015243"
-
-
 def test_get_or_create_records():
     names = ["ulabel" + str(i) for i in range(25)]
     labels = [ln.ULabel(name=name) for name in names]
