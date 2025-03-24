f355be3d92143a78b06cf2dd9bb1a7f5a8120b3a
Koncopd <koncopd@gmail.com>
2025-03-24 16:09:59 +0100

    fix

--- tests/permissions/scripts/setup_access.py
+++ tests/permissions/scripts/setup_access.py
@@ -3,6 +3,27 @@ import hubmodule.models as hm
 from hubmodule._setup import _install_db_module
 from laminhub_rest.core.db import DbRoleHandler
 
+# create a db connection url that works with RLS
+JWT_ROLE_NAME = "permissions_jwt"
+
+
+def create_jwt_user(dsn_admin: str, jwt_role_name: str):
+    db_role_handler = DbRoleHandler(dsn_admin)
+    jwt_db_url = db_role_handler.create(
+        jwt_role_name, expires_in=None, alter_if_exists=True
+    )
+    db_role_handler.permission.grant_write_jwt(jwt_role_name)
+    return jwt_db_url
+
+
+pgurl = "postgresql://postgres:pwd@0.0.0.0:5432/pgtest"  # admin db connection url
+jwt_db_url = create_jwt_user(pgurl, jwt_role_name=JWT_ROLE_NAME)
+_install_db_module(pgurl, jwt_role_name=JWT_ROLE_NAME)
+
+print("Created jwt db connection")
+
+# create models
+
 full_access = ln.models.Space(name="full access", uid="00000001").save()  # type: ignore
 select_access = ln.models.Space(name="select access", uid="00000002").save()  # type: ignore
 no_access = ln.models.Space(name="no access", uid="00000003").save()  # type: ignore
@@ -43,24 +64,7 @@ ulabel.projects.add(project)
 
 print("Created models")
 
+# save jwt db connection
 
-# create a db connection url that works with RLS
-JWT_ROLE_NAME = "permissions_jwt"
-
-
-def create_jwt_user(dsn_admin: str, jwt_role_name: str):
-    db_role_handler = DbRoleHandler(dsn_admin)
-    jwt_db_url = db_role_handler.create(
-        jwt_role_name, expires_in=None, alter_if_exists=True
-    )
-    db_role_handler.permission.grant_write_jwt(jwt_role_name)
-    return jwt_db_url
-
-
-pgurl = "postgresql://postgres:pwd@0.0.0.0:5432/pgtest"  # admin db connection url
-jwt_db_url = create_jwt_user(pgurl, jwt_role_name=JWT_ROLE_NAME)
-_install_db_module(pgurl, jwt_role_name=JWT_ROLE_NAME)
 ln.setup.settings.instance._db = jwt_db_url
 ln.setup.settings.instance._persist()
-
-print("Created jwt db connection")
