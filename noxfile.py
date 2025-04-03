a2adf76de8f403415976401492910418e5c3cd69
Sergei Rybakov <koncopd@gmail.com>
2025-04-03 13:26:12 +0200

    ⬆️ Upgrade fsspec packages to 2025.3.2  (#2631)

--- noxfile.py
+++ noxfile.py
@@ -146,12 +146,18 @@ def install_ci(session, group):
     elif group == "cli":
         extras += "jupyter,bionty"
     elif group == "permissions":
-        run(session, "uv pip install --system -e ./laminhub/rest-hub")
+        run(
+            session,
+            "uv pip install --system sentry_sdk line_profiler setuptools wheel==0.45.1 flit",
+        )
+        run(
+            session,
+            "uv pip install --system -e ./laminhub/rest-hub --no-build-isolation",
+        )
         run(
             session,
             "uv pip install --system -e ./laminhub/rest-hub/laminhub_rest/hubmodule",
         )
-        run(session, "uv pip install --system line_profiler")
 
     extras = "," + extras if extras != "" else extras
     run(session, f"uv pip install --system -e .[dev{extras}]")
