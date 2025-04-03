8e05ce9b91f23d4714fdd7439ee0d06e0ee340e6
Alex Wolf <f.alexander.wolf@gmail.com>
2025-04-03 07:48:23 +0200

    💡 More comments with explanations

--- tests/permissions/scripts/example_script.py
+++ tests/permissions/scripts/example_script.py
@@ -1,6 +1,5 @@
 import lamindb_setup as ln_setup
 
-AUTO_CONNECT = ln_setup.settings.auto_connect
 ln_setup.settings.auto_connect = False
 
 import lamindb as ln
@@ -9,16 +8,8 @@ assert ln.setup.settings.user.handle == "testuser1"
 ln.connect("laminlabs/lamin-dev")
 space_name = "Our test space for CI"
 ln.track(space=space_name)
-
 assert ln.context.space.name == space_name
-
 ulabel = ln.ULabel(name="My test ulabel in test space").save()
-
-print(ln.context.space.name)
-print(ulabel.space.name)
-
 assert ulabel.space.name == space_name
-
-ulabel.delete()
-
+ulabel.delete()  # delete silently passes in case another worker deleted the ulabel
 ln.context.transform.delete()
