9529948067926132ac9c3d8a7cc1bb05c8626a3b
Alex Wolf <f.alexander.wolf@gmail.com>
2025-04-14 14:42:05 +0200

    💚 Fix

--- lamindb/curators/__init__.py
+++ lamindb/curators/__init__.py
@@ -395,7 +395,7 @@ class DataFrameCurator(Curator):
         categoricals = []
         features = []
         feature_ids: set[int] = set()
-        if schema.itype is not None and isinstance(dataset, pd.DataFrame):
+        if schema.flexible and isinstance(dataset, pd.DataFrame):
             features += Feature.filter(name__in=self._dataset.keys()).list()
             feature_ids = {feature.id for feature in features}
         if schema.n > 0:
