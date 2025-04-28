182b4c172bdfd55f37401b7bdb2dcd0e838bd805
Koncopd <koncopd@gmail.com>
2025-04-28 19:12:32 +0200

    eliminate write_adata_zarr

--- lamindb/core/storage/_zarr.py
+++ lamindb/core/storage/_zarr.py
@@ -1,19 +1,14 @@
 from __future__ import annotations
 
-import warnings
 from typing import TYPE_CHECKING, Literal
 
-import scipy.sparse as sparse
 import zarr
 from anndata import __version__ as anndata_version
-from anndata._io.specs import write_elem
 from lamin_utils import logger
 from lamindb_setup.core.upath import LocalPathClasses, S3FSMap, UPath, create_mapper
 from packaging import version
 
 from lamindb.core._compat import with_package
-
-from ._anndata_sizes import _size_elem, _size_raw, size_adata
 
 if version.parse(anndata_version) < version.parse("0.11.0"):
     from anndata._io import read_zarr as read_anndata_zarr
@@ -25,12 +20,11 @@ if version.parse(zarr.__version__) >= version.parse("3.0.0a0"):
     from zarr.abc.store import Store
 else:
     IS_ZARR_V3 = False
+    from zarr.storage import Store  # noqa
 
 if TYPE_CHECKING:
-    from anndata import AnnData
     from fsspec import FSMap
     from lamindb_setup.core.types import UPathStr
-    from zarr.storage import Store
 
     from lamindb.core.types import ScverseDataStructures
 
@@ -126,71 +120,3 @@ def load_zarr(
                 "Unable to determine zarr store format and therefore cannot load Artifact."
             )
     return scverse_obj
-
-
-def write_adata_zarr(
-    adata: AnnData, storepath: UPathStr, callback=None, chunks=None, **dataset_kwargs
-) -> None:
-    store = get_zarr_store(storepath, create=True)
-    f = zarr.open(store, mode="w")
-
-    adata.strings_to_categoricals()
-    if adata.raw is not None:
-        adata.strings_to_categoricals(adata.raw.var)
-
-    f.attrs.setdefault("encoding-type", "anndata")
-    f.attrs.setdefault("encoding-version", "0.1.0")
-
-    adata_size = None
-    cumulative_val = 0
-
-    def _report_progress(key_write: str | None = None):
-        nonlocal adata_size
-        nonlocal cumulative_val
-
-        if callback is None:
-            return None
-        if adata_size is None:
-            adata_size = size_adata(adata)
-        if key_write is None:
-            # begin or finish
-            if cumulative_val < adata_size:
-                callback(adata_size, adata_size if cumulative_val > 0 else 0)
-            return None
-
-        elem = getattr(adata, key_write, None)
-        if elem is None:
-            return None
-        elem_size = _size_raw(elem) if key_write == "raw" else _size_elem(elem)
-        if elem_size == 0:
-            return None
-
-        cumulative_val += elem_size
-        callback(adata_size, cumulative_val)
-
-    def _write_elem_cb(f, k, elem, dataset_kwargs):
-        write_elem(f, k, elem, dataset_kwargs=dataset_kwargs)
-        _report_progress(k)
-
-    _report_progress(None)
-    with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", category=UserWarning, module="zarr")
-
-        if chunks is not None and not isinstance(adata.X, sparse.spmatrix):
-            _write_elem_cb(
-                f,
-                "X",
-                adata.X,
-                dataset_kwargs=dict(chunks=chunks, **dataset_kwargs),
-            )
-        else:
-            _write_elem_cb(f, "X", adata.X, dataset_kwargs=dataset_kwargs)
-        for elem in ("obs", "var"):
-            _write_elem_cb(f, elem, getattr(adata, elem), dataset_kwargs=dataset_kwargs)
-        for elem in ("obsm", "varm", "obsp", "varp", "layers", "uns"):
-            _write_elem_cb(
-                f, elem, dict(getattr(adata, elem)), dataset_kwargs=dataset_kwargs
-            )
-        _write_elem_cb(f, "raw", adata.raw, dataset_kwargs=dataset_kwargs)
-    # todo: fix size less than total at the end
-    _report_progress(None)
