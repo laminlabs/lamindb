b8be77376ea023a4d41854a4c000f6f4da7d932c
Alex Wolf <f.alexander.wolf@gmail.com>
2025-04-30 17:09:47 +0200

    💚 Fix

--- tests/core/test_notebooks.py
+++ tests/core/test_notebooks.py
@@ -13,10 +13,6 @@ def test_all_notebooks():
     env = os.environ
     env["LAMIN_TESTING"] = "true"
     nbproject_test.execute_notebooks(notebook_dir)
-    # re-run one of the notebooks to trigger hash lookup
-    nbproject_test.execute_notebooks(
-        notebook_dir / "with-title-initialized-consecutive-finish-not-last-cell.ipynb"
-    )
     nbproject_test.execute_notebooks(notebook_dir_duplicate)
     del env["LAMIN_TESTING"]
 
@@ -26,7 +22,7 @@ def test_run_after_rename_no_uid():
         notebook_dir / "with-title-initialized-consecutive-finish-not-last-cell.ipynb"
     )
     result = subprocess.run(  # noqa: S602
-        f"jupyter nbconvert --to notebook --inplace --execute {notebook_path}",
+        f"lamin save {notebook_path}",
         shell=True,
         capture_output=True,
     )
