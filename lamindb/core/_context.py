5ca3939a36f6a32795f51f1cb9ea7b25512bd291
Alex Wolf <f.alexander.wolf@gmail.com>
2025-05-01 13:21:07 +0200

    ♻️ Fix logging

--- lamindb/core/_context.py
+++ lamindb/core/_context.py
@@ -437,8 +437,8 @@ class Context:
             r_or_python = "."
             if self._path is not None:
                 r_or_python = "." if self._path.suffix in {".py", ".ipynb"} else "$"
-            project_str = f", project={project}" if project is not None else ""
-            space_str = f", space={space}" if space is not None else ""
+            project_str = f", project='{project}'" if project is not None else ""
+            space_str = f", space='{space}'" if space is not None else ""
             kwargs_str = f"{project_str}{space_str}"
             logger.important_hint(
                 f"recommendation: to identify the {notebook_or_script} across renames, pass the uid: ln{r_or_python}track('{self.transform.uid[:-4]}'{kwargs_str})"
