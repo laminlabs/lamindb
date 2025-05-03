6b5e54c7dce645c362b75b78339168d277e99a99
Alex Wolf <f.alexander.wolf@gmail.com>
2025-05-03 16:24:32 +0200

    💚 Fix

--- docs/scripts/curate_spatialdata.py
+++ docs/scripts/curate_spatialdata.py
@@ -1,6 +1,5 @@
 import lamindb as ln
 
-# curate a SpatialData
 spatialdata = ln.core.datasets.spatialdata_blobs()
 sdata_schema = ln.Schema.get(name="spatialdata_blobs_schema")
 curator = ln.curators.SpatialDataCurator(spatialdata, sdata_schema)
@@ -14,5 +13,5 @@ spatialdata.tables["table"].var.drop(index="ENSG00000999999", inplace=True)
 # validate again (must pass now) and save artifact
 artifact = ln.Artifact.from_spatialdata(
     spatialdata, key="examples/spatialdata1.zarr", schema=sdata_schema
-)
+).save()
 artifact.describe()
