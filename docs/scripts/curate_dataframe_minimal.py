6a32fab00268e1babf5f5b2ff41fcecd52a6db50
Alex Wolf <f.alexander.wolf@gmail.com>
2025-05-03 11:07:01 +0200

    💚 Fixes

--- docs/scripts/curate_dataframe_minimal.py
+++ docs/scripts/curate_dataframe_minimal.py
@@ -2,6 +2,7 @@ import lamindb as ln
 
 schema = ln.core.datasets.mini_immuno.define_mini_immuno_schema_flexible()
 df = ln.core.datasets.small_dataset1(otype="DataFrame")
-curator = ln.curators.DataFrameCurator(df, schema)
-artifact = curator.save_artifact(key="examples/dataset1.parquet")
-assert artifact.schema == schema
+artifact = ln.Artifact.from_df(
+    df, key="examples/dataset1.parquet", schema=schema
+).save()
+artifact.describe()
