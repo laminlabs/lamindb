a4e7a9b9ca8b63aaccffab5ad046f933286b737a
Koncopd <koncopd@gmail.com>
2025-05-05 18:31:33 +0200

    implement ArtifactQuerySet

--- lamindb/models/_artifact_query_set.py
+++ lamindb/models/_artifact_query_set.py
@@ -1,26 +1,53 @@
 from __future__ import annotations
 
+from collections.abc import Iterable, Iterator
 from typing import TYPE_CHECKING, Literal
+
+from lamin_utils import logger
+
+from ..core._mapped_collection import MappedCollection
+from ..core.storage._backed_access import _open_dataframe
 
 if TYPE_CHECKING:
     from collections.abc import Iterator
 
     from anndata import AnnData
+    from django.models import QuerySet
     from pandas import DataFrame
     from polars import LazyFrame as PolarsLazyFrame
     from pyarrow.dataset import Dataset as PyArrowDataset
+    from upath import UPath
 
-    from ..core._mapped_collection import MappedCollection
+
+def _check_ordered_artifacts(qs: QuerySet):
+    from ._artifact import Artifact
+
+    if qs.model != Artifact:
+        raise ValueError("A query set should consist of artifacts to be opened.")
+    if not qs.ordered:
+        logger.warning(
+            "this query set is unordered, consider using `.order_by()` first "
+            "to avoid opening the artifacts in an arbitrary order"
+        )
 
 
-class ArtifactQuerySet:
+class ArtifactQuerySet(Iterable):
     def load(
         self,
         join: Literal["inner", "outer"] = "outer",
         is_run_input: bool | None = None,
         **kwargs,
     ) -> DataFrame | AnnData:
-        pass
+        from .artifact import Artifact, _track_run_input
+        from .collection import _load_concat_artifacts
+
+        _check_ordered_artifacts(self)
+
+        artifacts: list[Artifact] = list(self)
+        concat_object = _load_concat_artifacts(artifacts, join, **kwargs)
+        # track only if successful
+        _track_run_input(artifacts, is_run_input)
+        return concat_object
 
     def open(
         self,
@@ -28,7 +55,17 @@ class ArtifactQuerySet:
         is_run_input: bool | None = None,
         **kwargs,
     ) -> PyArrowDataset | Iterator[PolarsLazyFrame]:
-        pass
+        from ._artifact import Artifact, _track_run_input
+
+        _check_ordered_artifacts(self)
+
+        artifacts: list[Artifact] = list(self)
+        paths: list[UPath] = [artifact.path for artifact in artifacts]
+
+        dataframe = _open_dataframe(paths, engine=engine, **kwargs)
+        # track only if successful
+        _track_run_input(artifacts, is_run_input)
+        return dataframe
 
     def mapped(
         self,
@@ -45,4 +82,34 @@ class ArtifactQuerySet:
         stream: bool = False,
         is_run_input: bool | None = None,
     ) -> MappedCollection:
-        pass
+        from ._artifact import Artifact, _track_run_input
+
+        _check_ordered_artifacts(self)
+
+        artifacts: list[Artifact] = []
+        paths: list[UPath] = []
+        for artifact in self:
+            if ".h5ad" not in artifact.suffix and ".zarr" not in artifact.suffix:
+                logger.warning(f"ignoring artifact with suffix {artifact.suffix}")
+                continue
+            elif not stream:
+                paths.append(artifact.cache())
+            else:
+                paths.append(artifact.path)
+            artifacts.append(artifact)
+        ds = MappedCollection(
+            paths,
+            layers_keys,
+            obs_keys,
+            obsm_keys,
+            obs_filter,
+            join,
+            encode_labels,
+            unknown_label,
+            cache_categories,
+            parallel,
+            dtype,
+        )
+        # track only if successful
+        _track_run_input(artifacts, is_run_input)
+        return ds
