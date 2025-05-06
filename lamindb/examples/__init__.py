1146659ab6dfb37cb399b7f63ef595075bae70d8
Alex Wolf <f.alexander.wolf@gmail.com>
2025-05-06 19:22:16 +0200

    ♻️ Fixes & polishes

--- lamindb/examples/__init__.py
+++ lamindb/examples/__init__.py
@@ -11,50 +11,16 @@ Modules:
 
 
 def ingest_mini_immuno_datasets():
-    """Ingest mini immuno datasets."""
-    import bionty as bt
+    """Ingest mini immuno datasets.
 
-    import lamindb as ln
+    .. literalinclude:: scripts/ingest_mini_immuno_datasets.py
+        :language: python
+    """
+    import sys
+    from pathlib import Path
 
-    # observation-level metadata
-    ln.Feature(name="perturbation", dtype="cat[ULabel]").save()
-    ln.Feature(name="sample_note", dtype="str").save()
-    ln.Feature(name="cell_type_by_expert", dtype="cat[bionty.CellType]").save()
-    ln.Feature(name="cell_type_by_model", dtype="cat[bionty.CellType]").save()
-    # dataset-level metadata
-    ln.Feature(name="temperature", dtype="float").save()
-    ln.Feature(name="experiment", dtype="cat[ULabel]").save()
-    ln.Feature(name="date_of_study", dtype="date").save()
-    ln.Feature(name="study_note", dtype="str").save()
-    ln.Feature(name="study_metadata", dtype=dict).save()
+    docs_path = Path(__file__).parent.parent.parent / "docs" / "scripts"
+    if str(docs_path) not in sys.path:
+        sys.path.append(str(docs_path))
 
-    ## Permissible values for categoricals
-    ln.ULabel.from_values(["DMSO", "IFNG"], create=True).save()
-    ln.ULabel.from_values(["Experiment 1", "Experiment 2"], create=True).save()
-    bt.CellType.from_values(["B cell", "T cell"], create=True).save()
-
-    schema = ln.schemas.anndata.ensembl_gene_ids_and_valid_features_in_obs()
-
-    ## Ingest dataset1
-    adata = ln.core.datasets.mini_immuno.get_dataset1(otype="AnnData")
-    artifact = ln.Artifact.from_anndata(
-        adata,
-        key="examples/dataset1.h5ad",
-        schema=schema,
-    ).save()
-    adhoc = {"study_metadata": {"detail1": "123", "detail2": 1}}
-    dataset_metadata = adata.uns
-    dataset_metadata.update(adhoc)
-    artifact.features.add_values(dataset_metadata)
-
-    # Ingest dataset2
-    adata2 = ln.core.datasets.mini_immuno.get_dataset2(otype="AnnData")
-    artifact2 = ln.Artifact.from_anndata(
-        adata2,
-        key="examples/dataset2.h5ad",
-        schema=schema,
-    ).save()
-    adhoc2 = {"study_metadata": {"detail1": "456", "detail2": 2}}
-    dataset_metadata2 = adata2.uns
-    dataset_metadata2.update(adhoc2)
-    artifact2.features.add_values(dataset_metadata2)
+    import ingest_mini_immuno_datasets  # noqa
