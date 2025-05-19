bc95e231d6cc28402106d375edc0046d267e1cba
Lukas Heumos <lukas.heumos@posteo.net>
2025-05-19 17:06:43 +0200

    🎨 Fix test
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- tests/core/test_artifact.py
+++ tests/core/test_artifact.py
@@ -171,7 +171,7 @@ def test_data_is_soma_experiment_paths():
         ("get_small_adata", "MuData", False),
         ("get_small_mdata", "AnnData", False),
         ("get_small_sdata", "AnnData", False),
-        ("get_small_adata", True),
+        ("get_small_adata", None, True),
         (pd.DataFrame(), "AnnData", False),
         (None, "AnnData", False),
         (None, None, False),
