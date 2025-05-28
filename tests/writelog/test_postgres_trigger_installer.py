abd82036529d38e20b4fe25b441c71b7b391e21a
Alex Rasmussen <alex@bitsondisk.com>
2025-05-27 22:49:10 -0700

    🎨 Add a test for featurevalue triggers

--- tests/writelog/test_postgres_trigger_installer.py
+++ tests/writelog/test_postgres_trigger_installer.py
@@ -7,6 +7,7 @@ import pytest
 from django.db import connection as django_connection_proxy
 from django.db import transaction
 from django.db.backends.utils import CursorWrapper
+from lamindb.core.datasets import small_dataset1
 from lamindb.core.writelog._constants import FOREIGN_KEYS_LIST_COLUMN_NAME
 from lamindb.core.writelog._db_metadata_wrapper import (
     PostgresDatabaseMetadataWrapper,
@@ -17,7 +18,7 @@ from lamindb.core.writelog._trigger_installer import (
 )
 from lamindb.core.writelog._types import Column, ColumnType, TableUID, UIDColumns
 from lamindb.models.artifact import Artifact
-from lamindb.models.feature import Feature
+from lamindb.models.feature import Feature, FeatureValue
 from lamindb.models.run import Run
 from lamindb.models.sqlrecord import Space
 from lamindb.models.transform import Transform
@@ -1467,35 +1468,17 @@ def test_aux_artifacts_backfill_before_real_ones(
 
 @pytest.fixture(scope="function")
 def artifact_with_feature_values(fake_run):
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        test_file = Path(tmpdirname) / "bar.txt"
+    df = small_dataset1(otype="DataFrame")
+    artifact = Artifact.from_df(df, description="test dataset").save()  # type: ignore
 
-        with test_file.open("w") as fp:
-            fp.write("another test file")
+    Feature(name="perturbation", dtype="int").save()
+    artifact.features.add_values({"perturbation": 50})  # type: ignore
 
-        temperature = Feature(name="temperature", dtype="cat").save()
+    yield artifact
 
-        artifact = Artifact(
-            data=str(test_file),
-            description="a fake normal artifact",
-            kind="dataset",
-            run=fake_run,
-        ).save()
-
-        ulabels = ULabel.from_values(["27.2"], create=True).save()
-
-        artifact.features.add_values({"temperature": "27.2"})  # type: ignore
-
-        artifact.save()
-
-        yield artifact
-
-        artifact.delete(permanent=True)
-
-        temperature.delete()
-
-        for u in ulabels:
-            u.delete()
+    artifact.delete(permanent=True)
+    ULabel.filter().all().delete()
+    Feature.filter().all().delete()
 
 
 @pytest.mark.pg_integration
@@ -1506,3 +1489,20 @@ def test_featurevalue_trigger(
         connection=django_connection, db_metadata=PostgresDatabaseMetadataWrapper()
     )
     installer.update_write_log_triggers()
+
+    write_log = [
+        w
+        for w in WriteLog.objects.all().order_by("id")
+        if w.table.table_name in {"lamindb_featurevalue"}
+    ]
+
+    assert len(write_log) == 1
+
+    write_log_entry = write_log[0]
+
+    assert len(write_log_entry.record_uid) == 2
+
+    feature_value = FeatureValue.objects.get(hash=write_log_entry.record_uid[0])
+    assert feature_value.feature is not None
+
+    assert write_log_entry.record_uid[1] == feature_value.feature.uid
