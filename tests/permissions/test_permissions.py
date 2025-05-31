b4f4f45bbfb6ea2452b3543df1ff12c63ab67e63
Koncopd <koncopd@gmail.com>
2025-05-31 12:07:12 +0200

    test security

--- tests/permissions/test_permissions.py
+++ tests/permissions/test_permissions.py
@@ -26,6 +26,9 @@ db_token_manager.set(db_token)
 
 
 def test_authentication():
+    # test access to the security schema
+    with connection.connection.cursor() as cur:
+        cur.execute("SELECT security.get_secret('jwt_secret');")
     # just check that the token was setup
     with connection.cursor() as cur:
         cur.execute("SELECT get_account_id();")
