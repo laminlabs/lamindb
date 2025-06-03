826554958d8d47ddc5545a2e86b5f97a4db5c2da
Alex Wolf <f.alexander.wolf@gmail.com>
2025-06-03 13:31:07 +0200

    🔖 Release 1.6.1

--- lamindb/__init__.py
+++ lamindb/__init__.py
@@ -109,7 +109,7 @@ Backwards compatibility.
 
 # ruff: noqa: I001
 # denote a release candidate for 0.1.0 with 0.1rc1, 0.1a1, 0.1b1, etc.
-__version__ = "1.6.0"
+__version__ = "1.6.1"
 
 import warnings
 
@@ -140,7 +140,6 @@ if _check_instance_setup(from_module="lamindb"):
     from .curators._legacy import CatManager as Curator
     from .models import (
         Artifact,
-        Branch,
         Collection,
         Feature,
         FeatureSet,  # backward compat
