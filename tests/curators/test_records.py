20772e93af220820d0dd135e84859bb4f8d2cef3
Alex Wolf <f.alexander.wolf@gmail.com>
2025-06-03 14:41:26 +0200

    💚 Fixes

--- tests/curators/test_records.py
+++ tests/curators/test_records.py
@@ -23,7 +23,7 @@ def test_record_example_compound_treatment():
 
     # features for treatments
     compound = ln.Feature(name="compound", dtype=compound_type).save()
-    concentration = ln.Feature(name="concentration", dtype=compound_type).save()
+    concentration = ln.Feature(name="concentration", dtype="num").save()
     # a sheet for treatments
     my_treatments = ln.Sheet(
         name="My treatments 2025-05"
@@ -49,10 +49,10 @@ def test_record_example_compound_treatment():
     sample_type = ln.Record(name="BioSample", is_type=True).save()
 
     # features for samples
-    perturbation = ln.Feature(name="perturbation", dtype=compound_type).save()
+    treatment = ln.Feature(name="treatment", dtype=compound_type).save()
     cell_line = ln.Feature(name="cell_line", dtype=bt.CellLine).save()
     schema = ln.Schema(
-        name="My samples schema 2025-05", features=[perturbation, cell_line]
+        name="My samples schema 2025-05", features=[treatment, cell_line]
     ).save()
     # a sheet for samples
     sheet1 = ln.Sheet(name="My samples 2025-05", schema=schema).save()
@@ -61,17 +61,13 @@ def test_record_example_compound_treatment():
 
     # populate sample1
     sample1 = ln.Record(name="sample1", type=sample_type, sheet=sheet1).save()
-    ln.models.RecordRecord(
-        record=sample1, feature=perturbation, value=treatment1
-    ).save()
+    ln.models.RecordRecord(record=sample1, feature=treatment, value=treatment1).save()
     bt.models.RecordCellLine(
         record=sample1, feature=cell_line, cellline=hek293t
     ).save()  # parallel to ArtifactCellLine
     # populate sample2
     sample2 = ln.Record(name="sample2", type=sample_type, sheet=sheet1).save()
-    ln.models.RecordRecord(
-        record=sample2, feature=perturbation, value=treatment2
-    ).save()
+    ln.models.RecordRecord(record=sample2, feature=treatment, value=treatment2).save()
     bt.models.RecordCellLine(
         record=sample2, feature=cell_line, cellline=hek293t
     ).save()  # parallel to ArtifactCellLine
@@ -80,23 +76,23 @@ def test_record_example_compound_treatment():
     sample_note = ln.Feature(name="sample_note", dtype="str").save()
     schema2 = ln.Schema(
         name="My samples schema 2025-06",
-        features=[perturbation, cell_line, sample_note],
+        features=[treatment, cell_line, sample_note],
     ).save()
     # the sheet
     sheet2 = ln.Sheet(name="My samples 2025-06", schema=schema2).save()
     # populate sample3
     sample3 = ln.Record(type=sample_type, sheet=sheet2).save()  # no name
-    ln.models.RecordRecord(
-        record=sample3, feature=perturbation, value=treatment1
-    ).save()
+    ln.models.RecordRecord(record=sample3, feature=treatment, value=treatment1).save()
     bt.models.RecordCellLine(
         record=sample3, feature=cell_line, cellline=hek293t
     ).save()  # parallel to ArtifactCellLine
     # populate sample4
     sample4 = ln.Record(type=sample_type, sheet=sheet2).save()
-    ln.models.RecordRecord(
-        record=sample4, feature=perturbation, value=treatment2
-    ).save()
+    ln.models.RecordRecord(record=sample4, feature=treatment, value=treatment2).save()
     bt.models.RecordCellLine(
         record=sample4, feature=cell_line, cellline=hek293t
     ).save()  # parallel to ArtifactCellLine
+
+
+if __name__ == "__main__":
+    test_record_example_compound_treatment()
