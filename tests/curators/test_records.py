0f13bb853ddd9a9419c1869f0699ed1911c2a038
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2025-06-05 11:32:17 +0200

    🧪 Nextflow samplesheet tests

--- tests/curators/test_records.py
+++ tests/curators/test_records.py
@@ -1,5 +1,6 @@
 import bionty as bt
 import lamindb as ln
+import pandas as pd
 
 
 def test_record_example_compound_treatment():
@@ -92,6 +93,95 @@ def test_record_example_compound_treatment():
     bt.models.RecordCellLine(
         record=sample4, feature=cell_line, cellline=hek293t
     ).save()  # parallel to ArtifactCellLine
+
+
+def test_record_nextflow_samples():
+    # Biosample schema and type
+    samples_schema = ln.Schema(
+        name="Biosample test schema",
+        features=[
+            ln.Feature(name="species", dtype="cat[bionty.Organism]").save(),
+            ln.Feature(name="cell_type", dtype="cat[bionty.CellType]").save(),
+            ln.Feature(name="tissue", dtype="cat[bionty.Tissue]").save(),
+        ],
+    ).save()
+
+    biosample_type = ln.Record(name="BioSample", is_type=True).save()
+
+    # Biosamples sheet
+    samples_sheet = ln.Sheet(name="My samples 2025-04", schema=samples_schema).save()
+    sample_x = ln.Record(
+        name="Sample_X", type=biosample_type, sheet=samples_sheet
+    ).save()
+    sample_y = ln.Record(
+        name="Sample_Y", type=biosample_type, sheet=samples_sheet
+    ).save()
+
+    organism_human = bt.Organism.from_source(name="human").save()
+    celltype_tcell = bt.CellType.from_source(name="T cell").save()
+    tissue_blood = bt.Tissue.from_source(name="blood").save()
+
+    features = ln.Feature.lookup()
+    for sample in [sample_x, sample_y]:
+        bt.models.RecordOrganism(
+            record=sample, feature=features.species, organism=organism_human
+        ).save()
+        bt.models.RecordCellType(
+            record=sample, feature=features.cell_type, celltype=celltype_tcell
+        ).save()
+        bt.models.RecordTissue(
+            record=sample, feature=features.tissue, tissue=tissue_blood
+        ).save()
+
+    # Nextflow samplesheet schema
+    nextflow_schema = ln.Schema(
+        name="RNA-seq standard",
+        features=[
+            ln.Feature(name="sample", dtype="cat[Record[BioSample]]").save(),
+            ln.Feature(name="fastq_1", dtype=str).save(),
+            ln.Feature(name="fastq_2", dtype=str).save(),
+            ln.Feature(name="expected_cells", dtype=int).save(),
+            ln.Feature(name="seq_center", dtype=str).save(),
+        ],
+    )
+
+    nextflowsample_type = ln.Record(name="NextflowSample", is_type=True).save()
+    nextflow_sheet = ln.Sheet(
+        schema=nextflow_schema, name="RNA-seq nextflow samplesheet 001"
+    ).save()
+
+    df = pd.DataFrame(
+        {
+            "sample": ["Sample_X", "Sample_Y", "Sample_Y"],
+            "fastq_1": [
+                "https://raw.githubusercontent.com/nf-core/test-datasets/scrnaseq/testdata/cellranger/Sample_X_S1_L001_R1_001.fastq.gz",
+                "https://raw.githubusercontent.com/nf-core/test-datasets/scrnaseq/testdata/cellranger/Sample_Y_S1_L001_R1_001.fastq.gz",
+                "https://raw.githubusercontent.com/nf-core/test-datasets/scrnaseq/testdata/cellranger/Sample_Y_S1_L002_R1_001.fastq.gz",
+            ],
+            "fastq_2": [
+                "https://raw.githubusercontent.com/nf-core/test-datasets/scrnaseq/testdata/cellranger/Sample_X_S1_L001_R2_001.fastq.gz",
+                "https://raw.githubusercontent.com/nf-core/test-datasets/scrnaseq/testdata/cellranger/Sample_Y_S1_L001_R2_001.fastq.gz",
+                "https://raw.githubusercontent.com/nf-core/test-datasets/scrnaseq/testdata/cellranger/Sample_Y_S1_L002_R2_001.fastq.gz",
+            ],
+            "expected_cells": [5000, 5000, 5000],
+        }
+    )
+
+    features = ln.Feature.lookup()
+    for _, row in df.iterrows():
+        sample = ln.Record(sheet=nextflow_sheet, type=nextflowsample_type).save()
+        ln.models.RecordRecord(
+            record=sample, feature=features.sample, value=ln.Record.get(name="Sample_X")
+        ).save()
+        ln.models.RecordJson(
+            record=sample, feature=features.fastq_1, value=row["fastq_1"]
+        ).save()
+        ln.models.RecordJson(
+            record=sample, feature=features.fastq_2, value=row["fastq_2"]
+        ).save()
+        ln.models.RecordJson(
+            record=sample, feature=features.expected_cells, value=row["expected_cells"]
+        ).save()
 
 
 if __name__ == "__main__":
