ca4affa08259c205d0cdba559b89ad92cb11c54c
Alex Wolf <f.alexander.wolf@gmail.com>
2025-06-14 11:59:01 +0200

    ♻️ More checks

--- lamindb/models/core.py
+++ lamindb/models/core.py
@@ -6,6 +6,8 @@ from typing import (
 )
 
 from django.db import models
+from lamindb_setup import settings as setup_settings
+from lamindb_setup.core._hub_core import get_storage_records_for_instance
 from lamindb_setup.core.upath import check_storage_is_empty
 
 from lamindb.base.fields import (
@@ -132,4 +134,22 @@ class Storage(SQLRecord, TracksRun, TracksUpdates):
 
         This errors in case the storage location is not empty.
         """
+        from lamindb_setup.core._hub_core import delete_storage_record
+
+        from .. import settings
+
         check_storage_is_empty(self.path)
+        assert settings.storage.root_as_str != self.root, (  # noqa: S101
+            "Cannot delete the current storage location, switch to another."
+        )
+        if setup_settings.user.handle != "anonymous":  # only attempt if authenticated
+            storage_records = get_storage_records_for_instance(
+                setup_settings.instance._id
+            )
+            for storage_record in storage_records:
+                if storage_record["lnid"] == self.uid:
+                    assert storage_record["is_default"] in {False, None}, (  # noqa: S101
+                        "Cannot delete default storage of instance."
+                    )
+                    delete_storage_record(storage_record)
+        super().delete()
--- tests/storage/test_switch_delete_storage_location.py
+++ tests/storage/test_switch_delete_storage_location.py
@@ -2,9 +2,10 @@ from pathlib import Path
 
 import lamindb as ln
 import pytest
+from lamindb_setup.core._hub_core import get_storage_records_for_instance
 
 
-def test_settings_switch_storage():
+def test_switch_delete_storage_location():
     ln.settings.storage = "./default_storage_unit_storage"
     assert (
         ln.settings.storage.root.resolve()
@@ -19,9 +20,22 @@ def test_settings_switch_storage():
     assert ln.setup.settings.storage.root.fs.cache_regions
     ln.settings.storage = new_storage_location, {"cache_regions": False}
     assert not ln.setup.settings.storage.root.fs.cache_regions
+    assert ln.setup.settings.storage.root.exists()
 
     # now work with the new storage location
     new_storage = ln.Storage.get(root=new_storage_location)
+    assert (
+        len(
+            [
+                r
+                for r in get_storage_records_for_instance(
+                    ln.setup.settings.instance._id
+                )
+                if r["lnid"] == new_storage.uid
+            ]
+        )
+        == 1
+    )
     artifact = ln.Artifact(".gitignore", key="test_artifact").save()
     assert new_storage.root in artifact.path.as_posix()
     with pytest.raises(ln.setup.errors.StorageNotEmpty) as err:
@@ -30,6 +44,40 @@ def test_settings_switch_storage():
         "'s3://lamindb-ci/test-settings-switch-storage/.lamindb' contains 1 objects"
         in err.exconly()
     )
+    assert (
+        len(
+            [
+                r
+                for r in get_storage_records_for_instance(
+                    ln.setup.settings.instance._id
+                )
+                if r["lnid"] == new_storage.uid
+            ]
+        )
+        == 1
+    )
+
+    # now delete the artifact so that the storage location is empty
     artifact.delete(permanent=True)
+    with pytest.raises(AssertionError) as err:
+        new_storage.delete()
+    assert (
+        "Cannot delete the current storage location, switch to another."
+        in err.exconly()
+    )
+
     # switch back to default storage
     ln.settings.storage = "./default_storage_unit_storage"
+    new_storage.delete()
+    assert (
+        len(
+            [
+                r
+                for r in get_storage_records_for_instance(
+                    ln.setup.settings.instance._id
+                )
+                if r["lnid"] == new_storage.uid
+            ]
+        )
+        == 0
+    )
