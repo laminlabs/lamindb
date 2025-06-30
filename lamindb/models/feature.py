248b0c38205329fd20853ec63b151c511f447d62
Lukas Heumos <lukas.heumos@posteo.net>
2025-06-30 18:43:09 +0200

    🎨 Docstring refactoring
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- lamindb/models/feature.py
+++ lamindb/models/feature.py
@@ -531,41 +531,42 @@ class Feature(SQLRecord, CanCurate, TracksRun, TracksUpdates):
         :class:`~lamindb.Schema`
             Feature sets.
 
-    Example::
+    Example:
 
-        A simple `"str"` feature.
+        A simple `"str"` feature.::
 
         ln.Feature(
             name="sample_note",
             dtype="str",
         ).save()
 
-        A dtype `"cat[ULabel]"` can be more easily passed as below.
+        A dtype `"cat[ULabel]"` can be more easily passed as below.::
 
         ln.Feature(
             name="project",
             dtype=ln.ULabel,
         ).save()
 
-        A dtype `"cat[ULabel|bionty.CellType]"` can be more easily passed as below.
+        A dtype `"cat[ULabel|bionty.CellType]"` can be more easily passed as below.::
 
         ln.Feature(
             name="cell_type",
             dtype=[ln.ULabel, bt.CellType],
         ).save()
 
-        A multivalue feature with a list of cell types.
+        A multivalue feature with a list of cell types.::
 
         ln.Feature(
             name="cell_types",
             dtype=list[bt.CellType],  # or list[str] for a list of strings
         ).save()
 
-        A path feature.
-        >>> ln.Feature(
-        ...     name="image_path",
-        ...     dtype="path",   # will be validated as `str`
-        ... ).save()
+        A path feature.::
+
+        ln.Feature(
+            name="image_path",
+            dtype="path",   # will be validated as `str`
+        ).save()
 
     Hint:
 
