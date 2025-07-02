392ebbd64e484eb80a055a517b8c695444cbf6f0
Alex Wolf <f.alexander.wolf@gmail.com>
2025-07-02 22:58:31 +0200

    ✨ Enable converting records to dataframes (#2882)

--- lamindb/models/artifact.py
+++ lamindb/models/artifact.py
@@ -124,6 +124,7 @@ if TYPE_CHECKING:
     from ._label_manager import LabelManager
     from .collection import Collection
     from .project import Project, Reference
+    from .record import Record
     from .transform import Transform
 
 
@@ -1312,6 +1313,8 @@ class Artifact(SQLRecord, IsVersioned, TracksRun, TracksUpdates):
     """Linked projects."""
     references: Reference
     """Linked references."""
+    records: Record
+    """Linked records."""
 
     @overload
     def __init__(
