cc194233704ec116331f7194385ecbc90919b111
Alex Wolf <f.alexander.wolf@gmail.com>
2025-07-02 13:27:42 +0200

    💚 Fix

--- tests/storage/test_connect_reconnect.py
+++ tests/storage/test_connect_reconnect.py
@@ -3,10 +3,9 @@ import pytest
 
 
 def test_connect_reconnect():
-    assert ln.setup.settings.user.handle == "testuser1"
     ln.connect("lamindb-unit-tests-storage")  # this is not changing anything
     count1 = ln.Artifact.filter().count()
-    # an instance that does not have bionty configured
+    # a public instance that does not have bionty configured
     ln.connect("laminlabs/lamin-site-assets")
     count2 = ln.Artifact.filter().count()
     assert count1 != count2
