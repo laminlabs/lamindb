eca2eec1b47c05c5a8e77d2ddf500e7be97babe7
Alex Wolf <f.alexander.wolf@gmail.com>
2025-07-02 11:43:47 +0200

    ♻️ An instance that does not have bionty configured

--- tests/storage/test_connect_reconnect.py
+++ tests/storage/test_connect_reconnect.py
@@ -3,14 +3,16 @@ import pytest
 
 
 def test_connect_reconnect():
-    ln.connect("laminlabs/lamindata")
+    assert ln.setup.settings.user.handle == "testuser1"
+    ln.connect("lamindb-unit-tests-storage")  # this is not changing anything
     count1 = ln.Artifact.filter().count()
+    # an instance that does not have bionty configured
     ln.connect("laminlabs/lamin-site-assets")
     count2 = ln.Artifact.filter().count()
     assert count1 != count2
     with pytest.raises(ln.setup.errors.ModuleWasntConfigured):
         import bionty as bt
-    ln.connect("laminlabs/lamindata")
+    ln.connect("lamindb-unit-tests-storage")
     import bionty as bt
 
     count3 = bt.Gene.filter().count()
