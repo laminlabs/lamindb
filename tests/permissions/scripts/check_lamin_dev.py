3e6e8d0499c0e9e0989e1cb0f35d9823b8fa3aa3
Alex Wolf <f.alexander.wolf@gmail.com>
2025-07-07 01:12:30 +0200

    💚 Number of fixes

--- tests/permissions/scripts/check_lamin_dev.py
+++ tests/permissions/scripts/check_lamin_dev.py
@@ -18,7 +18,7 @@ ln.track(space=space_name)
 
 assert ln.context.space.name == space_name
 ulabel = ln.ULabel(name="My test ulabel in test space").save()
-assert ulabel.space.name == "all"  # ulabel should end up in common space
+assert ulabel.space.name == space_name  # ulabel should end up in the restricted space
 ulabel.delete()  # delete silently passes in case another worker deleted the ulabel
 assert (
     ln.context.transform.space.name == space_name
