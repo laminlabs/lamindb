add21f28fbf64fe6bf4abb67841fc3cad5ddde03
Lukas Heumos <lukas.heumos@posteo.net>
2025-07-18 10:20:16 +0200

    🎨 Docs
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- lamindb/examples/cellxgene/__init__.py
+++ lamindb/examples/cellxgene/__init__.py
@@ -1,3 +1,12 @@
-from .cellxgene import save_cxg_defaults
+"""CELLxGENE.
 
-__all__ = ["save_cxg_defaults"]
+.. autosummary::
+   :toctree: .
+
+   save_cxg_defaults
+   get_cxg_schema
+"""
+
+from .cellxgene import get_cxg_schema, save_cxg_defaults
+
+__all__ = ["save_cxg_defaults", "get_cxg_schema"]
