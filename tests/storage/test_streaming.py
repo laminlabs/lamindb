56d65816bc929b8d4c774449454a05b3f06012c5
Lukas Heumos <lukas.heumos@posteo.net>
2025-07-27 20:54:58 +0200

    ✅ Clean up core tests properly (#2984)
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- tests/storage/test_streaming.py
+++ tests/storage/test_streaming.py
@@ -212,8 +212,7 @@ def test_backed_zarr_not_adata():
 
 def test_anndata_open_mode():
     fp = ln.core.datasets.anndata_file_pbmc68k_test()
-    artifact = ln.Artifact(fp, key="test_adata.h5ad")
-    artifact.save()
+    artifact = ln.Artifact(fp, key="test_adata.h5ad").save()
 
     with artifact.open(mode="r") as access:
         assert isinstance(access, AnnDataAccessor)
@@ -549,6 +548,8 @@ def test_open_dataframe_collection():
     artifact2.delete(permanent=True)
     artifact3.delete(permanent=True)
     artifact4.delete(permanent=True, storage=False)
+
+    Path("mini.csv").unlink(missing_ok=True)
 
     ln.settings.storage = "s3://lamindb-test/storage"
 
