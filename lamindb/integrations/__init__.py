f827562665e62bd17687edd5e754dacca98b010a
Alex Wolf <f.alexander.wolf@gmail.com>
2025-07-28 16:31:22 +0200

    ✨ Add `curate_from_croissant()` to curate from MLCommons `Croissant` files (#2989)

--- lamindb/integrations/__init__.py
+++ lamindb/integrations/__init__.py
@@ -5,8 +5,10 @@
 
    save_vitessce_config
    save_tiledbsoma_experiment
+   curate_from_croissant
 """
 
 from lamindb.core.storage import save_tiledbsoma_experiment
 
+from ._croissant import curate_from_croissant
 from ._vitessce import save_vitessce_config
