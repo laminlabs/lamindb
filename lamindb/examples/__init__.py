4f94a1fec316bac92675c5c486485fc083532dc4
Alex Wolf <f.alexander.wolf@gmail.com>
2025-07-29 16:09:00 +0200

    🚚 Move ingest_mini_immuno_dataset

--- lamindb/examples/__init__.py
+++ lamindb/examples/__init__.py
@@ -7,25 +7,8 @@
    datasets
    cellxgene
    croissant
-   ingest_mini_immuno_datasets
 
 """
 
 from . import croissant, datasets, schemas
 from .cellxgene import _cellxgene
-
-
-def ingest_mini_immuno_datasets():
-    """Ingest mini immuno datasets.
-
-    .. literalinclude:: scripts/ingest_mini_immuno_datasets.py
-        :language: python
-    """
-    import sys
-    from pathlib import Path
-
-    docs_path = Path(__file__).parent.parent.parent / "docs" / "scripts"
-    if str(docs_path) not in sys.path:
-        sys.path.append(str(docs_path))
-
-    import ingest_mini_immuno_datasets  # noqa
