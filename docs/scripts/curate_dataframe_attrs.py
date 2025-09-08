7556b5240497c16c4ad2b8d00013935290bfc6ec
Lukas Heumos <lukas.heumos@posteo.net>
2025-09-08 15:58:48 +0200

    🎨 Use example dataset module
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- docs/scripts/curate_dataframe_attrs.py
+++ docs/scripts/curate_dataframe_attrs.py
@@ -2,7 +2,7 @@ import lamindb as ln
 
 from .define_schema_df_metadata import study_metadata_schema
 
-df = ln.core.datasets.mini_immuno.get_dataset1(otype="DataFrame")
+df = ln.examples.datasets.mini_immuno.get_dataset1(otype="DataFrame")
 schema = ln.Schema(
     features=[ln.Feature(name="perturbation", dtype="str").save()],
     slots={"attrs": study_metadata_schema},
