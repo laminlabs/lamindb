226da5c2812316c2a1a4ba6777a781ecde8b48ab
Alex Wolf <f.alexander.wolf@gmail.com>
2025-10-01 00:53:47 +0200

    💚 Fix

--- lamindb/migrations/0127_alter_run_status_code_feature_dtype.py
+++ lamindb/migrations/0127_alter_run_status_code_feature_dtype.py
@@ -2,8 +2,6 @@
 
 from django.db import migrations, models
 
-import lamindb.base.fields
-
 
 class Migration(migrations.Migration):
     dependencies = [
@@ -21,11 +19,13 @@ class Migration(migrations.Migration):
                 db_default=-3, db_index=True, default=-3, null=True
             ),
         ),
-        migrations.AlterField(
+        migrations.AddConstraint(
             model_name="feature",
-            name="dtype",
-            field=lamindb.base.fields.CharField(
-                blank=True, db_index=True, default=None, max_length=255
+            constraint=models.CheckConstraint(
+                condition=models.Q(
+                    ("is_type", True), ("dtype__isnull", False), _connector="OR"
+                ),
+                name="dtype_not_null_when_is_type_false",
             ),
         ),
     ]
