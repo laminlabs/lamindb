35e1035c9bc2cfefc9211b44b5997d7ba6ee8437
Lukas Heumos <lukas.heumos@posteo.net>
2025-10-02 17:26:58 +0200

    🎨 Use schema
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- lamindb/examples/mlflow/__init__.py
+++ lamindb/examples/mlflow/__init__.py
@@ -3,17 +3,17 @@
 .. autosummary::
    :toctree: .
 
-   save_default_values
-
+   create_mlflow_schema
 """
 
 import lamindb as ln
+from lamindb.models import Schema
 
 
-def save_default_values() -> None:
-    """Save commonly tracked default values of MLflow runs to the instance.
+def create_mlflow_schema() -> Schema:
+    """Creates a MLflow schema that covers all MLflow experiment and run related features.
 
-    Stores the following features:
+    Saves the following features:
 
     - mlflow_run_id
     - mlflow_run_name
@@ -26,13 +26,28 @@ def save_default_values() -> None:
     - start_time
     - end_time
     """
-    ln.Feature(name="mlflow_run_id", dtype=str).save()
-    ln.Feature(name="mlflow_run_name", dtype=str).save()
-    ln.Feature(name="mlflow_experiment_id", dtype=str).save()
-    ln.Feature(name="mlflow_experiment_name", dtype=str).save()
-    ln.Feature(name="mlflow_user_id", dtype=str).save()
-    ln.Feature(name="mlflow_status", dtype=str).save()
-    ln.Feature(name="mlflow_lifecycle_stage", dtype=str).save()
-    ln.Feature(name="mlflow_artifact_uri", dtype=str).save()
-    ln.Feature(name="start_time", dtype=int).save()
-    ln.Feature(name="end_time", dtype=int).save()
+    # A different design could leverage records instead where ID and name are bundled.
+    # This could potentially show up nicer in the UI and allow for markdown comments on MLflow experiments or runs.
+    mlflow_features = [
+        ln.Feature(name="mlflow_run_id", dtype=str).save(),
+        ln.Feature(name="mlflow_run_name", dtype=str).save(),
+        ln.Feature(name="mlflow_experiment_id", dtype=str).save(),
+        ln.Feature(name="mlflow_experiment_name", dtype=str).save(),
+        ln.Feature(name="mlflow_user_id", dtype=str).save(),
+        ln.Feature(name="mlflow_status", dtype=str).save(),
+        ln.Feature(name="mlflow_lifecycle_stage", dtype=str).save(),
+        ln.Feature(name="mlflow_artifact_uri", dtype=str).save(),
+        ln.Feature(name="start_time", dtype=int).save(),
+        ln.Feature(name="end_time", dtype=int).save(),
+    ]
+
+    schema = ln.Schema(
+        name="MLflow schema",
+        features=mlflow_features,
+        otype="DataFrame",
+        minimal_set=False,
+        maximal_set=True,
+        coerce_dtype=True,
+    ).save()
+
+    return schema
