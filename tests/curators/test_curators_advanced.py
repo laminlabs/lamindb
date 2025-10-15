549561bcb1448345b4ec55d0f9b4b1369693f885
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2025-10-15 15:41:00 +0200

    💚 Refactor tests

--- tests/curators/test_curators_advanced.py
+++ tests/curators/test_curators_advanced.py
@@ -5,14 +5,24 @@ import pytest
 from lamindb.core.exceptions import ValidationError
 
 
-@pytest.fixture
-def df():
+@pytest.fixture(scope="module")
+def lists_df():
     return pd.DataFrame(
         {
             "sample_id": [["sample1", "sample2"], ["sample2"], ["sample3"]],
             "dose": [[1.2, 2.3], [1.2], [2.3]],
             "cell_type": [["B cell", "T cell"], ["B cell"], ["T cell"]],
             "tissue": [["blood", "pulmo"], ["blood"], ["lung"]],
+        }
+    )
+
+
+@pytest.fixture(scope="module")
+def nested_cat_df():
+    return pd.DataFrame(
+        {
+            "customer_id": ["C1", "C2", "C3", "C4", "C5", "C6"],
+            "us_customer_name": ["Alice", "Bob", "Charlie", "David", "Eve", "Frank"],
         }
     )
 
@@ -36,23 +46,8 @@ def lists_schema():
     bt.Tissue.filter().delete(permanent=True)
 
 
-def test_curator_df_multivalue(df, lists_schema):
-    curator = ln.curators.DataFrameCurator(df, lists_schema)
-    with pytest.raises(ValidationError):
-        curator.validate()
-    assert curator.cat._cat_vectors.keys() == {"columns", "tissue"}
-    assert curator.cat._cat_vectors["tissue"]._validated == ["blood", "lung"]
-    assert curator.cat._cat_vectors["tissue"]._non_validated == ["pulmo"]
-    assert curator.cat._cat_vectors["tissue"]._synonyms == {"pulmo": "lung"}
-
-    curator.cat.standardize("tissue")
-    assert curator.cat._cat_vectors["tissue"]._non_validated == []
-    assert df["tissue"].tolist() == [["blood", "lung"], ["blood"], ["lung"]]
-
-    assert curator.validate() is None
-
-
-def test_curators_df_nested_cat():
+@pytest.fixture(scope="module")
+def nested_cat_schema():
     schema = ln.Schema(
         name="schema_with_nested_cat",
         features=[
@@ -64,6 +59,31 @@ def test_curators_df_nested_cat():
         coerce_dtype=True,
     ).save()
 
+    yield schema
+
+    schema.delete(permanent=True)
+    ln.Feature.filter().delete(permanent=True)
+    ln.Record.filter().update(type=None)
+    ln.Record.filter().delete(permanent=True)
+
+
+def test_curator_df_multivalue(lists_df, lists_schema):
+    curator = ln.curators.DataFrameCurator(lists_df, lists_schema)
+    with pytest.raises(ValidationError):
+        curator.validate()
+    assert curator.cat._cat_vectors.keys() == {"columns", "tissue"}
+    assert curator.cat._cat_vectors["tissue"]._validated == ["blood", "lung"]
+    assert curator.cat._cat_vectors["tissue"]._non_validated == ["pulmo"]
+    assert curator.cat._cat_vectors["tissue"]._synonyms == {"pulmo": "lung"}
+
+    curator.cat.standardize("tissue")
+    assert curator.cat._cat_vectors["tissue"]._non_validated == []
+    assert lists_df["tissue"].tolist() == [["blood", "lung"], ["blood"], ["lung"]]
+
+    assert curator.validate() is None
+
+
+def test_curators_df_nested_cat(nested_cat_df, nested_cat_schema):
     # Create hierarchical Record types and some Records
     UScustomer = ln.Record(name="UScustomer", is_type=True).save()
     Customer = ln.Record(name="Customer", is_type=True, type=UScustomer).save()
@@ -77,21 +97,9 @@ def test_curators_df_nested_cat():
         ln.Record(name=name, type=Customer).save()
 
     # "test_customer" is not a UScustomer, so it should not be validated
-    df = pd.DataFrame(
-        {
-            "customer_id": ["C1", "C2", "C3", "C4", "C5", "C6"],
-            "us_customer_name": ["Alice", "Bob", "Charlie", "David", "Eve", "Frank"],
-        }
-    )
-
     with pytest.raises(ValidationError):
-        curator = ln.curators.DataFrameCurator(df, schema)
+        curator = ln.curators.DataFrameCurator(nested_cat_df, nested_cat_schema)
         curator.validate()
 
     assert len(curator.cat._cat_vectors["us_customer_name"]._validated) == 4
     assert len(curator.cat._cat_vectors["us_customer_name"]._non_validated) == 2
-
-    schema.delete(permanent=True)
-    ln.Feature.filter().delete(permanent=True)
-    ln.Record.filter().update(type=None)
-    ln.Record.filter().delete(permanent=True)
