2be3d7dfe721929e23313b8ca1e6cf2203d9a6b0
Alex Wolf <f.alexander.wolf@gmail.com>
2025-10-21 13:36:53 +0200

    🚸 Enable passing sync_git_repo via env variable

--- lamindb/core/_settings.py
+++ lamindb/core/_settings.py
@@ -142,21 +142,31 @@ class Settings:
     def sync_git_repo(self) -> str | None:
         """Sync transforms with scripts in git repository.
 
-        Provide the full git repo URL.
+        If set, scripts will be synced with the specified git repository.
+
+        Example::
+
+            ln.settings.sync_git_repo = https://github.com/laminlabs/schmidt22
+
+        You can also pass the git repo URL via the environment variable `LAMINDB_SYNC_GIT_REPO`::
+
+            export LAMINDB_SYNC_GIT_REPO=https://github.com/laminlabs/schmidt22
+
+        You'll then see::
+
+            ln.settings.sync_git_repo
+            #> 'https://github.com/laminlabs/schmidt22'
+
         """
         if self._sync_git_repo is not None:
             return self._sync_git_repo
-        elif os.environ.get("LAMINDB_MULTI_INSTANCE") == "true":
-            return None
+        elif os.environ.get("LAMINDB_SYNC_GIT_REPO") is not None:
+            return sanitize_git_repo_url(os.environ["LAMINDB_SYNC_GIT_REPO"])
         else:
             return setup_settings.instance.git_repo
 
     @sync_git_repo.setter
     def sync_git_repo(self, value) -> None:
-        """Sync transforms with scripts in git repository.
-
-        For example: `ln.settings.sync_git_repo = https://github.com/laminlabs/redun-lamin`
-        """
         self._sync_git_repo = sanitize_git_repo_url(value)
         if not self._sync_git_repo.startswith("https://"):  # pragma: nocover
             raise ValueError("git repository URL must start with 'https://'.")
