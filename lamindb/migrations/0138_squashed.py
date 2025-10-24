186da11be495cfb6edc1a99e88b9e0bc81af8990
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2025-10-24 14:49:23 +0200

    🚑️ Fix migration

--- lamindb/migrations/0138_squashed.py
+++ lamindb/migrations/0138_squashed.py
@@ -333,6 +333,7 @@ class Migration(migrations.Migration):
             "lamindb",
             "0137_remove_recordrecord__aux_remove_recordrecord_branch_and_more",
         ),
+        ("lamindb", "0138_remove_record_linked_users_user_linked_in_records"),
     ]
 
     dependencies = []  # type: ignore
