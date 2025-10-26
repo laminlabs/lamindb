5c974a7da6e6023705da2b50b65ede91021785b2
Alex Wolf <f.alexander.wolf@gmail.com>
2025-10-26 22:17:36 +0100

    ⬆️ Upgrade django

--- lamindb/models/query_set.py
+++ lamindb/models/query_set.py
@@ -403,12 +403,9 @@ def get_feature_annotate_kwargs(
             link_model = getattr(registry, obj.related_name).through
             if link_model.__name__ == "Record_parents":
                 continue
-            print(registry.__name__.lower())
             links = link_model.filter(
                 **{registry.__name__.lower() + "_id__in": ids_list}
             )
-            print(links.all())
-            print(links.to_dataframe(include="feature__name"))
             feature_names_for_link_model = links.values_list("feature__name", flat=True)
             feature_names += feature_names_for_link_model
         if registry is Record:
