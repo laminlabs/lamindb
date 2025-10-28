9b2c480345be1f80284428798668175db24977c2
Alex Wolf <f.alexander.wolf@gmail.com>
2025-10-28 08:26:03 +0100

    ♻️ Datasets

--- lamindb/examples/datasets/__init__.py
+++ lamindb/examples/datasets/__init__.py
@@ -1,69 +1,57 @@
 """Example datasets.
 
-The mini immuno dataset.
+The mini immuno dataset
+-----------------------
 
-.. autosummary::
-   :toctree: .
+.. autofunction:: mini_immuno
 
-   mini_immuno
+Small in-memory datasets
+------------------------
 
-Small in-memory datasets.
+.. autofunction:: anndata_with_obs
 
-.. autosummary::
-   :toctree: .
+Files
+-----
 
-   anndata_with_obs
+.. autofunction:: file_fcs
+.. autofunction:: file_fcs_alpert19
+.. autofunction:: file_tsv_rnaseq_nfcore_salmon_merged_gene_counts
+.. autofunction:: file_jpg_paradisi05
+.. autofunction:: file_tiff_suo22
+.. autofunction:: file_fastq
+.. autofunction:: file_bam
+.. autofunction:: file_mini_csv
 
-Files.
+Directories
+-----------
 
-.. autosummary::
-   :toctree: .
+.. autofunction:: dir_scrnaseq_cellranger
+.. autofunction:: dir_iris_images
 
-   file_fcs
-   file_fcs_alpert19
-   file_tsv_rnaseq_nfcore_salmon_merged_gene_counts
-   file_jpg_paradisi05
-   file_tiff_suo22
-   file_fastq
-   file_bam
-   file_mini_csv
+Dictionary, Dataframe, AnnData, MuData, SpatialData
+----------------------------------------------------
 
-Directories.
+.. autofunction:: dict_cellxgene_uns
+.. autofunction:: df_iris
+.. autofunction:: df_iris_in_meter
+.. autofunction:: df_iris_in_meter_study1
+.. autofunction:: df_iris_in_meter_study2
+.. autofunction:: anndata_mouse_sc_lymph_node
+.. autofunction:: anndata_human_immune_cells
+.. autofunction:: anndata_pbmc68k_reduced
+.. autofunction:: anndata_file_pbmc68k_test
+.. autofunction:: anndata_pbmc3k_processed
+.. autofunction:: anndata_with_obs
+.. autofunction:: anndata_suo22_Visium10X
+.. autofunction:: mudata_papalexi21_subset
+.. autofunction:: schmidt22_crispra_gws_IFNG
+.. autofunction:: schmidt22_perturbseq
+.. autofunction:: spatialdata_blobs
 
-.. autosummary::
-   :toctree: .
+Other
+-----
 
-   dir_scrnaseq_cellranger
-   dir_iris_images
-
-Dictionary, Dataframe, AnnData, MuData, SpatialData.
-
-.. autosummary::
-   :toctree: .
-
-   dict_cellxgene_uns
-   df_iris
-   df_iris_in_meter
-   df_iris_in_meter_study1
-   df_iris_in_meter_study2
-   anndata_mouse_sc_lymph_node
-   anndata_human_immune_cells
-   anndata_pbmc68k_reduced
-   anndata_file_pbmc68k_test
-   anndata_pbmc3k_processed
-   anndata_with_obs
-   anndata_suo22_Visium10X
-   mudata_papalexi21_subset
-   schmidt22_crispra_gws_IFNG
-   schmidt22_perturbseq
-   spatialdata_blobs
-
-Other.
-
-.. autosummary::
-   :toctree: .
-
-   fake_bio_notebook_titles
+.. autofunction:: fake_bio_notebook_titles
 """
 
 from . import mini_immuno
