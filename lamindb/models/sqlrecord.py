402d8529863e8045ed70e92423f8ee65c1c6bd01
Lukas Heumos <lukas.heumos@posteo.net>
2025-11-13 17:07:43 +0100

    🎨 Use username
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- lamindb/models/sqlrecord.py
+++ lamindb/models/sqlrecord.py
@@ -713,11 +713,9 @@ class Registry(ModelBase):
             )
 
             # TODO Replace _jwt with _public
-            print("one")
-            print(iresult)
             if (
-                "postgresql" in iresult["db_scheme"]
-                and iresult.get("db_permissions") == "read"
+                "_jwt" in iresult["db_user_name"]
+                and "postgresql" in iresult["db_scheme"]
             ):
                 db = cls._synchronize_clone(iresult["storage_root"])
                 is_fine_grained_access = False
@@ -746,13 +744,7 @@ class Registry(ModelBase):
             source_modules = isettings.modules
 
             # TODO Replace _jwt with _public
-            print("two")
-            print(isettings._db_permissions)
-            print(isettings.dialect)
-            if (
-                isettings.dialect == "postgresql"
-                and isettings._db_permissions == "read"
-            ):
+            if "_jwt" in isettings.db and isettings.dialect == "postgresql":
                 db = cls._synchronize_clone(isettings.storage.root_as_str)
                 is_fine_grained_access = False
             else:
