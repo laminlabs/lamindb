ab6552d023084835bfe36cf31c828d9ed60ee333
Alex Wolf <f.alexander.wolf@gmail.com>
2025-11-18 14:47:08 +0100

    💚 Fix migration

--- lamindb/migrations/0145_squashed.py
+++ lamindb/migrations/0145_squashed.py
@@ -4563,6 +4563,7 @@ class Migration(migrations.Migration):
                     models.ManyToManyField(
                         related_name="users",
                         through="lamindb.ArtifactUser",
+                        through_fields=("user", "artifact"),
                         to="lamindb.artifact",
                     ),
                 ),
