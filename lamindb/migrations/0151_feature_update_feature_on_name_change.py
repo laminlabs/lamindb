9b990c274ff89cc0a989f0901d3eb45aec0efd83
Alex Wolf <f.alexander.wolf@gmail.com>
2025-12-07 10:30:08 +0100

    ♻️ Fix migration for sqlite

--- lamindb/migrations/0151_feature_update_feature_on_name_change.py
+++ lamindb/migrations/0151_feature_update_feature_on_name_change.py
@@ -2,15 +2,19 @@
 
 import pgtrigger.compiler
 import pgtrigger.migrations
-from django.db import migrations
+from django.db import connection, migrations
 
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("lamindb", "0150_squashed"),
+        ("lamindb", "0150_rename_params_record_extra_data_and_more"),
     ]
 
-    operations = [
+    operations = []  # type: ignore
+
+
+if connection.vendor == "postgresql":
+    Migration.operations += [
         pgtrigger.migrations.AddTrigger(
             model_name="feature",
             trigger=pgtrigger.compiler.Trigger(
