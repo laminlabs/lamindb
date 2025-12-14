74924f5617a979dbf9de344bc2535f9db21b60a0
Alex Wolf <f.alexander.wolf@gmail.com>
2025-12-14 23:27:08 +0100

    🔖 Release 1.17.0

--- lamindb/__init__.py
+++ lamindb/__init__.py
@@ -148,7 +148,7 @@ Developer API
 
 # ruff: noqa: I001
 # denote a release candidate for 0.1.0 with 0.1rc1, 0.1a1, 0.1b1, etc.
-__version__ = "1.17a1"
+__version__ = "1.17.0"
 
 import warnings as _warnings
 
