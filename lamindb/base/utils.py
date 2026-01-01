74532115bce7b22f40474abb18fe10c7e0b27dae
Alex Wolf <f.alexander.wolf@gmail.com>
2026-01-01 13:11:59 +0100

    🚧 Add debugging statements

--- lamindb/base/utils.py
+++ lamindb/base/utils.py
@@ -44,19 +44,30 @@ class raise_error_if_called_on_object:
         wraps(func)(self)
 
     def __get__(self, obj, objtype=None):
+        print(f"DEBUG __get__ called for {self.func.__name__}")
+        print(f"  obj: {obj}")
+        print(f"  objtype: {objtype}")
+        print(f"  obj is not None: {obj is not None}")
+
         if obj is not None:  # Called on an instance
+            print("  -> Returning error_raiser for instance call")
+
             # Return a wrapper that will raise when called, not immediately
             def error_raiser(*args, **kwargs):
                 class_name = objtype.__name__ if objtype else obj.__class__.__name__
+                print(f"DEBUG error_raiser called: {class_name}.{self.func.__name__}")
                 raise TypeError(
                     f"{class_name}.{self.func.__name__}() is a class method and must be called on the {class_name} class, not on a {class_name} object"
                 )
 
             return error_raiser
+
         # Called on the class - return the bound classmethod
+        print("  -> Returning bound classmethod for class call")
         return self.func.__get__(obj, objtype)
 
     def __call__(self, *args, **kwargs):
+        print(f"DEBUG __call__ invoked for {self.func.__name__}")
         return self.func(*args, **kwargs)
 
 
