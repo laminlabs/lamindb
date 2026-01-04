6a552fa93612a4cb7caabb0bce51a0f75bd2f72c
Alex Wolf <f.alexander.wolf@gmail.com>
2026-01-04 17:35:02 +0100

    ♻️ Better

--- lamindb/models/run.py
+++ lamindb/models/run.py
@@ -296,7 +296,7 @@ class Run(SQLRecord):
     report: Artifact | None = ForeignKey(
         "Artifact", PROTECT, null=True, related_name="_report_of", default=None
     )
-    """The report of this run such as an `.html` file."""
+    """The report of this run such as an `.html` or `.txt` file."""
     _logfile: Artifact | None = ForeignKey(
         "Artifact", PROTECT, null=True, related_name="_logfile_of", default=None
     )
