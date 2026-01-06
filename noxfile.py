dae03170097cadbdc17e55db7e9561c52d5bba11
Alex Wolf <f.alexander.wolf@gmail.com>
2026-01-06 23:47:56 +0100

    💚 Fix

--- noxfile.py
+++ noxfile.py
@@ -173,7 +173,7 @@ def install_ci(session, group):
     if IS_PR or group == "docs":
         run(
             session,
-            "uv pip install --system ./sub/lamindb-setup ./sub/lamin-cli ./sub/bionty",
+            "uv pip install --system ./sub/lamindb-setup ./sub/lamin-cli ./sub/bionty ./sub/wetlab",
         )
     if group == "permissions":
         # have to install after lamindb installation
