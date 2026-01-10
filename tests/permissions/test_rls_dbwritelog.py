665d929aa0ee82a9bd7f59f65cafc3e23a11b55e
Koncopd <koncopd@gmail.com>
2026-01-10 23:40:38 +0200

    Rm special test for block

--- tests/permissions/test_rls_dbwritelog.py
+++ tests/permissions/test_rls_dbwritelog.py
@@ -261,10 +261,6 @@ def test_fine_grained_permissions_account_and_dbwrite():
     assert ulabel.projects.all().count() == 1
     # check select of a link table referencing unavailable rows
     assert ln.ULabel.get(name="select_ulabel").projects.all().count() == 0
-    # test RootBlock, can do due to write access to some spaces
-    block = ln.models.Block(key="README.md", content="test").save()
-    block.content = "test 2"
-    block.save()
     # test SpaceBlock
     space = ln.Space.get(name="select access")
     with pytest.raises(ln.errors.NoWriteAccess):
