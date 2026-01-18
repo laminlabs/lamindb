a2c4461c0cec1c275433d17fa00d5434f19363c0
Alex Wolf <f.alexander.wolf@gmail.com>
2026-01-18 21:11:12 +0100

    ♻️ Fix type signature

--- tests/profiling/profile_import_lamindb.py
+++ tests/profiling/profile_import_lamindb.py
@@ -3,7 +3,7 @@ from pyinstrument import Profiler
 profiler = Profiler()
 profiler.start()
 
-import lamindb  # noqa: E402, F401
+import lamindb as ln  # noqa: E402
 
 profiler.stop()
 
@@ -11,6 +11,18 @@ duration = profiler.last_session.duration
 threshold = 2.0
 
 print(profiler.output_text())
+
+ln.connect("laminlabs/lamindata")
+ln.track()
+sheet = ln.Record.get(name="ImportLaminDB")
+record = ln.Record(type=sheet).save()
+record.features.add_values(
+    {
+        "duration_in_sec": duration,
+        "lamindb_version": ln.__version__,
+    }
+)
+ln.finish()
 
 if duration > threshold:
     print(f"ERROR: Import time {duration:.3f}s exceeds threshold {threshold:.3f}s")
