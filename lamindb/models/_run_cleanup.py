696a35e45a955bcdb0c259e9d815bff336d6d9cf
Alex Wolf <f.alexander.wolf@gmail.com>
2026-02-01 01:31:27 +0100

    ♻️ Rather with id

--- lamindb/models/_run_cleanup.py
+++ lamindb/models/_run_cleanup.py
@@ -38,9 +38,9 @@ def main() -> None:
             )
             try:
                 artifact.delete(permanent=True)
-                logger.important(f"deleted artifact {artifact.uid}")
+                logger.important(f"deleted artifact id={artifact.id}")
             except Exception as e:
-                logger.error(f"did not delete artifact {artifact.uid}: {e}")
+                logger.error(f"did not delete artifact id={artifact.id}: {e}")
                 pass
 
 
