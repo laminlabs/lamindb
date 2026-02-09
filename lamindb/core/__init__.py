598f93abc77aa0fb4b4df2ec6dab3d8c8dd180d1
Alex Wolf <f.alexander.wolf@gmail.com>
2026-02-09 11:40:38 +0100

    ♻️ Refactor loaders

--- lamindb/core/__init__.py
+++ lamindb/core/__init__.py
@@ -36,16 +36,22 @@ Modules:
 from lamin_utils import logger
 from lamin_utils._inspect import InspectResult
 
-from .. import errors as exceptions
+from .. import errors as exceptions  # backward compat
 from ..examples import datasets  # backward compat
-from . import loaders, subsettings, types
+from . import subsettings, types
 from ._context import Context
 from ._settings import Settings
 
 
 def __getattr__(name: str):
-    # need to lazy import MappedCollection - it should likely moved out of the
-    # root namespace over time
+    # need to lazy import a few auxliary modules to maintain
+    # backward compat
+    # none of them should have been eagerly imported in the first place
+    if name == "loaders":
+        import importlib
+
+        loaders = importlib.import_module(".loaders", package=__name__)
+        return loaders
     if name == "MappedCollection":
         from ._mapped_collection import MappedCollection
 
--- lamindb/core/loaders.py
+++ lamindb/core/loaders.py
@@ -19,6 +19,8 @@ import re
 from pathlib import Path
 from typing import TYPE_CHECKING, Any
 
+import pandas as pd
+from anndata import read_h5ad
 from lamin_utils import logger
 from lamindb_setup import settings as setup_settings
 from lamindb_setup.core.upath import (
@@ -28,22 +30,18 @@ from lamindb_setup.core.upath import (
 )
 
 if TYPE_CHECKING:
-    import pandas as pd
     from anndata import AnnData
     from lamindb_setup.types import UPathStr
     from mudata import MuData
 
     from lamindb.core.types import ScverseDataStructures
 
+try:
+    from ..core.storage._zarr import load_zarr
+except ImportError:
 
-def load_zarr(storepath, **kwargs):
-    """Lazy-import to avoid loading storage at package import."""
-    try:
-        from ..core.storage._zarr import load_zarr as _load_zarr
-
-        return _load_zarr(storepath, **kwargs)
-    except ImportError:
-        raise ImportError("Please install zarr: pip install 'lamindb[zarr]'") from None
+    def load_zarr(storepath):  # type: ignore
+        raise ImportError("Please install zarr: pip install 'lamindb[zarr]'")
 
 
 is_run_from_ipython = getattr(builtins, "__IPYTHON__", False)
@@ -59,18 +57,14 @@ def load_fcs(*args, **kwargs) -> AnnData:
     return readfcs.read(*args, **kwargs)
 
 
-def load_tsv(path: UPathStr, **kwargs):
+def load_tsv(path: UPathStr, **kwargs) -> pd.DataFrame:
     """Load `.tsv` file to `DataFrame`."""
-    import pandas as pd
-
     path_sanitized = Path(path)
     return pd.read_csv(path_sanitized, sep="\t", **kwargs)
 
 
 def load_h5ad(filepath, **kwargs) -> AnnData:
     """Load an `.h5ad` file to `AnnData`."""
-    from anndata import read_h5ad
-
     fs, filepath = infer_filesystem(filepath)
     compression = kwargs.pop("compression", "infer")
     with fs.open(filepath, mode="rb", compression=compression) as file:
@@ -158,71 +152,35 @@ def load_rds(path: UPathStr) -> UPathStr:
     return path
 
 
-_file_loaders_cache: dict[str, Any] | None = None
+FILE_LOADERS = {
+    ".csv": pd.read_csv,
+    ".csv.gz": pd.read_csv,
+    ".csv.tar.gz": pd.read_csv,
+    ".tsv": load_tsv,
+    ".tsv.gz": load_tsv,
+    ".tsv.tar.gz": load_tsv,
+    ".h5ad": load_h5ad,
+    ".h5ad.gz": load_h5ad,
+    ".h5ad.tar.gz": load_h5ad,
+    ".parquet": pd.read_parquet,
+    ".fcs": load_fcs,
+    ".zarr": load_zarr,
+    ".anndata.zarr": load_zarr,
+    ".html": load_html,
+    ".json": load_json,
+    ".vitessce.json": load_json,
+    ".yaml": load_yaml,
+    ".h5mu": load_h5mu,
+    ".gif": load_image,
+    ".jpg": load_image,
+    ".png": load_image,
+    ".svg": load_svg,
+    ".rds": load_rds,
+    ".txt": load_txt,
+    ".fasta": load_txt,
+}
 
-
-def _get_file_loaders() -> dict[str, Any]:
-    """Lazy-build FILE_LOADERS to avoid importing pandas/anndata at module load."""
-    global _file_loaders_cache
-    if _file_loaders_cache is None:
-        import pandas as pd
-
-        _file_loaders_cache = {
-            ".csv": pd.read_csv,
-            ".csv.gz": pd.read_csv,
-            ".csv.tar.gz": pd.read_csv,
-            ".tsv": load_tsv,
-            ".tsv.gz": load_tsv,
-            ".tsv.tar.gz": load_tsv,
-            ".h5ad": load_h5ad,
-            ".h5ad.gz": load_h5ad,
-            ".h5ad.tar.gz": load_h5ad,
-            ".parquet": pd.read_parquet,
-            ".fcs": load_fcs,
-            ".zarr": load_zarr,
-            ".anndata.zarr": load_zarr,
-            ".html": load_html,
-            ".json": load_json,
-            ".vitessce.json": load_json,
-            ".yaml": load_yaml,
-            ".h5mu": load_h5mu,
-            ".gif": load_image,
-            ".jpg": load_image,
-            ".png": load_image,
-            ".svg": load_svg,
-            ".rds": load_rds,
-            ".txt": load_txt,
-            ".fasta": load_txt,
-        }
-    return _file_loaders_cache
-
-
-SUPPORTED_SUFFIXES = [
-    ".csv",
-    ".csv.gz",
-    ".csv.tar.gz",
-    ".tsv",
-    ".tsv.gz",
-    ".tsv.tar.gz",
-    ".h5ad",
-    ".h5ad.gz",
-    ".h5ad.tar.gz",
-    ".parquet",
-    ".fcs",
-    ".zarr",
-    ".anndata.zarr",
-    ".html",
-    ".json",
-    ".vitessce.json",
-    ".yaml",
-    ".h5mu",
-    ".gif",
-    ".jpg",
-    ".png",
-    ".svg",
-    ".txt",
-    ".fasta",
-]
+SUPPORTED_SUFFIXES = [sfx for sfx in FILE_LOADERS.keys() if sfx != ".rds"]
 """Suffixes with defined artifact loaders."""
 
 
@@ -238,7 +196,7 @@ def load_to_memory(
     """
     filepath = create_path(filepath)
     suffix = extract_suffix_from_path(filepath)
-    loader = _get_file_loaders().get(suffix, None)
+    loader = FILE_LOADERS.get(suffix, None)
     if loader is None:
         raise NotImplementedError(
             f"There is no loader for {suffix} files. Use .cache() to get the path."
