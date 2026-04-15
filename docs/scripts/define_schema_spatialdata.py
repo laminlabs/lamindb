57feffc3601b4a2d14652ae715ff8da45b846ccd
Alex Wolf <f.alexander.wolf@gmail.com>
2026-04-15 10:33:08 +0200

    :memo: Prettify and simplify spatialdata schema creation script (#3642)

--- docs/scripts/define_schema_spatialdata.py
+++ docs/scripts/define_schema_spatialdata.py
@@ -1,46 +1,31 @@
 import lamindb as ln
 import bionty as bt
 
+# a very comprehensive schema for different slots of a SpatialData object
 
-attrs_schema = ln.Schema(
-    features=[
-        ln.Feature(name="bio", dtype=dict).save(),
-        ln.Feature(name="tech", dtype=dict).save(),
-    ],
+# define or query features
+bio_dict = ln.Feature(name="bio", dtype=dict).save()
+tech_dict = ln.Feature(name="tech", dtype=dict).save()
+disease = ln.Feature(name="disease", dtype=bt.Disease, coerce=True).save()
+developmental_stage = ln.Feature(
+    name="developmental_stage",
+    dtype=bt.DevelopmentalStage,
+    coerce=True,
 ).save()
+assay = ln.Feature(name="assay", dtype=bt.ExperimentalFactor, coerce=True).save()
+sample_region = ln.Feature(name="sample_region", dtype=str).save()
+analysis = ln.Feature(name="analysis", dtype=str).save()
 
-sample_schema = ln.Schema(
-    features=[
-        ln.Feature(name="disease", dtype=bt.Disease, coerce=True).save(),
-        ln.Feature(
-            name="developmental_stage",
-            dtype=bt.DevelopmentalStage,
-            coerce=True,
-        ).save(),
-    ],
-).save()
-
-tech_schema = ln.Schema(
-    features=[
-        ln.Feature(name="assay", dtype=bt.ExperimentalFactor, coerce=True).save(),
-    ],
-).save()
-
-obs_schema = ln.Schema(
-    features=[
-        ln.Feature(name="sample_region", dtype="str").save(),
-    ],
-).save()
-
-uns_schema = ln.Schema(
-    features=[
-        ln.Feature(name="analysis", dtype="str").save(),
-    ],
-).save()
-
-# Schema enforces only registered Ensembl Gene IDs are valid (maximal_set=True)
+# define or query schema components
+attrs_schema = ln.Schema([bio_dict, tech_dict]).save()
+sample_schema = ln.Schema([disease, developmental_stage]).save()
+tech_schema = ln.Schema([assay]).save()
+obs_schema = ln.Schema([sample_region]).save()
+uns_schema = ln.Schema([analysis]).save()
+# enforces only registered Ensembl Gene IDs pass validation (maximal_set=True)
 varT_schema = ln.Schema(itype=bt.Gene.ensembl_gene_id, maximal_set=True).save()
 
+# compose the SpatialData schema
 sdata_schema = ln.Schema(
     name="spatialdata_blobs_schema",
     otype="SpatialData",
