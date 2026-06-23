9decac2b512d51fa3349ed2d73908091e0e7d7fd
Alex Wolf <f.alexander.wolf@gmail.com>
2026-06-23 17:27:52 +0200

    ♻️ Scope `mini_immuno` schema example to a feature type (#3760)

--- lamindb/examples/datasets/define_mini_immuno_features_labels.py
+++ lamindb/examples/datasets/define_mini_immuno_features_labels.py
@@ -10,11 +10,21 @@ bt.CellType.from_source(name="B cell").save()
 bt.CellType.from_source(name="T cell").save()
 
 # define valid features
-ln.Feature(name="perturbation", dtype=perturbation_type).save()
-ln.Feature(name="cell_type_by_expert", dtype=bt.CellType).save()
-ln.Feature(name="cell_type_by_model", dtype=bt.CellType).save()
-ln.Feature(name="assay_oid", dtype=bt.ExperimentalFactor.ontology_id).save()
-ln.Feature(name="concentration", dtype=str).save()
-ln.Feature(name="treatment_time_h", dtype="num", coerce=True).save()
-ln.Feature(name="donor", dtype=str, nullable=True).save()
-ln.Feature(name="donor_ethnicity", dtype=list[bt.Ethnicity]).save()
+mitype = ln.Feature(name="mini_immuno", is_type=True).save()
+ln.Feature(name="perturbation", dtype=perturbation_type, type=mitype).save()
+ln.Feature(name="cell_type_by_expert", dtype=bt.CellType, type=mitype).save()
+ln.Feature(name="cell_type_by_model", dtype=bt.CellType, type=mitype).save()
+ln.Feature(
+    name="assay_oid",
+    dtype=bt.ExperimentalFactor.ontology_id,
+    type=mitype,
+).save()
+ln.Feature(name="concentration", dtype=str, type=mitype).save()
+ln.Feature(
+    name="treatment_time_h",
+    dtype="num",
+    coerce=True,
+    type=mitype,
+).save()
+ln.Feature(name="donor", dtype=str, nullable=True, type=mitype).save()
+ln.Feature(name="donor_ethnicity", dtype=list[bt.Ethnicity], type=mitype).save()
--- lamindb/examples/datasets/define_mini_immuno_schema_flexible.py
+++ lamindb/examples/datasets/define_mini_immuno_schema_flexible.py
@@ -1,14 +1,16 @@
 import lamindb as ln
 
+mitype = ln.Feature.get(name="mini_immuno", is_type=True)
+
 schema = ln.Schema(
-    name="Mini immuno schema",
+    name="mini_immuno",
     features=[
-        ln.Feature.get(name="perturbation"),
-        ln.Feature.get(name="cell_type_by_model"),
-        ln.Feature.get(name="assay_oid"),
-        ln.Feature.get(name="donor"),
-        ln.Feature.get(name="concentration"),
-        ln.Feature.get(name="treatment_time_h"),
+        ln.Feature.get(name="perturbation", type=mitype),
+        ln.Feature.get(name="cell_type_by_model", type=mitype),
+        ln.Feature.get(name="assay_oid", type=mitype),
+        ln.Feature.get(name="donor", type=mitype),
+        ln.Feature.get(name="concentration", type=mitype),
+        ln.Feature.get(name="treatment_time_h", type=mitype),
     ],
     flexible=True,  # _additional_ columns in a dataframe are validated & annotated
 ).save()
