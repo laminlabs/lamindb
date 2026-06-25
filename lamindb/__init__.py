99c4eebd1854966d01d9a60e800f9ef7b5ec7072
Alex Wolf <f.alexander.wolf@gmail.com>
2026-06-25 12:00:21 +0200

    ⬆️ Make a pre-release

--- lamindb/__init__.py
+++ lamindb/__init__.py
@@ -150,7 +150,7 @@ Developer API
 
 # ruff: noqa: I001
 # denote a release candidate for 0.1.0 with 0.1rc1, 0.1a1, 0.1b1, etc.
-__version__ = "2.6.1"
+__version__ = "2.7a1"
 
 import warnings as _warnings
 
