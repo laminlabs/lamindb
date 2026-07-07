f0f708e4247a7f32f9497e47888530cd1109fecf
Alex Wolf <f.alexander.wolf@gmail.com>
2026-07-07 13:12:25 +0200

    💚 Fix curate.md

--- docs/scripts/curate_anndata_uns.py
+++ docs/scripts/curate_anndata_uns.py
@@ -2,7 +2,7 @@ import lamindb as ln
 
 ln.examples.datasets.mini_immuno.define_features_labels()
 adata = ln.examples.datasets.mini_immuno.get_dataset1(otype="AnnData")
-schema = ln.Schema.get(name="Study metadata schema")
+schema = ln.Schema.get(name="anndata_study_metadata")
 artifact = ln.Artifact.from_anndata(
     adata, schema=schema, key="examples/mini_immuno_uns.h5ad"
 )
