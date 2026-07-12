08392bbd2f1aeae3a118c5fc4e22ae7ddfe0be1b
Alex Wolf <f.alexander.wolf@gmail.com>
2026-07-12 22:28:58 +0200

    ♻️ Proper deprecation warning

--- lamindb/base/ids.py
+++ lamindb/base/ids.py
@@ -1,1 +1,9 @@
+import warnings
+
+warnings.warn(
+    "`lamindb.base.ids` is deprecated, use `lamindb.base.uids` instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
 from .uids import *  # noqa: F403
--- lamindb/core/exceptions.py
+++ lamindb/core/exceptions.py
@@ -1,1 +1,9 @@
+import warnings
+
+warnings.warn(
+    "`lamindb.core.exceptions` is deprecated, use `lamindb.errors` instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
 from ..errors import *  # noqa: F403 backward compat
