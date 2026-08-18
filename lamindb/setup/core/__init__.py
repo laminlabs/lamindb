834c2622f96bacf85f6d7d610b1b717f1207ad3b
Alex Wolf <f.alexander.wolf@gmail.com>
2026-08-18 15:25:02 +0200

    💚 Fix docs

--- lamindb/setup/core/__init__.py
+++ lamindb/setup/core/__init__.py
@@ -2,3 +2,7 @@ import lamindb_setup as _lamindb_setup
 from lamindb_setup.core import *  # noqa: F403
 
 __doc__ = _lamindb_setup.core.__doc__.replace("lamindb_setup", "lamindb.setup")
+
+
+def __getattr__(name: str):
+    return getattr(_lamindb_setup.core, name)
